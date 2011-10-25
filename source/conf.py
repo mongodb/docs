14c20fb9e9026c0a34a2e3faebe54cd846d70673
Sam Kleinman <samk@10gen.com>
2011-10-25 14:57:16 -0400

    meta: mms, removing indexes

--- source/conf.py
+++ source/conf.py
@@ -141,7 +141,7 @@ html_static_path = ['_static']
 #html_use_modindex = True
 
 # If false, no index is generated.
-#html_use_index = True
+html_use_index = False
 
 # If true, the index is split into individual pages for each letter.
 #html_split_index = False
