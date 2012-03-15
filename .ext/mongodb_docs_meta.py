7c7a24749afe2de5421f11ee4a54db944a22e50b
Eliot Horowitz <eliot@10gen.com>
2012-03-15 13:44:34 -0400

    make script compatible with older python

--- .ext/mongodb_docs_meta.py
+++ .ext/mongodb_docs_meta.py
@@ -1,11 +1,17 @@
 #
 #
 import subprocess
+import re
 
-def shell_value(command):
-    output = subprocess.check_output(command, shell=True).decode().split('\n')[0]
-    return output
+def shell_value( args ):
+    import subprocess
+    if isinstance( args , str ):
+        r = re.compile( "\s+" )
+        args = r.split( args )
+    p = subprocess.Popen( args , stdout=subprocess.PIPE , stderr=subprocess.PIPE )
+    r = p.communicate()
+    return r[0]
 
 class VersionMeta:
-    branch = shell_value('git symbolic-ref HEAD 2>/dev/null').split('/')[2]
+    branch = shell_value('git symbolic-ref HEAD').split('/')[2]
     commit = shell_value('git rev-parse --verify HEAD')
