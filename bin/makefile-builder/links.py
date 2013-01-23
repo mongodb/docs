57ad5b66ccb8f480679b3cd8e56bee2e5e41fd37
Sam Kleinman <samk@10gen.com>
2013-01-23 11:53:22 -0500

    build: fixing link problems

--- bin/makefile-builder/links.py
+++ bin/makefile-builder/links.py
@@ -36,13 +36,7 @@ def make_link(make_target, link_target, makefile_block):
         m.append_var('LINKS', make_target, makefile_block)
         m.target(make_target, link_location, makefile_block)
 
-    if makefile_block == 'redirect':
-        m.job('rm -rf %s' % make_target, block=makefile_block)
-        m.job('mkdir -p %s' % link_location)
-
-    m.job('@ln -s -f %s  $(notdir $@)' % link_target, makefile_block)
-    m.job('@mv $(notdir $@) %s' % link_location, makefile_block)
-
+    m.job('@bin/create-link %s $(notdir $@) %s' % ( link_target, link_location), makefile_block)
     m.msg('[symlink]: created a link at: %s' % make_target, makefile_block)
     m.newline(block=makefile_block)
 
