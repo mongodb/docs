ed7357c4b5b42d3077573c6ad1db393c121fdc44
Sam Kleinman <samk@10gen.com>
2013-03-01 13:24:29 -0500

    build: cleaning up dynamic url build generators

--- bin/update_release.py
+++ bin/update_release.py
@@ -2,47 +2,60 @@
 
 import sys
 import os
+import argparse
 import sphinx_conf
 
 def cli():
-    try:
-        builder = sys.argv[1]
-    except IndexError:
-        exit('[build]: Error. No builder specified.')
+    parser = argparse.ArgumentParser('Generate install files.')
+    parser.add_argument(choices=['linux-32', 'linux-64', 'osx-64'], dest='builder')
+    parser.add_argument(choices=['core', 'amz', 'rhel6', 'ubuntu11', 'ubuntu12', 'suse'], dest='release')
+    parser.add_argument(dest='output')
 
-    if builder == 'linux-32':
+    p = parser.parse_args()
+
+    if p.builder == 'linux-32':
         builder = 'linux-i686'
         platform = 'linux'
-    elif builder == 'linux-64':
+    elif p.builder == 'linux-64':
         builder = 'linux-x86_64'
         platform = 'linux'
-    elif builder == 'osx-64':
+    elif p.builder == 'osx-64':
         builder = 'osx-x86_64'
         platform = 'osx'
-    else:
-        print(sys.argv[1])
-        exit('[build]: Error. Builder argument, must be "linux-32", "linux-64", or "osx".')
 
-    try:
-        outputfile = sys.argv[2]
-    except IndexError:
-        exit('[build]: Error. No output file specified to "update_release.py"')
+    if p.release == 'core':
+        release = 'core'
+    if p.release == 'amz':
+        release = 'amzn64'
+    elif p.release == 'rhel6':
+        release = 'rhel62'
+    elif p.release == 'ubuntu11':
+        release = 'ubuntu1104'
+    elif p.release == 'ubuntu12':
+        release = 'ubuntu1204'
+    elif p.release == 'suse':
+        release = 'suse11'
 
-    return { 'outputfile': outputfile, 'builder': builder, 'platform': platform }
+    return  { 'outputfile': p.output, 'builder': builder, 'platform': platform, 'release': release }
 
-def generate_output(builder, platform, version):
+def generate_output(builder, platform, version, release):
     BREAK = '\n\n'
     INDENT = '   '
 
-    output = ('.. code-block:: sh' + '\n\n' +
-              '   curl http://downloads.mongodb.org/' + platform + '/mongodb-' +
-              builder + '-' + version + '.tgz > mongo.tgz' + '\n')
+    if release == 'core':
+        output = ('.. code-block:: sh' + '\n\n' +
+                  '   curl http://downloads.mongodb.org/' + platform + '/mongodb-' +
+                  builder + '-' + version + '.tgz > mongodb.tgz' + '\n')
+    else:
+        output = ('.. code-block:: sh' + '\n\n' +
+                  '   curl http://downloads.10gen.com/linux/mongodb-' + builder + '-subscription-' +
+                  release + '-' + version + '.tgz > mongodb.tgz' + '\n')
 
     return output
 
 def main():
     interface = cli()
-    output = generate_output(interface['builder'], interface['platform'], sphinx_conf.version)
+    output = generate_output(interface['builder'], interface['platform'], sphinx_conf.version, interface['release'])
 
     with open(interface['outputfile'], 'w') as f:
         f.write(output)
