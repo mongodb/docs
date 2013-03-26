15612f3e23e96de2278b5b0c69668b8c40f13d4c
Sam Kleinman <samk@10gen.com>
2013-03-26 17:13:34 -0400

    build: centralizing path information, improving organization of the init build phase

--- bin/makefile-builder/meta.py
+++ bin/makefile-builder/meta.py
@@ -3,23 +3,26 @@ import sys
 import os.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
-import mongodb_docs_meta as mongo_meta
+import mongodb_docs_meta as docs_meta
 from makecloth import MakefileCloth
 m = MakefileCloth()
 
 def generate_meta():
     m.section_break('branch/release meta', block='rel')
-    m.var('manual-branch', mongo_meta.MANUAL_BRANCH, block='rel')
-    m.var('current-branch', str(mongo_meta.get_branch()), block='rel')
-    m.var('last-commit', str(mongo_meta.get_commit()), block='rel')
-    m.var('current-if-not-manual', str(mongo_meta.get_manual_path()), block='rel')
+    m.var('manual-branch', docs_meta.MANUAL_BRANCH, block='rel')
+    m.var('current-branch', str(docs_meta.get_branch()), block='rel')
+    m.var('last-commit', str(docs_meta.get_commit()), block='rel')
+    m.var('current-if-not-manual', str(docs_meta.get_manual_path()), block='rel')
+
+    paths = docs_meta.render_paths(True)
 
     m.section_break('file system paths', block='paths')
-    m.var('public-output', '$(output)/public', block='paths')
-    m.var('branch-output', '$(output)/$(current-branch)', block='paths')
-    m.var('branch-source', '$(branch-output)/branch-source')
-    m.var('branch-source-current', '$(branch-source)-current')
-    m.var('public-branch-output', '$(public-output)/$(current-branch)', block='paths')
+    m.var('output', paths['output'], block='paths')
+    m.var('public-output', paths['public'], block='paths')
+    m.var('branch-output', paths['branch-output'], block='paths')
+    m.var('rst-include', paths['includes'], block='paths')
+    m.var('branch-source', paths['branch-source'], block='paths')
+    m.var('public-branch-output', paths['branch-staging'], block='paths')
 
 def main():
     generate_meta()
