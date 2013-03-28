b5e9a693cc25ccfbc01b25e8db1e719210c2a09b
Sam Kleinman <samk@10gen.com>
2013-03-28 12:19:23 -0400

    build: moving more deployment to fab, and renaming meta script

--- bin/makecloth/meta.py
+++ bin/makecloth/meta.py
@@ -3,7 +3,7 @@ import sys
 import os.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../')))
 
-import mongodb_docs_meta as docs_meta
+import docs_meta as docs_meta
 from makecloth import MakefileCloth
 m = MakefileCloth()
 
