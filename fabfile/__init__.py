651de3ddb11bdcdfd637cb801c9a9261824a563c
Sam Kleinman <samk@10gen.com>
2013-05-21 19:15:08 -0400

    build: refactoring intersphinx downloading

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -6,3 +6,11 @@ import deploy
 import sphinx
 import clean
 import git
+import intersphinx
+
+import fabric
+fabric.state.output.status = False
+fabric.state.output.aborts = True
+fabric.state.output.warnings = True
+fabric.state.output.running = True
+fabric.state.output.user = True
