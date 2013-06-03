31504d6dee45a8b80c7d6a7b8d228eaef8a7cc8b
Sam Kleinman <samk@10gen.com>
2013-06-03 17:41:29 -0400

    build: cleanup of unneccessary build block

--- fabfile/deploy.py
+++ fabfile/deploy.py
@@ -1,6 +1,7 @@
 from fabric.api import cd, local, task, abort, env, puts
 from fabric.utils import _AttributeDict as ad
-from docs_meta import PUBLISHED_BRANCHES, MANUAL_BRANCH, render_paths, get_branch
+from docs_meta import PUBLISHED_BRANCHES, MANUAL_BRANCH, render_paths, get_branch, get_commit
+from urllib2 import urlopen
 
 env.paths = render_paths('dict')
 
@@ -18,6 +19,7 @@ def staging(branch=None):
 
     env.hosts = ['public@test.docs.10gen.cc']
     env.remote_rsync_location = '/srv/public/test/' + str(branch)
+    env.release_info_url = 'http://test.docs.10gen.cc/{0}/release.txt'.format(str(branch))
 
 @task
 def production(branch=None):
@@ -25,6 +27,7 @@ def production(branch=None):
 
     env.hosts = ['www@www-c1.10gen.cc', 'www@www-c2.10gen.cc']
     env.remote_rsync_location = '/data/sites/docs/' + str(branch)
+    env.release_info_url = 'http://docs.mongodb.org/{0}/release.txt'.format(str(branch))
 
 def build_rsync_cmd(local_path, remote_string, recursive=True, delete=None):
     return [
@@ -36,6 +39,15 @@ def build_rsync_cmd(local_path, remote_string, recursive=True, delete=None):
     ]
 
 @task
+def check():
+    r = urlopen(env.release_info_url).readlines()[0].split('\n')[0]
+
+    if get_commit() == r:
+        abort('ERROR: the current published version of is the same as the current commit. Make a new commit before publishing.')
+    else:
+        puts('[build]: the current commit is different than the published version on. Building now.')
+
+@task
 def push(delete=None):
     if not env.hosts:
         abort('must specify a deployment mode: staging or production')
@@ -43,7 +55,6 @@ def push(delete=None):
     cmd = build_rsync_cmd(local_path=env.paths['branch-staging'] + '/',
                                  remote_string=env.host_string + ':' + env.remote_rsync_location,
                                  delete=delete)
-
     local(' '.join(cmd))
 
 @task
