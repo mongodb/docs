3d8dd0c77c766e7ae2b90f6b8370f5772b1a360b
Sam Kleinman <samk@10gen.com>
2013-07-03 14:40:01 -0400

    BUMP 2.4.5

--- conf.py
+++ conf.py
@@ -42,7 +42,7 @@ language = 'en'
 project = u'mongodb-manual'
 copyright = u'2011-' + meta['date'] + ', 10gen, Inc.'
 version = '2.4'
-release = '2.4.4'
+release = '2.4.5'
 
 BREAK = '\n'
 rst_epilog = ('.. |branch| replace:: ``' + meta['branch'] + '``' + BREAK +
