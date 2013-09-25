6b316afd896b1bae4b95d736fc9121b766cb561f
Sam Kleinman <samk@10gen.com>
2013-09-24 20:20:37 -0400

    config: updating external links

--- conf.py
+++ conf.py
@@ -69,6 +69,8 @@ extlinks = {
     'manual': ('http://docs.mongodb.org/manual%s', ''),
     'ecosystem': ('http://docs.mongodb.org/ecosystem%s', ''),
     'meta-driver': ('http://docs.mongodb.org/meta-driver/latest%s', ''),
+    'mms': ('http://mms.mongodb.org/help%s', ''),
+    'mms-hosted': ('http://mms.mongodb.org/help-hosted%s', ''),
     'about': ('http://www.mongodb.org/about%s', '')
 }
 
