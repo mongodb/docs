e4363fe4b00e71e0ad51b83cce2cc374d4f7a252
Sam Kleinman <samk@10gen.com>
2014-01-14 12:02:55 -0500

    build: updating bootstrap

--- bootstrap.py
+++ bootstrap.py
@@ -4,7 +4,9 @@ import argparse
 import yaml
 import sys
 
-master_conf = os.path.abspath(os.path.join('bin', 'docs_meta.yaml'))
+project_root = os.path.abspath(os.path.dirname(__file__))
+
+master_conf = os.path.join(project_root, 'bin', 'docs_meta.yaml')
 
 with open(master_conf, 'r') as f:
     conf = yaml.safe_load(f)
@@ -13,7 +15,7 @@ buildsystem = conf['build']['paths']['buildsystem']
 
 sys.path.append(os.path.join(buildsystem, 'bin'))
 
-def bootstrap():
+def bootstrap_init():
     repo = 'git://github.com/{0}.git'.format(conf['git']['remote']['tools'])
 
     if os.path.exists(buildsystem):
@@ -24,25 +26,26 @@ def bootstrap():
         cmd.append(['git', 'pull', '--quiet', 'origin', 'master'])
 
         for c in cmd:
-            p = subprocess.Popen(c, cwd=buildsystem)
-            p.wait()
-        print('[bootstrap]: updated git repository.')
-    else:
-        p = subprocess.Popen([ 'git', 'clone', repo, buildsystem])
-        p.wait()
+            subprocess.call(c, cwd=buildsystem)
 
-        import bootstrap_helper
+        print('[bootstrap]: updated git repository.')
+
+def bootstrap_base():
+    if not os.path.exists(buildsystem):
+        subprocess.call([ 'git', 'clone', repo, buildsystem])
         print('[bootstrap]: created buildsystem directory.')
+
+    import bootstrap_helper
 
     bootstrap_helper.init_fabric(buildsystem, master_conf)
     bootstrap_helper.bootstrap()
+    print('[bootstrap]: initialized buildsystem.')
 
-    p = subprocess.Popen(['make', 'noop', '--silent', '-i'])
-    p.wait()
+    subprocess.call(['make', 'noop', '--silent', '-i'])
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('op', nargs='?', choices=['clean', 'setup'], default='setup')
+    parser.add_argument('op', nargs='?', choices=['clean', 'setup', 'safe'], default='setup')
     ui = parser.parse_args()
 
     if ui.op == 'clean':
@@ -51,9 +54,11 @@ def main():
             bootstrap_helper.clean_buildsystem(buildsystem, conf['build']['paths']['output'])
         except ImportError:
             exit('[bootstrap]: Buildsystem not installed.')
+    elif ui.op == 'safe':
+        bootstrap_base()
     else:
-        bootstrap()
-
+        bootstrap_init()
+        bootstrap_base()
 
 if __name__ == '__main__':
     main()
