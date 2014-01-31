fcdad4463acf24f69021f2d055de48ee47b2c29d
Sam Kleinman <samk@10gen.com>
2014-01-30 20:10:45 -0500

    DOCS-2622: fixing mms urls

--- conf.py
+++ conf.py
@@ -82,8 +82,8 @@ extlinks = {
     'manual': ('http://docs.mongodb.org/manual%s', ''),
     'ecosystem': ('http://docs.mongodb.org/ecosystem%s', ''),
     'meta-driver': ('http://docs.mongodb.org/meta-driver/latest%s', ''),
-    'mms': ('http://mms.mongodb.com/help%s', ''),
-    'mms-hosted': ('http://mms.mongodb.org/help-hosted%s', ''),
+    'mms': ('https://mms.mongodb.com/help%s', ''),
+    'mms-hosted': ('https://mms.mongodb.org/help-hosted%s', ''),
     'about': ('http://www.mongodb.org/about%s', '')
 }
 
