73f659ff120d1bfced59306d9d6f2de099b1ddc9
Sam Kleinman <samk@10gen.com>
2014-10-06 16:41:12 -0400

    build/conf: remove legacy compatibility code

--- conf.py
+++ conf.py
@@ -11,41 +11,14 @@ import datetime
 
 from sphinx.errors import SphinxError
 
-try:
-    from giza.config.runtime import RuntimeStateConfig
-    from giza.config.helper import fetch_config, get_versions, get_manual_path
-    from giza.tools.strings import dot_concat
+from giza.config.runtime import RuntimeStateConfig
+from giza.config.helper import fetch_config, get_versions, get_manual_path
+from giza.tools.strings import dot_concat
 
-    conf = fetch_config(RuntimeStateConfig())
-    intersphinx_libs = conf.system.files.data.intersphinx
-    sconf = conf.system.files.data.sphinx_local
+conf = fetch_config(RuntimeStateConfig())
+sconf = conf.system.files.data.sphinx_local
 
-    print os.path.join(conf.paths.projectroot, conf.paths.buildsystem, 'sphinxext')
-    sys.path.append(os.path.join(conf.paths.projectroot, conf.paths.buildsystem, 'sphinxext'))
-except ImportError:
-    try:
-        project_root = os.path.join(os.path.abspath(os.path.dirname(__file__)))
-    except NameError:
-        project_root = os.path.abspath(os.getcwd())
-
-    sys.path.append(project_root)
-
-    from bootstrap import buildsystem
-
-    sys.path.append(os.path.join(project_root, buildsystem, 'sphinxext'))
-    sys.path.append(os.path.join(project_root, buildsystem, 'bin'))
-
-    from utils.config import get_conf
-    from utils.serialization import ingest_yaml, ingest_yaml_list
-    from utils.project import get_versions, get_manual_path
-    from utils.structures import BuildConfiguration
-    from utils.strings import dot_concat
-
-    conf = get_conf()
-
-    conf.paths.projectroot = project_root
-    intersphinx_libs = ingest_yaml_list(os.path.join(conf.paths.builddata, 'intersphinx.yaml'))
-    sconf = BuildConfiguration(os.path.join(conf.paths.builddata, 'sphinx_local.yaml'))
+sys.path.append(os.path.join(conf.paths.projectroot, conf.paths.buildsystem, 'sphinxext'))
 
 try:
     tags
@@ -114,18 +87,11 @@ for i in conf.git.branches.published:
     extlinks[i] = ( ''.join([ conf.project.url, '/', i, '%s' ]), '' )
 
 
-try:
-    intersphinx_mapping = {}
-    for i in intersphinx_libs:
-        intersphinx_mapping[i.name] = ( i.url, os.path.join(conf.paths.projectroot,
-                                                                  conf.paths.output,
-                                                                  i.path))
-except AttributeError:
-    intersphinx_mapping = {}
-    for i in intersphinx_libs:
-        intersphinx_mapping[i['name']] = ( i['url'], os.path.join(conf.paths.projectroot,
-                                                                  conf.paths.output,
-                                                                  i['path']))
+intersphinx_mapping = {}
+for i in conf.system.files.data.intersphinx:
+    intersphinx_mapping[i.name] = ( i.url, os.path.join(conf.paths.projectroot,
+                                                        conf.paths.output,
+                                                        i.path))
 
 languages = [
     ("ar", "Arabic"),
@@ -193,29 +159,10 @@ html_sidebars = sconf.sidebars
 
 # -- Options for LaTeX output --------------------------------------------------
 
-try:
-    latex_documents = []
-    if 'pdfs' in conf.system.files.data:
-        for pdf in conf.system.files.data.pdfs:
-            latex_documents.append((pdf.source, pdf.output, pdf.title, pdf.author, pdf.doc_class))
-except AttributeError:
-    # we have an old-style config object, do the old
-    latex_documents = []
-
-    if tags.has('latex'):
-        pdf_conf_path = os.path.join(conf.paths.builddata, 'pdfs.yaml')
-        if os.path.exists(pdf_conf_path):
-            pdfs = ingest_yaml_list(pdf_conf_path)
-        else:
-            raise SphinxError('[WARNING]: skipping pdf builds because of missing {0} file'.format(pdf_conf_path))
-    else:
-        pdfs = []
-
-    for pdf in pdfs:
-        _latex_document = ( pdf['source'], pdf['output'], pdf['title'], pdf['author'], pdf['class'])
-        latex_documents.append( _latex_document )
-except NameError:
-    latex_documents = []
+latex_documents = []
+if 'pdfs' in conf.system.files.data:
+    for pdf in conf.system.files.data.pdfs:
+        latex_documents.append((pdf.source, pdf.output, pdf.title, pdf.author, pdf.doc_class))
 
 latex_preamble_elements = [ r'\DeclareUnicodeCharacter{FF04}{\$}',
                             r'\DeclareUnicodeCharacter{FF0E}{.}',
@@ -239,27 +186,10 @@ latex_appendices = []
 
 # -- Options for manual page output --------------------------------------------
 
-try:
-    man_pages = []
-    if 'manpages' in conf.system.files.data:
-        for mp in conf.system.files.data.manpages:
-            man_pages.append((mp.file, mp.name, mp.title, mp.authors, mp.section))
-except AttributeError as e:
-    # we have an old-style config object, do the old
-    if tags.has('man'):
-        man_page_conf_path = os.path.join(conf.paths.builddata, 'manpages.yaml')
-        if os.path.exists(man_page_conf_path):
-            man_page_definitions = ingest_yaml_list(man_page_conf_path)
-        else:
-            raise SphinxError('[WARNING]: skipping man builds because of missing {0} file'.format(man_page_conf_path))
-    else:
-        man_page_definitions = []
-
-    man_pages = []
-    for mp in man_page_definitions:
-        man_pages.append((mp['file'], mp['name'], mp['title'], mp['authors'], mp['section']))
-except NameError:
-    man_pages = []
+man_pages = []
+if 'manpages' in conf.system.files.data:
+    for mp in conf.system.files.data.manpages:
+        man_pages.append((mp.file, mp.name, mp.title, mp.authors, mp.section))
 
 # -- Options for Epub output ---------------------------------------------------
 
