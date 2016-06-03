71f71e27bcda80c346af58ce222a168248745c5c
kay <kay.kim@10gen.com>
2016-06-02 22:06:39 -0400

    seed initial project

--- README.md
+++
@@ -1,2 +0,0 @@
-# docs-spark-connector
-Document for 
---
+++ conf.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+#
+# MongoDB documentation build configuration file, created by
+# sphinx-quickstart on Mon Oct  3 09:58:40 2011.
+#
+# This file is execfile()d with the current directory set to its containing dir.
+
+import sys
+import os.path
+import datetime
+
+project_root = os.path.join(os.path.abspath(os.path.dirname(__file__)))
+sys.path.append(project_root)
+
+from giza.config.runtime import RuntimeStateConfig
+from giza.config.helper import fetch_config, get_versions, get_manual_path
+
+conf = fetch_config(RuntimeStateConfig())
+intersphinx_libs = conf.system.files.data.intersphinx
+sconf = conf.system.files.data.sphinx_local
+
+sys.path.append(os.path.join(conf.paths.projectroot, conf.paths.buildsystem, 'sphinxext'))
+
+# -- General configuration ----------------------------------------------------
+
+needs_sphinx = '1.0'
+
+extensions = [
+    'sphinx.ext.extlinks',
+    'sphinx.ext.todo',
+    'mongodb',
+    'directives',
+    'intermanual'
+]
+
+templates_path = ['.templates']
+exclude_patterns = []
+languages = []
+source_suffix = '.txt'
+
+master_doc = sconf.master_doc
+language = 'en'
+project = sconf.project
+copyright = u'2008-{0}'.format(datetime.date.today().year)
+
+version = '1.0'
+release = version
+
+rst_epilog = '\n'.join([
+    '.. |branch| replace:: ``{0}``'.format(conf.git.branches.current),
+    '.. |copy| unicode:: U+000A9',
+    '.. |year| replace:: {0}'.format(datetime.date.today().year),
+    '.. |ent-build| replace:: MongoDB Enterprise',
+    '.. |hardlink| replace:: http://docs.mongodb.com/docs-spark-connector',
+])
+
+extlinks = {
+    'manual': ('http://docs.mongodb.org/manual%s', ''),
+}
+
+intersphinx_mapping = {}
+
+try:
+    for i in intersphinx_libs:
+        intersphinx_mapping[i['name']] = ( i['url'], os.path.join(conf.paths.projectroot,
+                                                              conf.paths.output,
+                                                              i['path']))
+except:
+    for i in intersphinx_libs:
+        intersphinx_mapping[i.name] = ( i.url, os.path.join(conf.paths.projectroot,
+                                                              conf.paths.output,
+                                                              i.path))
+
+# -- Options for HTML output ---------------------------------------------------
+
+html_theme = sconf.theme.name
+html_theme_path = [ os.path.join(conf.paths.buildsystem, 'themes') ]
+html_title = conf.project.title
+htmlhelp_basename = 'MongoDBdoc'
+
+html_logo = ".static/logo-mongodb.png"
+html_static_path = ['source/.static']
+html_last_updated_fmt = '%b %d, %Y'
+
+html_copy_source = False
+html_use_smartypants = True
+html_domain_indices = True
+html_use_index = False
+html_split_index = False
+html_show_sourcelink = False
+html_show_sphinx = True
+html_show_copyright = True
+html_add_permalinks = None
+
+manual_edition_path = '{0}/{1}/{2}.{3}'
+
+html_theme_options = {
+    'branch': conf.git.branches.current,
+    'translations': languages,
+    'language': language,
+    'manual_path': sconf.theme.project,
+    'repo_name': sconf.theme.project,
+    'jira_project': sconf.theme.jira,
+    'google_analytics': sconf.theme.google_analytics,
+    'project': sconf.project,
+    'nav_excluded': sconf.theme.nav_excluded,
+    'version': version
+}
+
+html_sidebars = sconf.sidebars
+
