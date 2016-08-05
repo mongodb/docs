5321c3113cfacaf7cd3b01d189b6ba4b7fb8565a
Allison Moore <allison.moore@10gen.com>
2016-08-05 14:08:27 -0400

    changes theme to use meta-driver and support branches

--- conf.py
+++ conf.py
@@ -9,15 +9,12 @@ import sys
 import os.path
 import datetime
 
-project_root = os.path.join(os.path.abspath(os.path.dirname(__file__)))
-sys.path.append(project_root)
-
 from giza.config.runtime import RuntimeStateConfig
 from giza.config.helper import fetch_config, get_versions, get_manual_path
-from giza.config.project import get_current_path
+
+from sphinx.errors import SphinxError
 
 conf = fetch_config(RuntimeStateConfig())
-intersphinx_libs = conf.system.files.data.intersphinx
 sconf = conf.system.files.data.sphinx_local
 
 sys.path.append(os.path.join(conf.paths.projectroot, conf.paths.buildsystem, 'sphinxext'))
@@ -44,8 +41,8 @@ language = 'en'
 project = sconf.project
 copyright = u'2008-{0}'.format(datetime.date.today().year)
 
-version = '1.0'
-release = version
+version = conf.version.branch
+release = conf.version.release
 
 rst_epilog = '\n'.join([
     '.. |branch| replace:: ``{0}``'.format(conf.git.branches.current),
@@ -63,46 +60,32 @@ extlinks = {
     'php': ('http://php.net/%s', '')
 }
 
-# Example configuration for intersphinx: refer to the Python standard library.
+## add `extlinks` for each published version.
+for i in conf.git.branches.published:
+    extlinks[i] = ( ''.join([ conf.project.url, '/', i, '%s' ]), '' )
+
 intersphinx_mapping = {}
-for i in intersphinx_libs:
-    intersphinx_mapping[i.name] = (i.url, os.path.join(conf.paths.projectroot,
+
+intersphinx_mapping = {}
+for i in conf.system.files.data.intersphinx:
+    intersphinx_mapping[i.name] = ( i.url, os.path.join(conf.paths.projectroot,
                                                         conf.paths.output,
                                                         i.path))
 
+languages = []
+
 pygments_style = 'default'
 
-languages = [
-    ("ar", "Arabic"),
-    ("cn", "Chinese"),
-    ("cs", "Czech"),
-    ("de", "German"),
-    ("es", "Spanish"),
-    ("fr", "French"),
-    ("hu", "Hungarian"),
-    ("id", "Indonesian"),
-    ("it", "Italian"),
-    ("jp", "Japanese"),
-    ("ko", "Korean"),
-    ("lt", "Lithuanian"),
-    ("pl", "Polish"),
-    ("pt", "Portuguese"),
-    ("ro", "Romanian"),
-    ("ru", "Russian"),
-    ("tr", "Turkish"),
-    ("uk", "Ukrainian")
-]
 
 # -- Options for HTML output ---------------------------------------------------
 
 html_theme = sconf.theme.name
 html_theme_path = [ os.path.join(conf.paths.buildsystem, 'themes') ]
 html_title = conf.project.title
-htmlhelp_basename = 'MongoDBdoc'
+htmlhelp_basename = 'MongoDB'
 
 html_logo = ".static/logo-mongodb.png"
-html_static_path = ['source/.static']
-html_last_updated_fmt = '%b %d, %Y'
+html_static_path = sconf.paths.static
 
 html_copy_source = False
 html_use_smartypants = True
@@ -112,18 +95,22 @@ html_split_index = False
 html_show_sourcelink = False
 html_show_sphinx = True
 html_show_copyright = True
-
-manual_edition_path = '{0}/{1}/{2}.{3}'
+html_add_permalinks = None
 
 html_theme_options = {
     'branch': conf.git.branches.current,
+    'manual_path': get_manual_path(conf),
     'translations': languages,
     'language': language,
-    'manual_path': "php-library",
-    'repo_name': 'docs-php-library',
-    'jira_project': 'DOCS',
+    'repo_name': sconf.theme.repo,
+    'jira_project': sconf.theme.jira,
     'google_analytics': sconf.theme.google_analytics,
-    'project': sconf.project,
+    'project': sconf.theme.project,
+    'version': version,
+    'version_selector':  get_versions(conf),
+    'active_branches': conf.version.active,
+    'stable': conf.version.stable,
+    'sitename': sconf.theme.sitename,
     'nav_excluded': sconf.theme.nav_excluded,
 }
 
