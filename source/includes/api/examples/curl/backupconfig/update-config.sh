263baa18eca51261d7d13f56afaf5c7cf156a133
Tony Sansone <tony.sansone@mongodb.com>
2017-05-18 11:13:30 -0500

    Updated to remove extra include files.

--- source/includes/api/examples/curl/backupconfig/update-config.sh
+++
@@ -1,9 +0,0 @@
-curl --user "{username}:{apiKey}" --digest \
- --header "Accept: application/json" \
- --header "Content-Type: application/json" \
- --include --request PATCH "https://cloud.mongodb.com/api/public/v1.0/groups/{GROUP-ID}/backupConfigs/{CLUSTER-ID}" 
- --include --request PATCH "https://{opsManagerHost}:{port}/api/public/v1.0/groups/{GROUP-ID}/backupConfigs/{CLUSTER-ID}" \
- --data '
- {
-   "statusName": "STOPPED"
- }'
