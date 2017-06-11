ee2b86800698e91242ce7e69db87acac864bfad1
ᴋɪʀʙʏ ᴋᴏʜʟᴍᴏʀɢᴇɴ <kirby@kkohlmorgen.com>
2017-06-11 17:18:27 -0400

    Revert "DOCSP-802: Animate search bar"
    This reverts commit c8e5a1e72c7493b273a831b5c0f74f99dcd2a8d5.

--- themes/mongodb-tutorials/src/home.js
+++ themes/mongodb-tutorials/src/home.js
@@ -45,10 +45,7 @@ class App extends React.Component {
         var input = document.querySelector('.gsc-input input.gsc-input')
 
         if (input) {
-          input.style = ''
-          input.className = 'navbar-search'
-          document.querySelector('.navbar__right').appendChild(input)
-          input.setAttribute('placeholder', "Search Documentation")
+          input.setAttribute('placeholder', "Search Documentation");
           elementClass(input).add('navbar-search')
           window.clearInterval(pollInput);
         }
--- themes/mongodb-tutorials/static/js/home.js
+++ themes/mongodb-tutorials/static/js/home.js
@@ -138,9 +138,6 @@
 	          var input = document.querySelector('.gsc-input input.gsc-input');
 	
 	          if (input) {
-	            input.style = '';
-	            input.className = 'navbar-search';
-	            document.querySelector('.navbar__right').appendChild(input);
 	            input.setAttribute('placeholder', "Search Documentation");
 	            (0, _elementClass2.default)(input).add('navbar-search');
 	            window.clearInterval(pollInput);
