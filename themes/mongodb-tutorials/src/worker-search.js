990d99fa74a54be7ef8fe2ac4ed6b17ad9ce206f
Andrew Aldridge <i80and@foxquill.com>
2017-06-18 11:28:20 -0400

    Build worker-search in production mode

--- themes/mongodb-tutorials/src/worker-search.js
+++ themes/mongodb-tutorials/src/worker-search.js
@@ -1,15 +1,15 @@
 'use strict'
 import lunr from 'lunr'
 
-let searchIndex = null
-let slugMapping = []
+var searchIndex = null
+var slugMapping = []
 
 function load(url) {
-    let start
-    return fetch(url).then((response) => {
+    var start
+    return fetch(url).then(function(response) {
         start = performance.now()
         return response.json()
-    }).then((data) => {
+    }).then(function(data) {
         searchIndex = lunr.Index.load(data)
         slugMapping = data.slugs
         return (performance.now() - start) / 1000
@@ -18,7 +18,7 @@ function load(url) {
 
 function search(query) {
     const start = performance.now()
-    const results = searchIndex.search(query).map((row) => {
+    const results = searchIndex.search(query).map(function(row) {
         return slugMapping[row.ref]
     })
     // console.log((performance.now() - start) / 1000)
@@ -27,7 +27,7 @@ function search(query) {
 
 self.onmessage = function(ev) {
     if (ev.data.load !== undefined) {
-        load(ev.data.load).then((enlapsed) => {
+        load(ev.data.load).then(function(enlapsed) {
             self.postMessage({ 'loaded': enlapsed })
         })
     } else if (ev.data.search !== undefined) {
