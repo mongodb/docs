d9aab2c082aa5dcb1696ea3492c205af5203d7a2
Andrew Aldridge <i80and@foxquill.com>
2017-08-18 11:42:13 -0400

    Revise landing page to use Marian

--- Makefile
+++ Makefile
@@ -12,6 +12,7 @@ CSS_WARNINGS=regex-selectors,unqualified-attributes,text-indent
 help:
 	@echo 'Targets'
 	@echo '  help         - Show this help message'
+	@echo '  build        - Build HTML artifacts for upload'
 	@echo '  stage        - Host online for review'
 	@echo '  deploy       - Deploy into production'
 	@echo ''
@@ -19,7 +20,7 @@ help:
 	@echo '  ARGS         - Arguments to pass to mut-publish'
 
 stage: build
-	mut-publish build/ docs-mongodb-org-staging --prefix=${PREFIX} --stage ${ARGS}
+	mut-publish build/ docs-mongodb-org-staging --prefix=${PREFIX} --stage --verbose ${ARGS}
 	@echo "Hosted at ${URL}/${PREFIX}/${USER}/${GIT_BRANCH}/index.html"
 	@echo "Hosted at ${URL}/${PREFIX}/${USER}/${GIT_BRANCH}/cloud/index.html"
 	@echo "Hosted at ${URL}/${PREFIX}/${USER}/${GIT_BRANCH}/tools/index.html"
@@ -45,12 +46,12 @@ build: style.min.css
 	cp -r static/images static/fonts static/css static/js $@/cloud
 	cp -r static/images static/fonts static/css static/js $@/tools
 	@# Run the script to generate each landing page
-	python ./gen_landings.py $@
+	python3 ./gen_landings.py $@
 
 # Don't grab node_modules unless we have to
 style.min.css: src/sass/style.scss node_modules/.npm_pulled
 	./node_modules/.bin/node-sass src/sass/style.scss | ./node_modules/.bin/cleancss --skip-rebase --semantic-merging -o ./static/css/$@
 
 node_modules/.npm_pulled: package.json
-	npm update
+	npm install
 	touch node_modules/.npm_pulled
--- gen_landings.py
+++ gen_landings.py
@@ -1,48 +1,57 @@
 #!/usr/bin/env python3
+import collections
 import os.path
-
 from sys import argv
+
+Site = collections.namedtuple('Site', ('input_path', 'output_path', 'title', 'search_properties'))
 
 # Build directory name will be the name of Makefile recipe
 BUILD_DIR = argv[1]
 
-# Paths of source files
-HOME_INPUT_PATH = './src/html/home.html'
-CLOUD_INPUT_PATH = './src/html/cloud.html'
-TOOLS_INPUT_PATH = './src/html/tools.html'
+SITES = [
+    Site('./src/html/home.html', os.path.join(BUILD_DIR, 'index.html'), 'MongoDB Documentation', ''),
+    Site('./src/html/cloud.html',
+         os.path.join(BUILD_DIR, 'cloud', 'index.html'),
+         'Cloud Products — MongoDB Documentation',
+         'atlas-master,mms-onprem-current,mms-cloud-master,stitch-master'),
+    Site('./src/html/tools.html',
+         os.path.join(BUILD_DIR, 'tools', 'index.html'),
+         'MongoDB Tools — MongoDB Documentation',
+         'bi-connector-current,spark-connector-current,compass-master')
+]
 
-# Path of the output files
-HOME_OUTPUT_PATH = os.path.join(BUILD_DIR, 'index.html')
-CLOUD_OUTPUT_PATH = os.path.join(BUILD_DIR, 'cloud', 'index.html')
-TOOLS_OUTPUT_PATH = os.path.join(BUILD_DIR, 'tools', 'index.html')
 
-
-def build_individual(template, input_path, output_path):
+def build_individual(template: str, site: Site) -> None:
     """Build a landing page from a single source file
        pyfe.g., Cloud landing page, Tools..."""
     html = ''
 
     # Read the source file
-    with open(input_path, 'r') as in_file:
+    with open(site.input_path, 'r') as in_file:
         html = in_file.read()
 
+    title_pair = site.title.split('—', 1)
+    project_title = title_pair[0] if len(title_pair) > 1 else ''
+
     # Substitute the template placeholder w/ HTML
-    file_contents = template.replace('{0}', html)
+    file_contents = template.replace('{title}', site.title)
+    file_contents = file_contents.replace('{projectTitle}', project_title)
+    file_contents = file_contents.replace('{searchProperties}', site.search_properties)
+    file_contents = file_contents.replace('{body}', html)
 
     # Write the output file
-    with open(output_path, 'w') as out_file:
+    with open(site.output_path, 'w') as out_file:
         out_file.write(file_contents)
 
 
-def main():
+def main() -> None:
     # Read the template
     with open('./src/html/_layout.html', 'r') as in_file:
         template = in_file.read()
 
     # Build each landing page using the template
-    build_individual(template, HOME_INPUT_PATH, HOME_OUTPUT_PATH)
-    build_individual(template, CLOUD_INPUT_PATH, CLOUD_OUTPUT_PATH)
-    build_individual(template, TOOLS_INPUT_PATH, TOOLS_OUTPUT_PATH)
+    for site in SITES:
+        build_individual(template, site)
 
 if __name__ == '__main__':
     main()
