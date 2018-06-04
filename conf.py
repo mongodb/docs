0619a80c3684c63519105b8f8c6ff379f29c742e
Andrew Aldridge <i80and@foxquill.com>
2018-06-04 17:16:44 -0400

    DOCSP-2558: Document the icon role

--- conf.py
+++ conf.py
@@ -39,7 +39,8 @@ extensions = [
     'directives',
     'intermanual',
     'testcode',
-    'tabs'
+    'tabs',
+    'icon'
 ]
 
 locale_dirs = [ os.path.join(conf.paths.projectroot, conf.paths.locale) ]
