6c4f6d2988185184cd8c7fd2d8d3b3584aad2ccd
jeff-allen-mongo <jeffrey.allen@10gen.com>
2019-01-17 14:53:01 -0500

    (DOCSP-4200): Updating Charts landing page for Atlas pricing
    Updates per Kay's feedback
    
    Updates per Kay's feedback pt 2
    
    Updates per Tom's feedback

--- conf.py
+++ conf.py
@@ -62,7 +62,8 @@ rst_epilog = [
     '.. |checkmark| unicode:: U+2713',
     '.. |tls-ssl| replace:: :abbr:`TLS (Transport Layer Security)`/:abbr:`SSL (Secure Sockets Layer)`',
     '.. |service| replace:: Atlas',
-    '.. |enc-channel| replace:: :ref:`encoding channel <encoding-channels>`'
+    '.. |enc-channel| replace:: :ref:`encoding channel <encoding-channels>`',
+    '.. |aws| replace:: :abbr:`AWS (Amazon Web Services)`'
 ]
 
 rst_epilog.extend(get_replacements(conf))
