2435b5445ae04062f3496903246714e7477b9980
Steve Renaker <steve.renaker@10gen.com>
2019-01-29 14:37:12 -0800

    DOCS-12396: HTTP Request Example for Create One Restore Job for One Cluster errors (#2103)

--- source/includes/api/requests/create-one-restore-job-http-ops.sh
+++ source/includes/api/requests/create-one-restore-job-http-ops.sh
@@ -2,12 +2,12 @@ curl --user "{USERNAME}:{APIKEY}" --digest \
   --header "Accept: application/json" \
   --header "Content-Type: application/json" \
   --include \
-  --request POST "https://{OPSMANAGER-HOST}:{PORT}/api/public/v1.0/groups/{GROUP-ID}/clusters/{CLUSTER-NAME}/restoreJobs?pretty=true" \
+  --request POST "https://{OPSMANAGER-HOST}:{PORT}/api/public/v1.0/groups/{GROUP-ID}/clusters/{CLUSTER-ID}/restoreJobs?pretty=true" \
   --data '
    {
      "delivery" : {
        "expirationHours" : "1",
-       "maxDownloads" : "1"
+       "maxDownloads" : "1",
        "methodName" : "HTTP"
      },
      "snapshotId" : "{SNAPSHOT-ID}"
