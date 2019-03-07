cfb1097a8ba900b375cdab77d12bed60dedbf5cf
Anthony Sansone <tony.sansone@mongodb.com>
2019-03-06 19:20:34 -0600

    Initial add of base files.

---
+++ conf.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+#
+# MongoDB documentation build configuration file, created by
+# sphinx-quickstart on Mon Oct  3 09:58:40 2011.
+#
+# This file is execfile()d with the current directory set to its containing dir.
+
+import base64
+import sys
+import os.path
+import datetime
+
+project_root = os.path.join(os.path.abspath(os.path.dirname(__file__)))
+sys.path.append(project_root)
+
+from giza.config.runtime import RuntimeStateConfig
+from giza.config.helper import fetch_config, get_versions, get_manual_path
+
+conf = fetch_config(RuntimeStateConfig())
+intersphinx_libs = conf.system.files.data.intersphinx
+pdfs = conf.system.files.data.pdfs
+sconf = conf.system.files.data.sphinx_local
+
+sys.path.append(os.path.join(conf.paths.projectroot, conf.paths.buildsystem, 'sphinxext'))
+
+# -- General configuration ----------------------------------------------------
+
+needs_sphinx = '1.0'
+
+extensions = [
+    'sphinx.ext.extlinks',
+    'sphinx.ext.todo',
+    'mongodb',
+    'directives',
+    'intermanual',
+    'fasthtml',
+    'fixed_only',
+    'tabs',
+    'icon',
+    'source_constants'
+]
+
+templates_path = ['.templates']
+exclude_patterns = []
+
+source_suffix = '.txt'
+
+master_doc = sconf.master_doc
+language = 'en'
+project = sconf.project
+copyright = u'2008-{0}'.format(datetime.date.today().year)
+
+version = conf.version.branch
+release = conf.version.release
+
+rst_epilog = '\n'.join([
+    '.. |branch| replace:: ``{0}``'.format(conf.git.branches.current),
+    '.. |copy| unicode:: U+000A9',
+    '.. |year| replace:: {0}'.format(datetime.date.today().year),
+    '.. |ent-build| replace:: MongoDB Enterprise',
+    '.. |hardlink| replace:: https://docs.mongodb.com/kubernetes-operator/',
+    '.. |k8s-op| replace:: MongoDB Enterprise Kubernetes Operator',
+    '.. |k8s-op-short| replace:: Kubernetes Operator',
+])
+
+extlinks = {
+    'issue': ('https://jira.mongodb.org/browse/%s', '' ),
+    'manual': ('http://docs.mongodb.com/manual%s', ''),
+}
+
+intersphinx_mapping = {}
+for i in intersphinx_libs:
+    intersphinx_mapping[i.name] = (i.url, os.path.join(conf.paths.projectroot,
+                                                       conf.paths.output, i.path))
+
+languages = [
+    ("ar", "Arabic"),
+    ("cn", "Chinese"),
+    ("cs", "Czech"),
+    ("de", "German"),
+    ("es", "Spanish"),
+    ("fr", "French"),
+    ("hu", "Hungarian"),
+    ("id", "Indonesian"),
+    ("it", "Italian"),
+    ("jp", "Japanese"),
+    ("ko", "Korean"),
+    ("lt", "Lithuanian"),
+    ("pl", "Polish"),
+    ("pt", "Portuguese"),
+    ("ro", "Romanian"),
+    ("ru", "Russian"),
+    ("tr", "Turkish"),
+    ("uk", "Ukrainian")
+]
+
+# -- Options for HTML output ---------------------------------------------------
+
+html_theme = sconf.theme.name
+html_theme_path = [ os.path.join(conf.paths.buildsystem, 'themes') ]
+html_title = conf.project.title
+htmlhelp_basename = 'MongoDBdoc'
+
+html_logo = ".static/logo-mongodb.png"
+html_static_path = ['source/.static']
+html_last_updated_fmt = '%b %d, %Y'
+
+html_copy_source = False
+html_domain_indices = True
+html_use_index = True
+html_split_index = False
+html_show_sourcelink = False
+html_show_sphinx = True
+html_show_copyright = True
+
+manual_edition_path = '{0}/{1}/{2}.{3}'
+
+html_theme_options = {
+    'branch': conf.git.branches.current,
+    'translations': languages,
+    'language': language,
+    'manual_path': "bi-connector",
+    'repo_name': 'docs-bi-connector',
+    'jira_project': 'DOCSP',
+    'google_analytics': sconf.theme.google_analytics,
+    'project': sconf.project,
+    'epubpath': manual_edition_path.format(conf.project.url,
+                                           conf.project.basepath,
+                                           'mongodb-bi-connector', 'epub'),
+    'nav_excluded': sconf.theme.nav_excluded,
+    'version_selector': get_versions(conf),
+    'is_upcoming': False,
+}
+
+html_sidebars = sconf.sidebars
+
+
+# -- Options for Epub output ---------------------------------------------------
+
+# Bibliographic Dublin Core info.
+epub_title = conf.project.title
+epub_author = u'MongoDB Documentation Project'
+epub_publisher = u'MongoDB, Inc.'
+epub_copyright = copyright
+epub_theme = 'epub_mongodb'
+epub_tocdup = True
+epub_tocdepth = 3
+epub_language = 'en'
+epub_scheme = 'url'
+epub_identifier = 'http://docs.mongodb.org/kubernetes-operator/'
+epub_exclude_files = []
+epub_pre_files = []
+epub_post_files = []
