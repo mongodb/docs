bcef6f276b48f5afd52b7dcb35af88d55a6eeb77
nate contino <nathan.contino@mongodb.com>
2019-12-11 15:00:22 -0500

    DOCSP-7606: delete a single document usage example (#9)

---
+++ source/code-snippets/usage-examples/deleteOne.js
@@ -0,0 +1,34 @@
+// ignored first line
+const { MongoClient } = require("mongodb");
+
+const uri =
+  "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("sample_mflix");
+    const collection = database.collection("movies");
+
+    // Query for a movie that has a title of type string
+    const query = { title: { $type: "string" } };
+
+    collection.deleteOne(query, options, function(error, result) {
+      if (error) {
+        console.log("Error: " + error.errmsg);
+      } else {
+        if (result.deletedCount == 1) {
+          console.dir("Successfully deleted one document.");
+        } else {
+          console.log("No documents matched the query.");
+        }
+      }
+    });
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
---
+++ source/code-snippets/usage-examples/findOneAndDelete.js
@@ -0,0 +1,42 @@
+// ignored first line
+const { MongoClient } = require("mongodb");
+
+const uri =
+  "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("sample_mflix");
+    const collection = database.collection("movies");
+
+    // Query for a movie that has the title of type string
+    const query = { title: { $type: "string" } };
+
+    const options = {
+      // sort matched documents in ascending order by rating
+      sort: { rating: 1 },
+      // Include only the `title` and `fullplot` fields in the returned document
+      projection: { _id: 0, title: 1, fullplot: 1 },
+    };
+
+    collection.findOneAndDelete(query, options, function(error, result) {
+      if (error) {
+        console.log("Error: " + error.errmsg);
+      } else {
+        if (result.lastErrorObject.n == 1) {
+          console.log("Deleted document:");
+          console.dir(result.value);
+        } else {
+          console.log("No documents matched the query.");
+        }
+      }
+    });
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
