b3a249d94fe5e809ffe62047aa54840a2ef1372f
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2020-01-14 15:52:36 -0500

    (DOCSP-8054): run a command usage example (#25)
    * update verbiage
    
    * update TOC
    
    * update TOC
    
    * added command() method
    
    * added missing link faq + more  description
    
    * Fixed note syntax
    
    * updated crud link
    
    * test push
    
    * (DOCSP-8054): Run a Command Usage Command init
    
    * added code snippet for db.command
    
    * updated verbiage
    
    * updated verbiage per pr comments
    
    * updated txt
    
    * updated txt

---
+++ source/code-snippets/usage-examples/command.js
@@ -0,0 +1,24 @@
+// ignored first line
+const { MongoClient } = require("mongodb");
+
+// Replace the following with your MongoDB deployment's connection string.
+const uri =
+  "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db("sample_mflix");
+    // find the storage statistics for the "sample_mflix" database using the 'dbStats' command
+    const result = await db.command({
+      dbStats: 1,
+    });
+    console.log(result);
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
