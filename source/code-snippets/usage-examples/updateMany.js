bbc94d0774bc43e57cb8ca5c92b2acd6a10ebc86
Mohammad Hunan Chughtai <mohammad.hunan@10gen.com>
2020-01-16 14:08:48 -0500

    (DOCSP-7604): update multiple docs (#22)
    * (DOCSP-7604): iupdate many doc
    
    * updated link to update op
    
    * updated link to update op
    
    * updated link to update op
    
    * updated link to update op
    
    * added literal includes for code
    
    * updated verbiage
    
    * updated link
    
    * updated verbiage and code
    
    * updated code with additional options
    
    * Removed unneeded comment
    
    * updated verbiage
    
    * updated code comments
    
    * fixed grammar
    
    * removed upsert opt
    
    * removed upsert opt
    
    * removed upsert opt
    
    * removed upsert opt
    
    * fixed grammar
    
    * Removed unneeded line
    
    * removed additional opts

---
+++ source/code-snippets/usage-examples/updateMany.js
@@ -0,0 +1,32 @@
+// ignored first line
+const { MongoClient } = require("mongodb");
+
+// Replace the following with your MongoDB deployment's connection string.
+const uri =
+  "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db("sample_mflix");
+    const collection = database.collection("movies");
+
+    // create a filter to update all movies with a 'G' rating
+    const filter = { rated: "G" };
+
+    // increment every document matching the filter with 2 more comments
+    const updateDoc = {
+      $inc: {
+        num_mflix_comments: 2,
+      },
+    };
+    const result = await collection.updateMany(filter, updateDoc);
+    console.log(result);
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
