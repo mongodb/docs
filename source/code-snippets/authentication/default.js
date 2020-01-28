26fdc8920cf558a4f8df8d005c93976d7d59ef75
Chris Cho <chris.cho@mongodb.com>
2020-01-28 16:35:36 -0500

    DOCSP-8454: Migrate TLS Options to docs-node (#43)
    * DOCSP-8454: add table for TLS options, add connection page

---
+++ source/code-snippets/authentication/default.js
@@ -0,0 +1,33 @@
+// ignored first line
+const { MongoClient } = require("mongodb");
+
+// specify the placeholder values for your environment in the following lines
+const username = encodeURIComponent("<username>");
+const password = encodeURIComponent("<password>");
+const clusterUrl = "<MongoDB cluster url>";
+
+const authMechanism = "DEFAULT";
+
+// Replace the following with your MongoDB deployment's connection
+// string.
+const uri = 
+  `mongodb+srv://${username}:${password}@${clusterUrl}/?authMechanism=${authMechanism}`;
+
+// Create a new MongoClient
+const client = new MongoClient(uri);
+
+// Function to connect to the server
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
---
+++ source/code-snippets/connection/srv.js
@@ -0,0 +1,23 @@
+const { MongoClient } = require("mongodb");
+
+// Connection URI
+const uri =
+  "mongodb+srv://sample-hostname:27017/?poolSize=20&useUnifiedTopology=true";
+
+// Create a new MongoClient
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    // Connect the client to the server
+    await client.connect();
+
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
