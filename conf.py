c6746d427508a997d605dc56afb8733f3679533d
John Williams <john.williams@10gen.com>
2020-02-18 14:12:30 -0500

    (DOCSP-9016): update latest-mongodb-eol constant

--- conf.py
+++ conf.py
@@ -173,7 +173,7 @@ source_constants = {
     'data-lake-stores': 'data stores',
     'data-lake' : 'Atlas Data Lake',
     'data-lake-short' : 'Data Lake',
-    'latest-mongodb-eol': '3.2',
+    'latest-mongodb-eol': '3.4',
     'service-api-v1-base-uri' : 'https://cloud.mongodb.com/api/atlas/v1.0'
 }
 
