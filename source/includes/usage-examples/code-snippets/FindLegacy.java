f59239574f1566ea9ef884ab1c9eb00912fe02ae
Chris Cho <chris.cho@10gen.com>
2020-04-10 18:01:32 -0400

    fix examples

--- source/includes/usage-examples/code-snippets/FindLegacy.java
+++ source/includes/usage-examples/code-snippets/FindLegacy.java
@@ -15,33 +15,32 @@ import com.mongodb.client.model.Sorts;
 
 public class Find
 {
-  public static void main( String[] args )
-  {
-    // Replace the uri string with your MongoDB deployment's connection string
-     MongoClientURI uri = new MongoClientURI("mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority");
+    public static void main( String[] args )
+    {
+        // Replace the uri string with your MongoDB deployment's connection string
+        MongoClientURI uri = new MongoClientURI("mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority");
 
-     MongoClient mongoClient = new MongoClient(uri);
+        MongoClient mongoClient = new MongoClient(uri);
 
-     MongoDatabase database = mongoClient.getDatabase("sample_mflix");
-     MongoCollection<Document> collection = database.getCollection("movies");
+        MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+        MongoCollection<Document> collection = database.getCollection("movies");
 
-     Bson projectionFields = Projections.fields(
-         Projections.include("title", "imdb"),
-         Projections.excludeId());
+        Bson projectionFields = Projections.fields(
+                Projections.include("title", "imdb"),
+                Projections.excludeId());
 
-     MongoCursor<Document> cursor = collection
-         .find(lt("runtime", 15))
-         .projection(projectionFields)
-         .sort(Sorts.descending("title")).iterator();
+        MongoCursor<Document> cursor = collection.find(lt("runtime", 15))
+                .projection(projectionFields)
+                .sort(Sorts.descending("title")).iterator();
 
-     try {
-         while(cursor.hasNext()) {
-             System.out.println(cursor.next().toJson());
-         }
-     } finally {
-         cursor.close();
-     }
+        try {
+            while(cursor.hasNext()) {
+                System.out.println(cursor.next().toJson());
+            }
+        } finally {
+            cursor.close();
+        }
 
-    mongoClient.close();
-  }
+        mongoClient.close();
+    }
 }
