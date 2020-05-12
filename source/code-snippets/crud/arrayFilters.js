f782001d1d865a42d1b3e63d83cc879cf2b462ff
Chris Cho <chris.cho@mongodb.com>
2020-05-12 17:56:00 -0400

    DOCSP-10259: address feedback on update arrays (#97)
    * DOCSP-10259: Address feedback on Update Arrays

--- source/code-snippets/crud/arrayFilters.js
+++ source/code-snippets/crud/arrayFilters.js
@@ -7,7 +7,75 @@ const uri =
   "mongodb+srv://<user>:<password>@<cluster-url>?w=majority";
 const client = new MongoClient(uri);
 
-async function run() {
+
+async function loadData() {
+  try {
+    await client.connect();
+
+    const database = client.db("test");
+    const collection = database.collection("pizza");
+
+    await collection.drop();
+
+    await collection.insertMany([
+      {
+        name: "Steve Lobsters",
+        address: "731 Yexington Avenue",
+        items: [
+          {
+            type: "pizza",
+            size: "large",
+            toppings: ["pepperoni"],
+          },
+          {
+            type: "pizza",
+            size: "medium",
+            toppings: ["mushrooms", "sausage", "green peppers"],
+            comment: "Extra green peppers please!",
+          },
+          {
+            type: "pizza",
+            size: "large",
+            toppings: ["pineapple, ham"],
+            comment: "red pepper flakes on top",
+          },
+          {
+            type: "calzone",
+            fillings: ["canadian bacon", "sausage", "onion"],
+          },
+          {
+            type: "beverage",
+            name: "Diet Pepsi",
+            size: "16oz",
+          },
+        ],
+      },
+      {
+        name: "Popeye",
+        address:"1 Sweetwater",
+        items: [
+          {
+            type: "pizza",
+            size: "large",
+            toppings: ["garlic", "spinach"]
+          },
+          {
+            type: "calzone",
+            toppings: ["ham"],
+          },
+        ]
+      }
+    );
+
+    console.log(JSON.stringify(await (await collection.find()).toArray()));
+  } finally {
+    await client.close();
+  }
+}
+
+
+async function runAllArrayElements() {
+
   try {
     await client.connect();
 
@@ -16,7 +84,85 @@ async function run() {
 
     console.log(JSON.stringify(await (await collection.find()).toArray()));
 
-    // start arrayFilters example
+    // start allArrayElement example
+    const query = { "name": "Popeye" };
+    const updateDocument = {
+      $push: { "items.$[].toppings": "fresh mozzarella" }
+    };
+    const result = await collection.updateOne(query, updateDocument);
+    // end allArrayElement example
+    console.log(result.modifiedCount);
+    console.log(JSON.stringify(await (await collection.find()).toArray()));
+  } finally {
+    await client.close();
+  }
+}
+async function runFirstArrayElement() {
+
+  try {
+    await client.connect();
+
+    const database = client.db("test");
+    const collection = database.collection("pizza");
+
+    console.log(JSON.stringify(await (await collection.find()).toArray()));
+
+    // start firstArrayElement example
+    const query = { name: "Steve Lobsters", "items.type": "pizza" };
+    const updateDocument = {
+      $set: { "items.$.size": "extra large" }
+    };
+    const result = await collection.updateOne(query, updateDocument);
+    // end firstArrayElement example
+    console.log(result.modifiedCount);
+    console.log(JSON.stringify(await (await collection.find()).toArray()));
+  } finally {
+    await client.close();
+  }
+}
+
+
+async function arrayFiltersOne() {
+  try {
+    await client.connect();
+
+    const database = client.db("test");
+    const collection = database.collection("pizza");
+
+    console.log(JSON.stringify(await (await collection.find()).toArray()));
+
+    // start arrayFiltersOne example
+    const query = { name: "Steve Lobsters" };
+    const updateDocument = {
+      $push: { "items.$[orderItem].toppings": "garlic" }
+    };
+    const options = {
+      arrayFilters: [{
+        "orderItem.type": "pizza",
+        "orderItem.size": "large",
+      }]
+    };
+
+    const result = await collection.updateMany(query, updateDocument, options);
+    // end arrayFiltersOne example
+
+    console.log(result.modifiedCount);
+    console.log(JSON.stringify(await (await collection.find()).toArray()));
+  } finally {
+    await client.close();
+  }
+}
+
+async function arrayFiltersTwo() {
+  try {
+    await client.connect();
+
+    const database = client.db("test");
+    const collection = database.collection("pizza");
+
+    console.log(JSON.stringify(await (await collection.find()).toArray()));
+
+    // start arrayFiltersTwo example
     const query = { name: "Steve Lobsters" };
     const updateDocument = {
       $push: { "items.$[item].toppings": "salami" },
@@ -30,7 +176,7 @@ async function run() {
       ],
     };
     const result = await collection.updateOne(query, updateDocument, options);
-    // end arrayFilters example
+    // end arrayFiltersTwo example
     console.log(result.modifiedCount);
 
     collection.insertOne({
@@ -71,4 +217,4 @@ async function run() {
     await client.close();
   }
 }
-run().catch(console.dir);
+//run().catch(console.dir);
