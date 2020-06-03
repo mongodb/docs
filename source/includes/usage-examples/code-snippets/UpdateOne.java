3d1bae6ceec2d138b96bcd52152ef4a2967bc025
Chris Cho <chris.cho@10gen.com>
2020-06-03 18:38:17 -0400

    updateOne usage example: add example and fill in text

--- source/includes/usage-examples/code-snippets/UpdateOne.java
+++ source/includes/usage-examples/code-snippets/UpdateOne.java
@@ -1,1 +1,46 @@
 // ignored first line
+package usage.examples;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import com.mongodb.MongoException;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.UpdateOptions;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.UpdateResult;
+
+public class UpdateOne {
+
+    public static void main(String[] args) {
+        // Replace the uri string with your MongoDB deployment's connection string
+        String uri = "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
+        MongoClient mongoClient = MongoClients.create(uri);
+
+        MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+        MongoCollection<Document> collection = database.getCollection("movies");
+
+        try {
+            Document query = new Document().append("title",  "Cool Runnings");
+
+            Bson updates = Updates.combine(
+                    Updates.set("runtime", 99),
+                    Updates.addToSet("genres", "Sports"),
+                    Updates.currentTimestamp("lastUpdated"));
+
+            UpdateOptions options = new UpdateOptions().upsert(true);
+
+            UpdateResult result = collection.updateOne(query, updates, options);
+
+            System.out.println("Modified document count: " + result.getModifiedCount());
+
+        } catch (MongoException me) {
+            System.err.println("Unable to update due to an error: " + me);
+        }
+        mongoClient.close();
+    }
+}
+
