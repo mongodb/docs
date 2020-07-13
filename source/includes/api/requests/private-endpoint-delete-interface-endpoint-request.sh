549862b2a957cd898131d803521e5f6a6a60448c
John Williams <john.williams@10gen.com>
2020-07-13 14:30:09 -0400

    (DOCS-13761): Typo in Docs for Deleting Interface Endpoints in AWS PrivateLink

--- source/includes/api/requests/private-endpoint-delete-interface-endpoint-request.sh
+++ source/includes/api/requests/private-endpoint-delete-interface-endpoint-request.sh
@@ -1,3 +1,3 @@
 curl --user "{PUBLIC-KEY}:{PRIVATE-KEY}" --digest \
   --header "Accept: application/json" \
-  --request DELETE "https://cloud.mongodb.com/api/atlas/v1.0/groups/{GROUP-ID}/privateEndpoint/{privateLinkId}/interfaceEndpoint/{interfaceEndpointId}
+  --request DELETE "https://cloud.mongodb.com/api/atlas/v1.0/groups/{GROUP-ID}/privateEndpoint/{privateLinkId}/interfaceEndpoints/{interfaceEndpointId}
