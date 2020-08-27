c15cc1459e32d99b02a5048b9ed455a08a4f457d
jwilliams-mongo <55147273+jwilliams-mongo@users.noreply.github.com>
2020-08-27 09:47:35 -0400

    (DOCSP-11953): mongocli 1.4.1 release notes (#201)
    * (DOCSP-11953): mongocli 1.4.1 release notes
    
    * (DOCSP-11953): update version in conf.py

--- conf.py
+++ conf.py
@@ -61,7 +61,7 @@ source_constants = {
     'version': version,
     'mcli': 'MongoDB CLI',
     'mcli-long': 'MongoDB Command Line Interface (``mongocli``)',
-    'mcli-version': '1.4.0',
+    'mcli-version': '1.4.1',
     'mdbVersion': '4.2',
     'mdbagent': 'MongoDB Agent',
     'aagent': 'Automation Agent',
