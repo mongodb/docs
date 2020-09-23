c3db34a09d1bd89a1f18f25c23949fbc49cf25d7
madelinezec <mez2113@columbia.edu>
2020-09-23 10:02:20 -0400

    Dop 1487 (#56)
    * test
    
    * add fake readme
    
    * test
    
    * revert makefile
    
    * remove readme

--- gen_landings.py
+++ gen_landings.py
@@ -17,10 +17,6 @@ SITES = [
          os.path.join(BUILD_DIR, 'tools', 'index.html'),
          'MongoDB Tools — MongoDB Documentation',
          'bi-connector-current,spark-connector-current,compass-master'),
-    Site('./src/html/announcements/login-and-access-changes.html',
-         os.path.join(BUILD_DIR, 'announcements/login-and-access-changes', 'index.html'),
-         'MongoDB Announcements — MongoDB Documentation',
-         '')
 ]
 
 
