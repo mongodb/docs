004c31889a52068588276d2eb19d5e9db8dfe0ce
John Williams <john.williams@10gen.com>
2020-11-23 09:37:10 -0600

    (DOCSP-13303): fix typo

--- source/includes/api/requests/private-endpoint-update-regional-mode.sh
+++ source/includes/api/requests/private-endpoint-update-regional-mode.sh
@@ -1,7 +1,7 @@
 curl --user "{PUBLIC-KEY}:{PRIVATE-KEY}" --digest \
      --header "Content-Type: application/json" \
      --include \
-     --request PATCH "https://cloud.mongodb.com/api/atlas/v1.0/groups/{GROUP-ID}/privatEndpoint/regionMode" \
+     --request PATCH "https://cloud.mongodb.com/api/atlas/v1.0/groups/{GROUP-ID}/privatEndpoint/regionalMode" \
      --data '
        {
          "enabled" : true
