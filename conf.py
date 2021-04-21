8653ae01641daf669c20c975b21c30d2bbcd7971
andf-mongodb <55111344+andf-mongodb@users.noreply.github.com>
2021-04-21 12:51:58 -0400

    DOCSP-15685 fix extra slash in slug

--- conf.py
+++ conf.py
@@ -113,14 +113,14 @@ extlinks = {
     'java-async-docs': ('https://mongodb.github.io/mongo-java-driver-reactivestreams/1.6/%s', ''),
     'java-async-api': ('https://mongodb.github.io/mongo-java-driver-reactivestreams/1.6/javadoc/%s', ''),
     'go-api': ('https://godoc.org/github.com/mongodb/mongo-go-driver/%s', ''),
-    'v4.0': ('https://docs.mongodb.com/v4.0/%s', ''),
-    'v3.6': ('https://docs.mongodb.com/v3.6/%s', ''),
-    'v3.4': ('https://docs.mongodb.com/v3.4/%s', ''),
-    'v3.2': ('https://docs.mongodb.com/v3.2/%s', ''),
-    'v3.0': ('https://docs.mongodb.com/v3.0/%s', ''),
-    'v2.6': ('https://docs.mongodb.com/v2.6/%s', ''),
-    'v2.4': ('https://docs.mongodb.com/v2.4/%s', ''),
-    'v2.2': ('https://docs.mongodb.com/v2.2/%s', '')
+    'v4.0': ('https://docs.mongodb.com/v4.0%s', ''),
+    'v3.6': ('https://docs.mongodb.com/v3.6%s', ''),
+    'v3.4': ('https://docs.mongodb.com/v3.4%s', ''),
+    'v3.2': ('https://docs.mongodb.com/v3.2%s', ''),
+    'v3.0': ('https://docs.mongodb.com/v3.0%s', ''),
+    'v2.6': ('https://docs.mongodb.com/v2.6%s', ''),
+    'v2.4': ('https://docs.mongodb.com/v2.4%s', ''),
+    'v2.2': ('https://docs.mongodb.com/v2.2%s', '')
 }
 
 ## add `extlinks` for each published version.
