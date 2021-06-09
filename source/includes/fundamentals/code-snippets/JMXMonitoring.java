c7487d0b22fe262f3cf742e73eef92c056b3d82e
Aleksander Binion <77393289+biniona-mongodb@users.noreply.github.com>
2021-06-09 09:09:15 -0400

    Docsp 9573 monitoring (#88)
    Co-authored-by: kyuan-mongodb <78768401+kyuan-mongodb@users.noreply.github.com>

---
+++ source/includes/fundamentals/code-snippets/JMXMonitoring.java
@@ -0,0 +1,29 @@
+package com.mycompany.app;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.*;
+import com.mongodb.management.JMXConnectionPoolListener;
+
+public class JMXMonitoring {
+
+    private static final ConnectionString URI = new ConnectionString(System.getenv("DRIVER_URL"));
+
+    public static void main(String[] args) throws InterruptedException {
+        // start jmx-example
+        JMXConnectionPoolListener connectionPoolListener = new JMXConnectionPoolListener();
+        MongoClientSettings settings =
+                MongoClientSettings.builder()
+                        .applyConnectionString(URI)
+                        .applyToConnectionPoolSettings(builder -> builder.addConnectionPoolListener(connectionPoolListener))
+                        .build();
+        MongoClient mongoClient = MongoClients.create(settings);
+        try {
+            System.out.println("Navigate to JConcole to see your connection pools...");
+            Thread.sleep(Long.MAX_VALUE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        // end jmx-example
+    }
+}
