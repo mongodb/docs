26f35381e7b25a9deb5bf8c842bfa6bbb8385dd0
John Williams <55147273+jwilliams-mongo@users.noreply.github.com>
2021-06-24 16:21:11 -0500

    (DOCSP-16315): atlas search API tutorial (#2388)
    * (DOCSP-16315): atlas search terminal tutorials
    
    * (DOCSP-16315): copy review feedback
    
    * (DOCSP-16315): copy review feedback pt 2
    
    * (DOCSP-16315): remove mongocli tutorial
    
    * (DOCSP-16315): remove mongocli example command files
    
    * (DOCSP-16315): tech review feedback
    
    * (DOCSP-16315): tech review feedback pt 2
    
    * (DOCSP-16315): tech review feedback pt 3
    
    * (DOCSP-16315): tech review feedback pt 4

---
+++ source/includes/fts-tutorial/api-create-dynamic-mapping.sh
@@ -0,0 +1,17 @@
+PUBLIC_KEY=MY_PUBLIC_KEY # replace replace with your public key
+PRIVATE_KEY=MY_PRIVATE_KEY # replace with your private key
+GROUP_ID=YOUR_GROUP_ID # replace with your project ID
+CLUSTER_NAME=YOUR_CLUSTER_NAME # replace with your cluster's name
+
+curl --user "$PUBLIC_KEY:$PRIVATE_KEY" --digest \
+     --header "Content-Type: application/json" \
+     --include \
+     --request POST "https://cloud.mongodb.com/api/atlas/v1.0/groups/$GROUP_ID/clusters/$CLUSTER_NAME/fts/indexes?pretty=true" \
+     --data '{
+         "collectionName": "movies",
+         "database": "sample_mflix",
+         "mappings": {
+             "dynamic": true
+         },
+         "name": "default"
+       }'
---
+++ source/includes/fts-tutorial/api-create-static-mapping.sh
@@ -0,0 +1,37 @@
+PUBLIC_KEY=MY_PUBLIC_KEY # replace replace with your public key
+PRIVATE_KEY=MY_PRIVATE_KEY # replace with your private key
+GROUP_ID=YOUR_GROUP_ID # replace with your project ID
+CLUSTER_NAME=YOUR_CLUSTER_NAME # replace with your cluster's name
+
+curl --user "$PUBLIC_KEY:$PRIVATE_KEY" --digest \
+     --header "Content-Type: application/json" \
+     --include \
+     --request POST "https://cloud.mongodb.com/api/atlas/v1.0/groups/$GROUP_ID/clusters/$CLUSTER_NAME/fts/indexes?pretty=true" \
+     --data '{
+         "collectionName": "movies",
+         "database": "sample_mflix",
+         "mappings": {
+           "dynamic": false,
+           "fields": {
+             "title": {
+               "type": "string",
+               "analyzer": "lucene.standard",
+               "multi": {
+                 "keywordAnalyzer": {
+                   "type": "string",
+                   "analyzer": "lucene.keyword"
+                 }
+               }
+             },
+             "genres": {
+               "type": "string",
+               "analyzer": "lucene.standard"
+             },
+             "plot": {
+               "type": "string",
+               "analyzer": "lucene.standard"
+             }
+           }
+         },
+         "name": "default"
+       }'
