b045145331b4cdb1bbad3d9a877d5fab1c906852
Nathan Leniz <nathan.leniz@mongodb.com>
2021-07-06 10:49:02 -0700

    nits/suggestions

--- source/code-snippets/versioned-api/options.js
+++ source/code-snippets/versioned-api/options.js
@@ -2,6 +2,13 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const uri = "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { serverApi: { strict: true, deprecationErrors: true, version: ServerApiVersion.v1 }});
+const client = new MongoClient(uri,
+    {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
 // end serverApiVersion
 
