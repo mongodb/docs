64cef5fefaee8fca60ccb5af8dce630d36677b50
Chris Cho <chris.cho@mongodb.com>
2021-07-12 14:48:32 -0400

    DOCSP-17443: versioned api client instantiation snippets - Part 2 (#743)
    * DOCSP-17443: versioned api client instantiation snippets for PyMongo, Scala, and Reactive Streams

---
+++ source/includes/versioned-api-snippets/java-rs-client.java
@@ -0,0 +1,22 @@
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerApi;
+import com.mongodb.ServerApiVersion;
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+
+// ...
+
+// Replace <connection string> with your MongoDB deployment's connection string.
+ConnectionString connString = new ConnectionString("<connection string>");
+
+// Set the version of the Versioned API on the client.
+ServerApi serverApi = ServerApi.builder()
+        .version(ServerApiVersion.V1)
+         .build();
+
+MongoClientSettings settings = MongoClientSettings.builder()
+         .applyConnectionString(connString)
+         .serverApi(serverApi)
+         .build();
+MongoClient mongoClient = MongoClients.create(settings);
---
+++ source/includes/versioned-api-snippets/scala-client.scala
@@ -0,0 +1,14 @@
+import com.mongodb.{ServerApi, ServerApiVersion}
+import org.mongodb.scala._
+
+// ...
+
+// Replace <connection string> with your MongoDB deployment's connection string.
+val uri = "<connection string>"
+
+// Set the version of the Versioned API on the client.
+val mongoClientSettings = MongoClientSettings.builder().applyConnectionString(ConnectionString(uri))
+    .serverApi(ServerApi.builder().version(ServerApiVersion.V1).build())
+    .build()
+
+val mongoClient = MongoClient(mongoClientSettings)
