9f8387db6a6486f442165d64547f0756d9e8c590
Aleksander Binion <77393289+biniona-mongodb@users.noreply.github.com>
2021-08-06 13:38:43 -0400

    Docsp 17936 insert one type script example (#225)
    Co-authored-by: Nathan <nathan.leniz@mongodb.com>

--- source/code-snippets/usage-examples/insertOne.ts
+++ source/code-snippets/usage-examples/insertOne.ts
@@ -1,4 +1,4 @@
-const { MongoClient } = require("mongodb");
+import { MongoClient } from "mongodb";
 
 // Replace the uri string with your MongoDB deployment's connection string.
 const uri =
@@ -6,19 +6,23 @@ const uri =
 
 const client = new MongoClient(uri);
 
+interface Haiku {
+  title: string;
+  content: string;
+}
+
 async function run() {
   try {
     await client.connect();
 
     const database = client.db("insert_db");
-    const haiku = database.collection("haiku");
-    // create a document to insert
-    const doc = {
+    // Specifying a Schema is optional, but it enables type hints on
+    // finds and inserts
+    const haiku = database.collection<Haiku>("haiku");
+    const result = await haiku.insertOne({
       title: "Record of a Shriveled Datum",
       content: "No bytes, no problem. Just insert a document, in MongoDB",
-    }
-    const result = await haiku.insertOne(doc);
-
+    });
     console.log(`A document was inserted with the _id: ${result.insertedId}`);
   } finally {
     await client.close();
