91c1c33ff4cf75080a382e759e0ece7c7a130004
Nathan <nathan.leniz@10gen.com>
2021-08-10 15:42:58 -0700

    Docsp 13857 (#16)
    * wip
    
    * fix links
    
    * remove redundant `here'
    
    * edits
    
    * DOCSP-13857: insertOne fundamentals page
    
    * move go module files
    
    * add note about new namespace
    
    * testing more constants
    
    * more testing
    
    * prefer slashes
    
    * nits
    
    * wording
    
    * nits
    
    * haiku

---
+++ source/includes/usage-examples/code-snippets/insertOne.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+
+	var uri string
+	if uri = os.Getenv("MONGODB_URI"); uri == "" {
+		log.Fatal("You must set your `MONGODB_URI' environmental variable. See\n\t https://docs.mongodb.com/drivers/go/current/usage-examples/")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// begin insertOne
+	coll := client.Database("insertDB").Collection("haikus")
+	doc := bson.D{{"title", "Record of a Shriveled Datum"}, {"text", "No bytes, no problem. Just insert a document, in MongoDB"}}
+
+	result, err := coll.InsertOne(context.TODO(), doc)
+	// end insertOne
+
+	if err != nil {
+		panic(err)
+	}
+
+	// When you run this file it should print "Document inserted with ID: ObjectID("...")
+	fmt.Printf("Document inserted with ID: %s\n", result.InsertedID)
+}
