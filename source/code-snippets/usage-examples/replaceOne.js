267b2e461ce92bd0ac125b31df062712717ead2d
Aleksander Binion <77393289+biniona-mongodb@users.noreply.github.com>
2021-08-20 14:58:16 -0400

    (DOCSP 17936) Replace One TS (#239)
    Co-authored-by: Chris Bush <chris.bush@10gen.com>
    Co-authored-by: Daria Pardue <81593090+dariakp@users.noreply.github.com>

--- source/code-snippets/usage-examples/replaceOne.js
+++ source/code-snippets/usage-examples/replaceOne.js
@@ -14,35 +14,14 @@ async function run() {
     const movies = database.collection("movies");
 
     // create a query for a movie to update
-    const query = { title: "Blacksmith Scene" };
-    const options = {
-      // create a document if no documents match the query
-      upsert: true,
-    };
+    const query = { title: { $regex: "The Cat from" } };
     // create a new document that will be used to replace the existing document
     const replacement = {
-      title: "Sandcastles in the Sand",
-      plot:
-        "Robin Sparkles mourns for a relationship with a mall rat at an idyllic beach.",
+      title: `The Cat from Sector ${Math.floor(Math.random() * 1000) + 1}`,
     };
 
-    const result = await movies.replaceOne(query, replacement, options);
-
-    if (result.modifiedCount === 0 && result.upsertedCount === 0) {
-      console.log("No changes made to the collection.");
-    } else {
-      if (result.matchedCount === 1) {
-        console.log("Matched " + result.matchedCount + " documents.");
-      }
-      if (result.modifiedCount === 1) {
-        console.log("Updated one document.");
-      }
-      if (result.upsertedCount === 1) {
-        console.log(
-          "Inserted one new document with an _id of " + result.upsertedId._id
-        );
-      }
-    }
+    const result = await movies.replaceOne(query, replacement);
+    console.log(`Modified ${result.modifiedCount} document(s)`);
   } finally {
     await client.close();
   }
