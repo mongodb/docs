ba1be7b3cb49d237e41e1d099dc493bcf3b0fe8b
kyuan-mongodb <78768401+kyuan-mongodb@users.noreply.github.com>
2021-08-24 14:13:11 -0400

    DOCSP-13861 replace usage example (#23)
    * added replace usage example page

---
+++ source/includes/usage-examples/code-snippets/replace.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+
+	var uri string
+	if uri = os.Getenv("MONGODB_URI"); uri == "" {
+		log.Fatal("You must set your `MONGODB_URI' environmental variable. See\n\t https://docs.mongodb.com/drivers/go/current/usage-examples/")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// begin replace
+	coll := client.Database("insertDB").Collection("haikus")
+	filter := bson.D{{"title", "Record of a Shriveled Datum"}}
+	replacement := bson.D{{"title", "Dodging Greys"}, {"text", "When there're no matches, no longer need to panic. You can use upsert"}}
+
+	result, err := coll.ReplaceOne(context.TODO(), filter, replacement)
+	// end replace
+
+	if err != nil {
+		panic(err)
+	}
+
+	// When you run this file for the first time, it should print: "Number of documents replaced: 1"
+	if result.MatchedCount != 0 {
+		fmt.Println("Number of documents replaced: %d\n", result.ModifiedCount)
+	}
+}
