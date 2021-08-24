1741fc6d7893d7f39e29867ad09b61064d7bddf4
kyuan-mongodb <78768401+kyuan-mongodb@users.noreply.github.com>
2021-08-24 14:05:19 -0400

    DOCSP-13865 distinct usage examples (#25)
    * added distinct usage examples page

---
+++ source/includes/usage-examples/code-snippets/distinct.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	var uri string
+	if uri = os.Getenv("MONGODB_URI"); uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://docs.mongodb.com/drivers/go/current/usage-examples/")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// begin distinct
+	coll := client.Database("sample_mflix").Collection("movies")
+	filter := bson.D{{"directors", "Natalie Portman"}}
+
+	results, err := coll.Distinct(context.TODO(), "title", filter)
+	// end distinct
+
+	if err != nil {
+		panic(err)
+	}
+
+	// When you run this file, it should print: 
+	// A Tale of Love and Darkness
+	// New York, I Love You
+	for _, result := range results {
+		fmt.Println(result)
+	}
+}
