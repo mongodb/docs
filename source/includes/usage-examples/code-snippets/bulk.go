33d89e94307d81340bcfd340aa8108e0ba84aef0
kyuan-mongodb <78768401+kyuan-mongodb@users.noreply.github.com>
2021-08-26 16:27:57 -0400

    DOCSP-13868 bulk usage example (#26)
    * added bulk usage example page

---
+++ source/includes/usage-examples/code-snippets/bulk.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	var uri string
+	if uri = os.Getenv("MONGODB_URI"); uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://docs.mongodb.com/drivers/go/current/usage-examples/")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// begin bulk
+	coll := client.Database("insertDB").Collection("haikus")
+	models := []mongo.WriteModel{
+		mongo.NewReplaceOneModel().SetFilter(bson.D{{"title", "Record of a Shriveled Datum"}}).
+			SetReplacement(bson.D{{"title", "Dodging Greys"}, {"text", "When there're no matches, no longer need to panic. You can use upsert"}}),
+		mongo.NewUpdateOneModel().SetFilter(bson.D{{"title", "Dodging Greys"}}).
+			SetUpdate(bson.D{{"$set", bson.D{{"title", "Dodge The Greys"},}},}),
+	}
+	opts := options.BulkWrite().SetOrdered(true)
+
+	results, err := coll.BulkWrite(context.TODO(), models, opts)
+	// end bulk
+
+	if err != nil {
+		panic(err)
+	}
+
+	// When you run this file for the first time, it should print: 
+	// Number of documents replaced or modified: 2
+	fmt.Printf("Number of documents replaced or modified: %d", results.ModifiedCount)
+}
