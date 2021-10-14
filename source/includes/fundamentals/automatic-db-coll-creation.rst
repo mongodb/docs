506aea9ee6184a0c10890df85be807103b37b89c
kyuan-mongodb <78768401+kyuan-mongodb@users.noreply.github.com>
2021-10-13 20:03:56 -0400

    DOCSP-13848 crud compound operations (#64)
    * added CRUD Compound Operations page

--- source/includes/fundamentals/automatic-db-coll-creation.rst
+++ source/includes/fundamentals/automatic-db-coll-creation.rst
@@ -1,6 +1,6 @@
 .. tip:: Non-existent Databases and Collections
 
-   The server automatically creates the necessary database and
+   The server implicitly creates the necessary database and
    collection when you perform a write operation against them if they
    don't already exist.
    
---
+++ source/includes/fundamentals/code-snippets/CRUD/compoundOperations.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	var uri string
+	if uri = os.Getenv("MONGODB_URI"); uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://docs.mongodb.com/drivers/go/current/usage-examples/")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	client.Database("tea").Collection("ratings").Drop(context.TODO())
+
+	// begin insertDocs
+	coll := client.Database("tea").Collection("ratings")
+	docs := []interface{}{
+		bson.D{{"type", "Masala"}, {"rating", 10}},
+		bson.D{{"type", "Assam"}, {"rating", 5}},
+		bson.D{{"type", "Oolong"}, {"rating", 7}},
+		bson.D{{"type", "Earl Grey"}, {"rating", 8}},
+		bson.D{{"type", "English Breakfast"}, {"rating", 5}},
+	}
+
+	result, insertErr := coll.InsertMany(context.TODO(), docs)
+	if insertErr != nil {
+		panic(insertErr)
+	}
+	fmt.Printf("Number of documents inserted: %d\n", len(result.InsertedIDs))
+	//end insertDocs
+
+	fmt.Println("FindOneAndDelete:")
+	{
+		//begin FindOneAndDelete
+		filter := bson.D{{"type", "Assam"}}
+
+		var deletedDoc bson.D
+		err := coll.FindOneAndDelete(context.TODO(), filter).Decode(&deletedDoc)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(deletedDoc)
+		//end FindOneAndDelete
+	}
+
+	fmt.Println("FindOneAndReplace:")
+	{
+		//begin FindOneAndReplace
+		filter := bson.D{{"type", "English Breakfast"}}
+		replacement := bson.D{{"type", "Ceylon"}, {"rating", 6}}
+
+		var previousDoc bson.D
+		err := coll.FindOneAndReplace(context.TODO(), filter, replacement).Decode(&previousDoc)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(previousDoc)
+		//end FindOneAndReplace
+	}
+
+	fmt.Println("FindOneAndUpdate:")
+	{
+		//begin FindOneAndUpdate
+		filter := bson.D{{"type", "Oolong"}}
+		update := bson.D{{"$set", bson.D{{"rating", 9}}}}
+		opts := options.FindOneAndUpdate().SetReturnDocument(options.After)
+
+		var updatedDoc bson.D
+		err := coll.FindOneAndUpdate(context.TODO(), filter, update, opts).Decode(&updatedDoc)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(updatedDoc)
+		//end FindOneAndUpdate
+	}
+}
