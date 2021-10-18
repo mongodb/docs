87d6b129bf8d8d2ca8ebb0a413cc29e3288c3ce7
kyuan-mongodb <78768401+kyuan-mongodb@users.noreply.github.com>
2021-10-18 10:14:41 -0400

    DOCSP-13844 crud upsert (#71)
    * added CRUD upsert page

---
+++ source/includes/fundamentals/code-snippets/CRUD/upsert.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	var uri string
+	if uri = os.Getenv("MONGODB_URI"); uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://docs.mongodb.com/drivers/go/current/usage-examples/")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	client.Database("tea").Collection("ratings").Drop(context.TODO())
+
+	// begin insert docs
+	coll := client.Database("tea").Collection("ratings")
+	docs := []interface{}{
+		bson.D{{"type", "Masala"}, {"rating", 10}},
+		bson.D{{"type", "Assam"}, {"rating", 5}},
+	}
+
+	result, err := coll.InsertMany(context.TODO(), docs)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Number of documents inserted: %d\n", len(result.InsertedIDs))
+	//end insert docs
+
+	fmt.Println("Upsert:")
+	{
+		// begin upsert
+		filter := bson.D{{"type", "Oolong"}}
+		update := bson.D{{"$set", bson.D{{"rating", 8}}}}
+		opts := options.Update().SetUpsert(true)
+
+		result, err := coll.UpdateOne(context.TODO(), filter, update, opts)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Number of documents updated: %v\n", result.ModifiedCount)
+		fmt.Printf("Number of documents upserted: %v\n", result.UpsertedCount)
+		// end upsert
+	}
+}
