a6c52d32a1ade0269063aa9cb5d139a697fa1490
John Williams <john.williams@10gen.com>
2021-11-08 09:34:25 -0600

    (DOCSP-19551): fix GCP private endpoint API example

--- source/includes/api/requests/private-endpoint-create-one-endpoint-gcp.sh
+++ source/includes/api/requests/private-endpoint-create-one-endpoint-gcp.sh
@@ -1,7 +1,7 @@
 curl --user "{PUBLIC-KEY}:{PRIVATE-KEY}" --digest \
      --header "Accept: application/json" \
      --header "Content-Type: application/json" \
-     --request POST "https://cloud.mongodb.com/api/atlas/v1.0/groups/{GROUP-ID}/privateEndpoint/AZURE/endpointService/{ENDPOINT-SERVICE-ID}/endpoint?pretty=true" \
+     --request POST "https://cloud.mongodb.com/api/atlas/v1.0/groups/{GROUP-ID}/privateEndpoint/GCP/endpointService/{ENDPOINT-SERVICE-ID}/endpoint?pretty=true" \
      --data '
        {
          "endpointGroupName" : "google-endpoint-group",
