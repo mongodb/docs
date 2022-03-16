5fbafbfb56cfd02b5b8c28428c614e56d8b68a1b
zach-carr <54542042+zach-carr@users.noreply.github.com>
2022-03-16 14:00:01 -0400

    (DOCSP-21406) Fix incorrect date label (#3144)

--- source/includes/insert-data.js
+++ source/includes/insert-data.js
@@ -19,8 +19,8 @@ const client = new MongoClient(url);
          // Construct a document                                                                                                                                                              
          let personDocument = {
              "name": { "first": "Alan", "last": "Turing" },
-             "birth": new Date(1912, 5, 23), // June 23, 1912                                                                                                                                 
-             "death": new Date(1954, 5, 7),  // June 7, 1954                                                                                                                                  
+             "birth": new Date(1912, 5, 23), // May 23, 1912                                                                                                                                 
+             "death": new Date(1954, 5, 7),  // May 7, 1954                                                                                                                                  
              "contribs": [ "Turing machine", "Turing test", "Turingery" ],
              "views": 1250000
          }
