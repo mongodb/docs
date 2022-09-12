8998d90ee893a18a794073e045e7b8415ea973db
Chris Cho <chris.cho@mongodb.com>
2022-09-12 12:56:36 -0400

    091222-fix-version-constant-in-code (#835)

--- source/includes/stable-api-snippets/scala-client.scala
+++ source/includes/stable-api-snippets/scala-client.scala
@@ -6,7 +6,7 @@ import org.mongodb.scala._
 // Replace <connection string> with your MongoDB deployment's connection string.
 val uri = "<connection string>"
 
-// Set the version of the {+stable-api+} on the client.
+// Set the Stable API version on the client.
 val mongoClientSettings = MongoClientSettings.builder().applyConnectionString(ConnectionString(uri))
     .serverApi(ServerApi.builder().version(ServerApiVersion.V1).build())
     .build()
