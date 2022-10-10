e57ef2afb2e4b756dba2967a9ee2f85ccd4995bb
Jordan Smith <45415425+jordan-smith721@users.noreply.github.com>
2022-10-10 10:35:19 -0600

    DOCSP-24545 Fundamentals - Retrieve (#24)

---
+++ source/includes/code-examples/Restaurant.cs
@@ -0,0 +1,17 @@
+public class Restaurant
+{
+    public ObjectId Id { get; set; }
+
+    public string Name { get; set; }
+
+    [BsonElement("restaurant_id")]
+    public string RestaurantId { get; set; }
+
+    public string Cuisine { get; set; }
+
+    public object Address { get; set; }
+
+    public string Borough { get; set; }
+
+    public List<object> Grades { get; set; }
+}
