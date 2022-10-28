2cacc690a73219260656520fa8bba43d5c907255
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2022-10-28 16:44:56 -0400

    DOCSP-18237: run a command (#176)
    * DOCSP-18237: run a command page
    
    * first pass fixes
    
    * changed examples
    
    * MW PR fixes 1
    
    * small fixes
    
    * link to logical clock ref

---
+++ source/includes/fundamentals/code-snippets/CRUD/runCommand.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+
+	var uri string
+	if uri = os.Getenv("MONGODB_URI"); uri == "" {
+		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+	}
+
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		}
+	}()
+
+	// start-runcommand
+	db := client.Database("plants")
+	filter := bson.D{{"price", bson.D{{"$lte", 9.99}}}}
+	command := bson.D{{"count", "flowers"}, {"query", filter}}
+
+	var result bson.M
+	err = db.RunCommand(context.TODO(), command).Decode(&result)
+	// end-runcommand
+
+	if err != nil {
+		panic(err)
+	}
+	output, err := json.MarshalIndent(result, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", output)
+}
