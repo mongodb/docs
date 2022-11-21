01d8d97686e32a85380391b0eb03c9ea91eb4a0d
Chris Cho <chris.cho@mongodb.com>
2022-11-21 16:45:11 -0500

    DOCSP-25962: MONGODB-AWS aws sdk info (#298)
    * DOCSP-25962: AWS SDK for MONGODB-AWS auth

--- source/includes/fundamentals/code-snippets/MongoDbAwsAuth.java
+++ source/includes/fundamentals/code-snippets/MongoDbAwsAuth.java
@@ -28,6 +28,12 @@ public class MongoDbAwsAuth {
         // end connectionString
     }
 
+    private static void mechOnlyConnectionString() {
+        // start mechOnlyConnectionString
+        MongoClient mongoClient = MongoClients.create("mongodb://<atlasUri>?authMechanism=MONGODB-AWS");
+        // end mechOnlyConnectionString
+    }
+
     private static void connectionStringSessionToken() {
         // start connectionStringSessionToken
         MongoClient mongoClient = MongoClients.create("mongodb://<awsKeyId>:<awsSecretKey>@<atlasUri>?authMechanism=MONGODB-AWS&authMechanismProperties=AWS_SESSION_TOKEN:<awsSessionToken>");
@@ -47,6 +53,19 @@ public class MongoDbAwsAuth {
         // end mongoCredential
     }
 
+    private static void mechOnlyMongoCredential() {
+        // start mechOnlyMongoCredential
+        MongoCredential credential = MongoCredential.createAwsCredential(null, null);
+
+        MongoClient mongoClient = MongoClients.create(
+                MongoClientSettings.builder()
+                .applyToClusterSettings(builder ->
+                builder.hosts(Arrays.asList(new ServerAddress("<atlasUri>"))))
+                .credential(credential)
+                .build());
+        // end mechOnlyMongoCredential
+    }
+
     private static void mongoCredentialSessionTokenConnString() {
         // start mongoCredentialSessionTokenConnString
         MongoCredential credential = MongoCredential.createAwsCredential("<awsKeyId>", "<awsSecretKey>".toCharArray());
@@ -63,32 +82,38 @@ public class MongoDbAwsAuth {
     private static void mongoCredentialSessionTokenCredential() {
         // start mongoCredentialSessionTokenCredential
         MongoCredential credential = MongoCredential.createAwsCredential("<awsKeyId>", "<awsSecretKey>".toCharArray()).withMechanismProperty("AWS_SESSION_TOKEN",  "<awsSessionToken>");
-        ConnectionString connectionString = new ConnectionString("mongodb://<atlasUri>/?authMechanism=MONGODB-AWS");
 
         MongoClient mongoClient = MongoClients.create(
                 MongoClientSettings.builder()
-                .applyConnectionString(connectionString)
+                .applyToClusterSettings(builder ->
+                builder.hosts(Arrays.asList(new ServerAddress("<atlasUri>"))))
                 .credential(credential)
                 .build());
         // end mongoCredentialSessionTokenCredential
     }
 
+    private static void mongoCredentialECSorEC2() {
+        // start mongoCredentialECSorEC2
+        MongoCredential credential = MongoCredential.createAwsCredential(null, null);
+        // end mongoCredentialECSorEC2
+    }
+
     private static void refreshCredentials() {
-        
+
         // start refreshCredentials
         Supplier<AwsCredential> awsFreshCredentialSupplier = () -> {
-            // Add your code to fetch new credentials, such as assuming a role using the AWS SDK.
-            
-            // Ensure you return the temporary credentials.
+            // Add your code here to fetch new credentials here
+
+            // Return the new credentials
             return new AwsCredential("<awsKeyId>", "<awsSecretKey>", "<awsSessionToken>");
         };
 
-        MongoCredential credential = MongoCredential.createAwsCredential(null, null)
+        MongoCredential credential = MongoCredential.createAwsCredential("<awsKeyId>", "<awsSecretKey>".toCharArray())
                 .withMechanismProperty(MongoCredential.AWS_CREDENTIAL_PROVIDER_KEY, awsFreshCredentialSupplier);
         MongoClient mongoClient = MongoClients.create(
                 MongoClientSettings.builder()
                 .applyToClusterSettings(builder ->
-                builder.hosts(Collections.singletonList(new ServerAddress("<hostname>", 27017))))
+                    builder.hosts(Collections.singletonList(new ServerAddress("<hostname>", <port>))))
                 .credential(credential)
                 .build());
         // end refreshCredentials
