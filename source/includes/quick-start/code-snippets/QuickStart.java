757e26d9af4018d3b1dcd6131541299f6dd4966a
Chris Cho <chris.cho@mongodb.com>
2022-12-08 11:00:09 -0500

    DOCSP-26555: update quickstart (#311)
    * DOCSP-26555: update quickstart

--- source/includes/quick-start/code-snippets/QuickStart.java
+++ source/includes/quick-start/code-snippets/QuickStart.java
@@ -10,17 +10,23 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-public static void main( String[] args ) {
+public class QuickStart {
+    public static void main( String[] args ) {
 
-    // Replace the uri string with your MongoDB deployment's connection string
-    String uri = "<connection string uri>";
+        // Replace the placeholder with your MongoDB deployment's connection string
+        String uri = "<connection string uri>";
 
-    try (MongoClient mongoClient = MongoClients.create(uri)) {
-        MongoDatabase database = mongoClient.getDatabase("sample_mflix");
-        MongoCollection<Document> collection = database.getCollection("movies");
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> collection = database.getCollection("movies");
 
-        Document doc = collection.find(eq("title", "Back to the Future")).first();
-        System.out.println(doc.toJson());
+            Document doc = collection.find(eq("title", "Back to the Future")).first();
+            if (doc != null) {
+                System.out.println(doc.toJson());
+            } else {
+                System.out.println("No matching documents found.");
+            }
+        }
     }
 }
 // end QuickStart
