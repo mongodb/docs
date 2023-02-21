97d06cba667e6f21ac0f960202f1942ea931db90
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2023-02-21 10:29:00 -0500

    DOCSP-27919: stableapi connection for atlas (#79)
    * DOCSP-27919: stableapi connection for atlas
    
    * MW suggestions
    
    * Empty commit

--- source/includes/fundamentals/code-examples/connection/AtlasConnection.cs
+++ source/includes/fundamentals/code-examples/connection/AtlasConnection.cs
@@ -1,7 +1,11 @@
 using MongoDB.Driver;
 
 // Connection URI
-const string connectionUri = "mongodb+srv://user1:password1@cluster0.sample.mongodb.net/?retryWrites=true&w=majority";
+const string connectionUri = "mongodb+srv://<username>:<password>@cluster0.sample.mongodb.net/?retryWrites=true&w=majority";
+
+var serverApi = new ServerApi(ServerApiVersion.V1);
+var settings = MongoClientSettings.FromConnectionString(connectionString);
+settings.ServerApi = serverApi;
 
 // Create a new client and connect to the server
-var client = new MongoClient(connectionUri);
+var client = new MongoClient(settings);
