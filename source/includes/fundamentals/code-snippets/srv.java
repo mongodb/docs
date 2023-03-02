ec8a98443870e0361b7860116ccd80bbcd7ab977
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2023-03-02 18:41:21 -0500

    DOCSP-27922: connection updates atlas (#338)
    * DOCSP-27922: update connection guide to have stableapi
    
    * error msg fix
    
    * CC PR fixes 1
    
    * Chris feedback
    
    * Chris feedback 2
    
    * monospace
    
    ---------
    
    Co-authored-by: Jordan Smith <jordan.smith@mongodb.com>

--- source/includes/fundamentals/code-snippets/srv.java
+++ source/includes/fundamentals/code-snippets/srv.java
@@ -1,31 +1,40 @@
 package fundamentals;
 
+import com.mongodb.*;
 import org.bson.BsonDocument;
 import org.bson.BsonInt64;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 
-import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoException;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 
 public class MongoClientConnectionExample {
     public static void main(String[] args) {
-        // Replace the uri string with your MongoDB deployment's connection string
-        String uri = "mongodb://user:pass@sample.host:27017/?maxPoolSize=20&w=majority";
+        // Replace the placeholders with your credentials and hostname
+        String uri = "mongodb+srv://<username>:<password>@<hostname>/?retryWrites=true&w=majority";
 
-        try (MongoClient mongoClient = MongoClients.create(uri)) {
+        // Construct a ServerApi instance using the ServerApi.builder() method
+        ServerApi serverApi = ServerApi.builder()
+                .version(ServerApiVersion.V1)
+                .build();
 
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(uri))
+                .serverApi(serverApi)
+                .build();
+
+        // Create a new client and connect to the server
+        try (MongoClient mongoClient = MongoClients.create(settings)) {
             MongoDatabase database = mongoClient.getDatabase("admin");
-
             try {
+                // Send a ping to confirm a successful connection
                 Bson command = new BsonDocument("ping", new BsonInt64(1));
                 Document commandResult = database.runCommand(command);
-                System.out.println("Connected successfully to server.");
+                System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
             } catch (MongoException me) {
-                System.err.println("An error occurred while attempting to run a command: " + me);
+                System.err.println(me);
             }
         }
     }
