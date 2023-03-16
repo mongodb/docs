d4977cf8df80dc336fc8ab9da2c16f48689a0d02
kyuan-mongodb <78768401+kyuan-mongodb@users.noreply.github.com>
2023-03-16 09:37:12 -0500

    DOCSP-28211 csharp getting started section (#4219)
    * added csharp to getting started section

---
+++ source/includes/fts-tutorial/complex-fts-query.cs
@@ -0,0 +1,54 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using MongoDB.Driver.Search;
+
+public class ComplexQuery {
+    private static IMongoCollection<MovieDocument> moviesCollection;
+    private static string _mongoConnectionString = "<connection-string>";
+
+    public static void Main(string[] args) {
+        // allow automapping of the camelCase database fields to our MovieDocument
+        var camelCaseConvention = new ConventionPack { new CamelCaseElementNameConvention() };
+        ConventionRegistry.Register("CamelCase", camelCaseConvention, type => true);
+
+        // connect to your Atlas cluster
+        var mongoClient = new MongoClient(_mongoConnectionString);
+        var mflixDatabase = mongoClient.GetDatabase("sample_mflix");
+        moviesCollection = mflixDatabase.GetCollection<MovieDocument>("movies");
+
+        // declare data for the compound query
+        string[] terms = { "Hawaii", "Alaska" };
+        string[] genres = { "Comedy", "Romance" };
+        string[] titles = { "Beach", "Snow" };
+
+        // define and run pipeline
+        var results = moviesCollection.Aggregate()
+            .Search(Builders<MovieDocument>.Search.Compound()
+                .Must(Builders<MovieDocument>.Search.Text(movie => movie.Plot, terms))
+                .Must(Builders<MovieDocument>.Search.Regex(movie => movie.Plot, "([0-9]{4})", true))
+                .MustNot((Builders<MovieDocument>.Search.Text(movie => movie.Genres, genres)))
+                .MustNot((Builders<MovieDocument>.Search.Text(movie => movie.Title, titles))))
+            .Project<MovieDocument>(Builders<MovieDocument>.Projection
+                .Include(movie => movie.Genres)
+                .Include(movie => movie.Plot)
+                .Include(movie => movie.Title)
+                .Exclude(movie => movie.Id))
+            .ToList();
+
+        // print results
+        foreach (var movie in results) {
+            Console.WriteLine(movie.ToJson());
+        }
+    }
+}
+
+[BsonIgnoreExtraElements]
+public class MovieDocument {
+    [BsonIgnoreIfDefault]
+    public ObjectId Id { get; set; }
+    public string[] Genres { get; set; }
+    public string Plot { get; set; }
+    public string Title { get; set; }
+}
