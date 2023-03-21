91fde5acbb8de121b83564afe0d2d3197d15fecc
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2023-03-21 16:17:36 -0400

    DOCSP-28359: PyMongo and Motor stable api connection updates (#863)

---
+++ source/includes/connection-snippets/scram/py-motor-connection-no-stableapi.py
@@ -0,0 +1,18 @@
+import asyncio
+from motor.motor_asyncio import AsyncIOMotorClient
+
+async def ping_server():
+  # Replace the placeholder with your Atlas connection string
+  uri = "<connection string>"
+
+  # Create a new client and connect to the server
+  client = AsyncIOMotorClient(uri)
+
+  # Send a ping to confirm a successful connection
+  try:
+      client.admin.command('ping')
+      print("Pinged your deployment. You successfully connected to MongoDB!")
+  except Exception as e:
+      print(e)
+      
+asyncio.run(ping_server())
---
+++ source/includes/connection-snippets/scram/py-motor-connection-tornado.py
@@ -0,0 +1,18 @@
+import tornado
+import motor
+
+async def ping_server():
+    # Replace the placeholder with your Atlas connection string
+    uri = "<connection string>"
+
+    # Set a 5-second connection timeout when creating a new client
+    client = motor.motor_tornado.MotorClient(uri, serverSelectionTimeoutMS=5000)
+
+    # Send a ping to confirm a successful connection
+    try:
+      client.admin.command('ping')
+      print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+      print(e)
+
+tornado.ioloop.IOLoop.current().run_sync(ping_server)
