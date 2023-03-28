c4b8f136a4ce2b0e3af0546afea64ed8c5352bc1
davidhou17 <55004296+davidhou17@users.noreply.github.com>
2023-03-28 15:58:26 -0400

    (DOCSP-28223): C sharp examples [atlas-search/tutorial/null-check/] (#4238)
    * add csharp examples to page
    
    * copy review feedback
    
    * tech review feedback
    
    * add search score update

---
+++ source/includes/fts-tutorial/null-check/notnull-tutorial.cs
@@ -0,0 +1,59 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using MongoDB.Driver.Search;
+
+public class NotNullQuery 
+{
+    private static IMongoCollection<UserDocument> usersCollection;
+    private static string _mongoConnectionString = "<connection-string>";
+
+    public static void Main(string[] args) 
+    {
+        // allow automapping of the camelCase database fields to our UserDocument
+        var camelCaseConvention = new ConventionPack { new CamelCaseElementNameConvention() };
+        ConventionRegistry.Register("CamelCase", camelCaseConvention, type => true);
+
+        // connect to your Atlas cluster
+        var mongoClient = new MongoClient(_mongoConnectionString);
+        var mflixDatabase = mongoClient.GetDatabase("sample_mflix");
+        usersCollection = mflixDatabase.GetCollection<UserDocument>("users");
+
+        // define a builder for the search score
+        var scoreBuilder = new SearchScoreDefinitionBuilder<UserDocument>();
+
+        // define and run pipeline
+        var results = usersCollection.Aggregate()
+            .Search(Builders<UserDocument>.Search.Compound()
+                .Should(Builders<UserDocument>
+                    .Search.Wildcard(user => user.Password, "*", true))
+                .Should(Builders<UserDocument>.Search.Compound(scoreBuilder.Constant(2))
+                    .MustNot(Builders<UserDocument>.Search.Exists(user => user.Password))))
+                .Limit(5)
+                .Project<UserDocument>(Builders<UserDocument>.Projection
+                    .Include(user => user.Name)
+                    .Include(user => user.Password)
+                    .MetaSearchScore("score")
+                    .Exclude(user => user.Id))
+                .ToList();
+
+        // print results
+        foreach (var user in results) 
+        {
+            Console.WriteLine(user.ToJson());
+        }
+    }
+}
+
+[BsonIgnoreExtraElements]
+public class UserDocument 
+{
+    [BsonIgnoreIfDefault]
+    public string Id { get; set; }
+    public string Name { get; set; }
+    [BsonIgnoreIfDefault]
+    public string Password { get; set; }
+    [BsonElement("score")]
+    public double Score { get; set; }
+}
---
+++ source/includes/fts-tutorial/null-check/tutorial.cs
@@ -0,0 +1,46 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using MongoDB.Driver.Search;
+
+public class IsNullQuery 
+{
+    private static IMongoCollection<UserDocument> usersCollection;
+    private static string _mongoConnectionString = "<connection-string>";
+
+    public static void Main(string[] args) 
+    {
+        // allow automapping of the camelCase database fields to our UserDocument
+        var camelCaseConvention = new ConventionPack { new CamelCaseElementNameConvention() };
+        ConventionRegistry.Register("CamelCase", camelCaseConvention, type => true);
+
+        // connect to your Atlas cluster
+        var mongoClient = new MongoClient(_mongoConnectionString);
+        var mflixDatabase = mongoClient.GetDatabase("sample_mflix");
+        usersCollection = mflixDatabase.GetCollection<UserDocument>("users");
+
+        // define and run pipeline
+        var results = usersCollection.Aggregate()
+            .Search(Builders<UserDocument>.Search.Compound()
+                .Must(Builders<UserDocument>.Search.Exists(user => user.Password))
+                .MustNot(Builders<UserDocument>.Search.Wildcard(user => user.Password, "*", true)))
+                .ToList();
+
+        // print results
+        foreach (var user in results) 
+        {
+            Console.WriteLine(user.ToJson());
+        }
+    }
+}
+
+[BsonIgnoreExtraElements]
+public class UserDocument 
+{
+    [BsonIgnoreIfDefault]
+    public ObjectId Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
