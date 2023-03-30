a709f89ea22a317bd206c49a388ed8ab2237b272
Jordan Smith <45415425+jordan-smith721@users.noreply.github.com>
2023-03-30 15:43:31 -0700

    DOCSP-28590 Add POCOs to fully runnable examples (#90)

--- source/includes/code-examples/delete-one/DeleteOne.cs
+++ source/includes/code-examples/delete-one/DeleteOne.cs
@@ -53,3 +53,43 @@ public class DeleteOne
         _restaurantsCollection = restaurantsDatabase.GetCollection<Restaurant>("restaurants");
     }
 }
+
+public class Restaurant
+{
+    public ObjectId Id { get; set; }
+
+    public string Name { get; set; }
+
+    [BsonElement("restaurant_id")]
+    public string RestaurantId { get; set; }
+
+    public string Cuisine { get; set; }
+
+    public Address Address { get; set; }
+
+    public string Borough { get; set; }
+
+    public List<GradeEntry> Grades { get; set; }
+}
+
+public class Address
+{
+    public string Building { get; set; }
+
+    [BsonElement("coord")]
+    public double[] Coordinates { get; set; }
+
+    public string Street { get; set; }
+
+    [BsonElement("zipcode")]
+    public string ZipCode { get; set; }
+}
+
+public class GradeEntry
+{
+    public DateTime Date { get; set; }
+
+    public string Grade { get; set; }
+
+    public float Score { get; set; }
+}
