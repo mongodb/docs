d9f995995d36ea24deb2c4031251dde405d2a6f7
kanchana-mongodb <54281287+kanchana-mongodb@users.noreply.github.com>
2023-04-10 09:17:11 -0700

    DOCSP-28215 csharp for geo tutorial (#4284)
    * DOCSP-28215 csharp for geo tutorial
    
    * Apply suggestions from code review
    
    Co-authored-by: kyuan-mongodb <78768401+kyuan-mongodb@users.noreply.github.com>
    
    * DOCSP-28215 updates for CR feedback
    
    ---------
    
    Co-authored-by: kyuan-mongodb <78768401+kyuan-mongodb@users.noreply.github.com>

---
+++ source/includes/fts-tutorial/geo/geo-tutorial-query.cs
@@ -0,0 +1,73 @@
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using MongoDB.Driver.GeoJsonObjectModel;
+using MongoDB.Driver.Search;
+using System;
+
+public class GeoQuery 
+{
+    private static IMongoCollection<AirbnbDocument> airbnbCollection;
+    private static string _mongoConnectionString = "<connection-string>";
+
+    public static void Main(string[] args) 
+    {
+        // allow automapping of the camelCase database fields to our AirbnbDocument
+        var camelCaseConvention = new ConventionPack { new CamelCaseElementNameConvention() };
+        ConventionRegistry.Register("CamelCase", camelCaseConvention, type => true);
+
+        // connect to your Atlas cluster
+        var mongoClient = new MongoClient(_mongoConnectionString);
+        var airbnbDatabase = mongoClient.GetDatabase("sample_airbnb");
+        airbnbCollection = airbnbDatabase.GetCollection<AirbnbDocument>("listingsAndReviews");
+
+        // declare data for the compound query
+        string property_type = "Condominium";
+        var coordinates = new GeoJson2DCoordinates[]
+        {
+          new(-161.323242, 22.512557),
+          new(-152.446289, 22.065278),
+          new(-156.09375, 17.811456),
+          new(-161.323242, 22.512557)
+        };
+        var polygon = GeoJson.Polygon(coordinates);
+
+        // define and run pipeline
+        var results = airbnbCollection.Aggregate()
+            .Search(Builders<AirbnbDocument>.Search.Compound()
+                .Must(Builders<AirbnbDocument>.Search.GeoWithin(airbnb => airbnb.Address.Location, polygon))
+                .Should((Builders<AirbnbDocument>.Search.Text(airbnb => airbnb.PropertyType, property_type))))
+            .Limit (10)
+            .Project<AirbnbDocument>(Builders<AirbnbDocument>.Projection
+                .Include(airbnb => airbnb.PropertyType)
+                .Include(airbnb => airbnb.Address.Location)
+                .Include(airbnb => airbnb.Name)
+                .Exclude(airbnb => airbnb.Id)
+                .MetaSearchScore("score"))
+            .ToList();
+
+        // print results
+        foreach (var x in results) {
+            Console.WriteLine(x.ToJson());
+        }
+    }
+}
+[BsonIgnoreExtraElements]
+public class AirbnbDocument 
+{
+    [BsonIgnoreIfDefault]
+    public ObjectId Id { get; set; }
+    public String Name { get; set; }
+    [BsonElement("property_type")] 
+    public string PropertyType { get; set; }
+    public Address Address { get; set; }
+    public double Score { get; set; }
+}
+[BsonIgnoreExtraElements]
+public class Address 
+{
+    public GeoJsonPoint<GeoJson2DCoordinates> Location { get; set; }
+}
