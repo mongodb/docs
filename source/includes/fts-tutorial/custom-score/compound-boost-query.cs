44b879181546d21690c3a1fafef5f623373592c8
kyuan-mongodb <78768401+kyuan-mongodb@users.noreply.github.com>
2023-04-10 14:22:14 -0500

    DOCSP-28213 csharp compound score tutorial (#4294)
    * added csharp to compound score tutorial

---
+++ source/includes/fts-tutorial/custom-score/compound-boost-query.cs
@@ -0,0 +1,57 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using MongoDB.Driver.Search;
+
+public class CompoundBoostSingleExample
+{
+    private static IMongoCollection<MovieDocument> moviesCollection;
+    private static string _mongoConnectionString = "<connection-string>";
+
+    public static void Main(string[] args)
+    {
+        // allow automapping of the camelCase database fields to our MovieDocument
+        var camelCaseConvention = new ConventionPack { new CamelCaseElementNameConvention() };
+        ConventionRegistry.Register("CamelCase", camelCaseConvention, type => true);
+
+        // connect to your Atlas cluster
+        var mongoClient = new MongoClient(_mongoConnectionString);
+        var mflixDatabase = mongoClient.GetDatabase("sample_mflix");
+        moviesCollection = mflixDatabase.GetCollection<MovieDocument>("movies");
+
+        // define and run pipeline
+        var results = moviesCollection.Aggregate()
+            .Search(Builders<MovieDocument>.Search.Compound()
+                .Must(Builders<MovieDocument>.Search.Range(movie => movie.Year, SearchRangeBuilder.Gte(2013).Lte(2015)))
+                .Should(Builders<MovieDocument>.Search.Text(movie => movie.Title, "snow", score: new SearchScoreDefinitionBuilder<MovieDocument>().Boost(2))), 
+                    new SearchHighlightOptions<MovieDocument>(movie => movie.Title))
+            .Project<MovieDocument>(Builders<MovieDocument>.Projection
+                .Include(movie => movie.Title)
+                .Include(movie => movie.Year)
+                .Exclude(movie => movie.Id)
+                .MetaSearchScore("score")
+                .MetaSearchHighlights("highlights"))
+            .Limit(10)
+            .ToList();
+
+        // print results
+        foreach (var movie in results)
+        {
+            Console.WriteLine(movie.ToJson());
+        }
+    }
+}
+
+[BsonIgnoreExtraElements]
+public class MovieDocument
+{
+    [BsonIgnoreIfDefault]
+    public ObjectId Id { get; set; }
+    public string Title { get; set; }
+    public int Year { get; set; }
+    [BsonElement("highlights")]
+    public List<SearchHighlight> Highlights { get; set; }
+    [BsonElement("score")]
+    public double Score { get; set; }
+}
---
+++ source/includes/fts-tutorial/custom-score/compound-function-query.cs
@@ -0,0 +1,67 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+using MongoDB.Driver.Search;
+
+public class CompoundFunctionExample
+{
+    private static IMongoCollection<MovieDocument> moviesCollection;
+    private static string _mongoConnectionString = "<connection-string>";
+
+    public static void Main(string[] args)
+    {
+        // allow automapping of the camelCase database fields to our MovieDocument
+        var camelCaseConvention = new ConventionPack { new CamelCaseElementNameConvention() };
+        ConventionRegistry.Register("CamelCase", camelCaseConvention, type => true);
+
+        // connect to your Atlas cluster
+        var mongoClient = new MongoClient(_mongoConnectionString);
+        var mflixDatabase = mongoClient.GetDatabase("sample_mflix");
+        moviesCollection = mflixDatabase.GetCollection<MovieDocument>("movies");
+
+        var scoreFunction = Builders<MovieDocument>.SearchScore.Function(Builders<MovieDocument>.SearchScoreFunction.Add(Builders<MovieDocument>.SearchScoreFunction.Path(movie => movie.Imdb.Rating, 2), Builders<MovieDocument>.SearchScoreFunction.Relevance()));
+        // define and run pipeline
+        var results = moviesCollection.Aggregate()
+            .Search(Builders<MovieDocument>.Search.Compound()
+                .Must(Builders<MovieDocument>.Search.Range(movie => movie.Year, SearchRangeBuilder.Gte(2013).Lte(2015)))
+                .Should(Builders<MovieDocument>.Search.Text(movie => movie.Title, "snow", score: scoreFunction)),
+                    new SearchHighlightOptions<MovieDocument>(movie => movie.Title))
+            .Project<MovieDocument>(Builders<MovieDocument>.Projection
+                .Include(movie => movie.Genres)
+                .Include(movie => movie.Title)
+                .Include(movie => movie.Year)
+                .Exclude(movie => movie.Id)
+                .MetaSearchScore("score")
+                .MetaSearchHighlights("highlights"))
+            .Limit(10)
+            .ToList();
+        
+        // print results
+        foreach (var movie in results)
+        {
+            Console.WriteLine(movie.ToJson());
+        }
+    }
+}
+
+[BsonIgnoreExtraElements]
+public class MovieDocument
+{
+    [BsonIgnoreIfDefault]
+    public ObjectId Id { get; set; }
+    public string [] Genres { get; set; }
+    public IMDB Imdb { get; set; }
+    public string Title { get; set; }
+    public int Year { get; set; }
+    [BsonElement("highlights")]
+    public List<SearchHighlight> Highlights { get; set; }
+    [BsonElement("score")]
+    public double Score { get; set; }
+}
+
+[BsonIgnoreExtraElements]
+public class IMDB
+{
+    public double Rating { get; set; }
+}
