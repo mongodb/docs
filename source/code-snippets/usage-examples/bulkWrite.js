ba22885aed30c30481648e707e30e380c62e6ba7
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2023-04-10 11:26:50 -0400

    DOCSP-29035: remove deprecated accessors BW (#661)

--- source/code-snippets/usage-examples/bulkWrite.js
+++ source/code-snippets/usage-examples/bulkWrite.js
@@ -1,4 +1,4 @@
-import { MongoClient } from "mongodb";
+const { MongoClient } = require("mongodb");
 
 // Replace the uri string with your MongoDB deployment's connection string.
 const uri = "<connection string uri>";
