e8af2a9c11c7d2cde2d9f8d2fe71f1df8f2c9ea0
cbullinger <115956901+cbullinger@users.noreply.github.com>
2023-05-01 15:03:58 -0400

    (DOCSP-29216): CRUD > Change a Doc page (#26)
    * (DOCSP-29216): CRUD > Change a Doc page
    
    * Delete unused snippets
    
    * Refactor examples to use data class
    
    * Fix snippet
    
    * Optimize imports
    
    * Add updateOne() example to page

---
+++ source/examples/generated/ChangeTest.snippet.replace-one.kt
@@ -0,0 +1,5 @@
+val filter = Filters.eq(PaintOrder::color.name, "pink")
+val update = PaintOrder(5, "orange", 25)
+val result = collection.replaceOne(filter, update)
+println("Matched document count: $result.matchedCount")
+println("Modified document count: $result.modifiedCount")
