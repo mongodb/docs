a32f4c07dd17b94e3b9940f943ab32a182d336b6
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2023-08-01 10:49:14 -0400

    DOCSP-39542: delete crud fundamentals (#19)

---
+++ source/includes/fundamentals/code-snippets/crud/delete.rs
@@ -0,0 +1,27 @@
+use bson::{ Document, bson };
+use mongodb::{ bson::doc, options::{ DeleteOptions, Hint }, Client, Collection };
+use std::{ env };
+
+#[tokio::main]
+async fn main() -> mongodb::error::Result<()> {
+    let uri = "<connection string>";
+    let client = Client::with_uri_str(uri).await?;
+
+    let my_coll: Collection<Document> = client.database("db").collection("inventory");
+
+    // begin-delete
+    let filter = doc! { "category": "garden" };
+    let hint = Hint::Name("_id_".to_string());
+    let opts: DeleteOptions = DeleteOptions::builder().hint(hint).build();
+
+    let res = my_coll.delete_many(filter, opts).await?;
+    println!("Deleted documents: {}", res.deleted_count);
+    // end-delete
+
+    // begin-options
+    let opts: DeleteOptions = DeleteOptions::builder().comment(bson!("hello!")).build();
+    let _res = my_coll.delete_one(filter, opts).await?;
+    // end-options
+
+    Ok(())
+}
