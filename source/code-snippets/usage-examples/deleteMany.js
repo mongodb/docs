ffe0cc86afb9f44105a1e849cb13f965fa2a4bab
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2023-08-31 13:02:11 -0400

    DOCSP-32718: deletemany UE code comments (#754)
    * DOCSP-32718: deletemany UE code comments
    
    * add ts code
    
    * CC comments js file
    
    * CC comments ts file

--- source/code-snippets/usage-examples/deleteMany.js
+++ source/code-snippets/usage-examples/deleteMany.js
@@ -1,21 +1,30 @@
+/* Delete multiple documents */
+
+// Import the MongoClient type from the mongodb package.
 import { MongoClient } from "mongodb";
 
 // Replace the uri string with your MongoDB deployment's connection string.
 const uri = "<connection string uri>";
 
+// Create a new client and connect to MongoDB.
 const client = new MongoClient(uri);
 
 async function run() {
   try {
     const database = client.db("sample_mflix");
     const movies = database.collection("movies");
-    // Query for all movies with a title containing the string "Santa"
-    const query = { title: { $regex: "Santa" } };
 
+    /* Delete all documents that match the specified regular
+    expression in the title field from the "movies" collection */
+    const query = { title: { $regex: "Santa" } };
     const result = await movies.deleteMany(query);
+
+    // Print the number of deleted documents
     console.log("Deleted " + result.deletedCount + " documents");
   } finally {
+    // Close the client after the operation completes
     await client.close();
   }
 }
+// Run the program and print any thrown exceptions
 run().catch(console.dir);
--- source/code-snippets/usage-examples/deleteMany.ts
+++ source/code-snippets/usage-examples/deleteMany.ts
@@ -1,22 +1,29 @@
+/* Delete multiple documents */
+
+// Import the MongoClient type from the mongodb package
 import { MongoClient } from "mongodb";
 
-// Replace the uri string with your MongoDB deployment's connection string.
+// Replace the uri string with your MongoDB deployment's connection string
 const uri = "<connection string uri>";
 
+// Create a new client and connect to MongoDB
 const client = new MongoClient(uri);
-
-interface Movie {
-  title: string;
-}
 
 async function run() {
   try {
     const database = client.db("sample_mflix");
-    const movies = database.collection<Movie>("movies");
+    const movies = database.collection("movies");
+
+    /* Delete all documents that match the specified regular
+    expression in the title field from the "movies" collection */
     const result = await movies.deleteMany({ title: { $regex: "Santa" } });
+    
+    // Print the number of deleted documents
     console.log("Deleted " + result.deletedCount + " documents");
   } finally {
+    // Close the client after the operation completes
     await client.close();
   }
 }
+// Run the program and print any thrown exceptions
 run().catch(console.dir);
