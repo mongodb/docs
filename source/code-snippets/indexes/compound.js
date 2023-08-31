f751b1b24b15b3266cd3228521a23794f67be27b
Nora Reidy <nora.reidy@mongodb.com>
2023-08-31 15:00:40 -0400

    DOCSP-32718: Compound comments (#771)
    * DOCSP-32718: insertOne comments
    
    * DOCSP-32718: runCommand comments
    
    * DOCSP-32718: compound comments
    
    * Revert "DOCSP-32718: insertOne comments"
    
    This reverts commit 6d1c8ac3dbfc84ce88a3ce0ded4a3b89bf6c238a.
    
    * Revert "DOCSP-32718: runCommand comments"
    
    This reverts commit d7a2f4965ff77f4ac8f0bac7b554e6b5b4070805.
    
    * addressing feedback

--- source/code-snippets/indexes/compound.js
+++ source/code-snippets/indexes/compound.js
@@ -5,12 +5,15 @@ const { MongoClient } = require("mongodb");
 const uri =
   "mongodb+srv://<user>:<password>@<cluster-url>?writeConcern=majority";
 
+// Create a new client and connect to MongoDB
 const client = new MongoClient(uri);
 
 async function run() {
   try {
     // begin-idx
+    // Connect to the "sample_mflix" database
     const database = client.db("sample_mflix");
+    // Access the database's "movies" collection
     const movies = database.collection("movies");
 
     // Create an ascending index on the "type" and "genre" fields
@@ -20,10 +23,14 @@ async function run() {
     // end-idx
 
     // begin-query
+    // Define a query to find movies in the "Drama" genre
     const query = { type: "movie", genre: "Drama" };
+    // Define sorting criteria for the query results
     const sort = { type: 1, genre: 1 };
+    // Include only the type and genre fields in the query results
     const projection = { _id: 0, type: 1, genre: 1 };
 
+    // Execute the query using the defined criteria and projection
     const cursor = movies
       .find(query)
       .sort(sort)
@@ -31,7 +38,9 @@ async function run() {
     // end-query
 
   } finally {
+    // Close the MongoDB client connection
     await client.close();
   }
 }
+// Run the function and handle any errors
 run().catch(console.dir);
