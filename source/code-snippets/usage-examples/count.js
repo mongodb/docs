482ef66250ffa606bb071c2b4053a3cef4d63174
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2023-08-31 13:10:17 -0400

    DOCSP-32718: count UE code comments (#755)
    * DOCSP-32718: count UE code comments
    
    * CC comments

--- source/code-snippets/usage-examples/count.js
+++ source/code-snippets/usage-examples/count.js
@@ -1,6 +1,8 @@
+// Count documents in a collection
+
 import { MongoClient } from "mongodb";
 
-// Replace the uri string with your MongoDB deployment's connection string.
+// Replace the uri string with your MongoDB deployment's connection string
 const uri = "<connection string uri>";
 
 const client = new MongoClient(uri);
@@ -10,21 +12,20 @@ async function run() {
     const database = client.db("sample_mflix");
     const movies = database.collection("movies");
 
-    // Estimate the total number of documents in the collection
-    // and print out the count.
+    /* Print the estimate of the number of documents in the
+    "movies" collection */
     const estimate = await movies.estimatedDocumentCount();
     console.log(`Estimated number of documents in the movies collection: ${estimate}`);
 
-    // Query for movies from Canada.
+    /* Print the number of documents in the "movies" collection that
+    match the specified query */
     const query = { countries: "Canada" };
-
-    // Find the number of documents that match the specified
-    // query, (i.e. with "Canada" as a value in the "countries" field)
-    // and print out the count.
     const countCanada = await movies.countDocuments(query);
     console.log(`Number of movies from Canada: ${countCanada}`);
   } finally {
+    // Close the connection after the operations complete
     await client.close();
   }
 }
+// Run the program and print any thrown exceptions
 run().catch(console.dir);
