a7c43408316f3de1bdf720c56fdda674bb23c5c1
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2023-08-31 13:15:30 -0400

    DOCSP-32718: deleteone UE code comments (#756)
    * DOCSP-32718: deleteone UE code comments
    
    * CC comments
    
    * CC comments

--- source/code-snippets/usage-examples/deleteOne.js
+++ source/code-snippets/usage-examples/deleteOne.js
@@ -1,6 +1,8 @@
+// Delete a document
+
 import { MongoClient } from "mongodb";
 
-// Replace the uri string with your MongoDB deployment's connection string.
+// Replace the uri string with your MongoDB deployment's connection string
 const uri = "<connection string uri>";
 
 const client = new MongoClient(uri);
@@ -10,17 +12,22 @@ async function run() {
     const database = client.db("sample_mflix");
     const movies = database.collection("movies");
 
-    // Query for a movie that has title "Annie Hall"
+    /* Delete the first document in the "movies" collection that matches
+    the specified query document */
     const query = { title: "Annie Hall" };
-
     const result = await movies.deleteOne(query);
+
+    /* Print a message that indicates whether the operation deleted a
+    document */
     if (result.deletedCount === 1) {
       console.log("Successfully deleted one document.");
     } else {
       console.log("No documents matched the query. Deleted 0 documents.");
     }
   } finally {
+    // Close the connection after the operation completes
     await client.close();
   }
 }
+// Run the program and print any thrown exceptions
 run().catch(console.dir);
