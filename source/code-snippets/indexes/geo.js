0171bc7d78063a86aedc11eb3bcfcf551f802138
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2023-09-01 14:59:50 -0400

    DOCSP-32718: geo index code comments (#766)
    * DOCSP-32718: geo index code comments
    
    * fixes

--- source/code-snippets/indexes/geo.js
+++ source/code-snippets/indexes/geo.js
@@ -1,7 +1,8 @@
+// Create a geospatial index
+
 const { MongoClient } = require("mongodb");
 
-// Replace the following with your MongoDB deployment's connection
-// string.
+// Replace the placeholders with your MongoDB deployment's credentials
 const uri =
   "mongodb+srv://<user>:<password>@<cluster-url>?writeConcern=majority";
 
@@ -13,12 +14,17 @@ async function run() {
     const database = client.db("sample_mflix");
     const movies = database.collection("movies");
 
-    // Create a 2dsphere index on the "location.geo" field in the "theaters" collection.
+    /* Create a 2dsphere index on the "location.geo" field in the
+    "movies" collection */
     const result = await movies.createIndex({ "location.geo": "2dsphere" });
+    
+    // Print the result of the index creation
     console.log(`Index created: ${result}`);
     // end-idx
   } finally {
+    // Close the connection after the operation completes
     await client.close();
   }
 }
+// Run the program and print thrown errors
 run().catch(console.dir);
