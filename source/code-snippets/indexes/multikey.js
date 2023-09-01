e21df549dbbbf5c64282b995de66811d97ea758c
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2023-09-01 15:48:40 -0400

    DOCSP-32718: multikey code comments (#768)
    * DOCSP-32718: multikey code comments
    
    * fixes

--- source/code-snippets/indexes/multikey.js
+++ source/code-snippets/indexes/multikey.js
@@ -1,7 +1,8 @@
+// Create a multikey index
+
 const { MongoClient } = require("mongodb");
 
-// Replace the following with your MongoDB deployment's connection
-// string.
+// Replace the placeholders with your credentials
 const uri =
   "mongodb+srv://<user>:<password>@<cluster-url>?writeConcern=majority";
 
@@ -13,23 +14,27 @@ async function run() {
     const database = client.db("sample_mflix");
     const movies = database.collection("movies");
 
-    // Create a multikey index on the "cast" field
+    // Create a multikey index on the "cast" field in the "movies" collection
     const result = await movies.createIndex({ cast: 1 });
     // end-idx
 
+    // Print the result of creating the index
     console.log(`Index created: ${result}`);
 
     // begin-query
     const query = { cast: "Viola Davis" };
     const projection = { _id: 0, cast: 1 , title: 1 };
 
+    // Perform a find operation with the preceding filter and projection
     const cursor = movies
       .find(query)
       .project(projection);
     // end-query
 
   } finally {
+    // Close the connection after the operation completes
     await client.close();
   }
 }
+// Run the program and print any errors
 run().catch(console.dir);
