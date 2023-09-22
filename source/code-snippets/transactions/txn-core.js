870878fd9bd66b72ed2f159b2cc868131f6c8b5e
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2023-09-22 12:33:37 -0400

    DOCSP-31789: transaction return type (#751)
    * DOCSP-31789: transaction return type v6
    
    * draft
    
    * fix build errors
    
    * draft
    
    * note
    
    * fix
    
    * small fix to use include
    
    * NB tech review
    
    * wip
    
    * CC PR fixes 1 + restructuring
    
    * missed save
    
    * small fix
    
    * list test
    
    * improvements
    
    * reorder bc of merge
    
    * CC PR fixes 3
    
    * save fix
    
    * IA
    
    * add simple examples
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * code comment cleanup
    
    * line highlighting
    
    * vale fixes
    
    * CC PR fixes cont.
    
    * CC suggestions
    
    * line highlighting

--- source/code-snippets/transactions/txn-core.js
+++ source/code-snippets/transactions/txn-core.js
@@ -2,7 +2,8 @@
 
 const { MongoError, MongoClient } = require('mongodb');
 
-// Drop the "customers", "inventory", and "orders" collections from the "testdb" database
+/* Drop the "customers", "inventory", and "orders" collections from the
+"testdb" database */
 async function cleanUp(client) {
   await Promise.all( ['customers', 'inventory', 'orders'].map(async c => {
     try {
@@ -22,8 +23,8 @@ async function setup(client) {
 
     // Insert inventory data for "sunblock" and "beach towel"
     await inventoryColl.insertMany([
-      { name: 'sunblock', sku: 5432, qty: 85 },
-      { name: 'beach towel', sku: 7865, qty: 41 },
+      { item: 'sunblock', item_id: 5432, qty: 85 },
+      { item: 'beach towel', item_id: 7865, qty: 41 },
     ]);
   } catch (e) {
     // Print the exception if one was thrown
@@ -42,14 +43,13 @@ async function queryData() {
     }, client));
 
   } finally {
-    // Close the database connection
+    // Close your connection
     client.close();
   }
 }
 
 // start placeOrder
 async function placeOrder(client, cart, payment) {
-  // Specify readConcern, writeConcern, and readPreference transaction options
   const transactionOptions = {
     readConcern: { level: 'snapshot' },
     writeConcern: { w: 'majority' },
@@ -63,8 +63,8 @@ async function placeOrder(client, cart, payment) {
     session.startTransaction(transactionOptions);
 
     const ordersCollection = client.db('testdb').collection('orders');
-    // Within the session, insert an order that contains information about the
-    // customer, items purchased, and the total payment.
+    /* Within the session, insert an order that contains information about the
+    customer, items purchased, and the total payment */
     const orderResult = await ordersCollection.insertOne(
       {
         customer: payment.customer,
@@ -75,36 +75,27 @@ async function placeOrder(client, cart, payment) {
     );
 
     const inventoryCollection = client.db('testdb').collection('inventory');
-
-    // Within the session, for each item purchased, decrement the purchased quantity in the "inventory" collection.
-    // Cancel the transaction when you have insufficient inventory or if the item SKU does not exist.
-    for (let i=0; i<cart.length; i++) {
-      const item = cart[i];
-
-      // Retrieve the inventory information for the item
-      const checkInventory = await inventoryCollection.findOne(
+    
+    for (const item of order) {  
+      /* Update the inventory for the purchased items. End the
+      transaction if the quantity of an item in the inventory is
+      insufficient to complete the purchase. */
+      const inStock = await inventoryCollection.findOneAndUpdate(
         {
-          sku: item.sku,
-          qty: { $gte: item.qty }
+          item_id: item.item_id,
+          item_id: { $gte: item.qty }
         },
-        { session }
-      )
-      // Throw an exception if the item lacks sufficient quantity or SKU does not exist.
-      if (checkInventory === null) {
-        throw new Error('Insufficient quantity or SKU not found.');
-      }
-
-      // Decrement the inventory of the item by the amount specified in the order.
-      await inventoryCollection.updateOne(
-        { sku: item.sku },
         { $inc: { 'qty': -item.qty }},
         { session }
-      );
+      )
+      if (inStock === null) {
+        throw new Error('Insufficient quantity or item ID not found.');
+      }
     }
 
     const customerCollection = client.db('testdb').collection('customers');
 
-    // Within the session, add the order details to the "orders" array of the customer document.
+    // Within the session, add the order details to the "orders" array of the customer document
     await customerCollection.updateOne(
       { _id: payment.customer },
       { $push:  { orders: orderResult.insertedId }},
@@ -121,16 +112,16 @@ async function placeOrder(client, cart, payment) {
       transaction. Roll back all the updates performed in the transaction.
     */
     if (error instanceof MongoError && error.hasErrorLabel('UnknownTransactionCommitResult')) {
-      // add your logic to retry or handle the error
+      // Add your logic to retry or handle the error
     }
     else if (error instanceof MongoError && error.hasErrorLabel('TransientTransactionError')) {
-      // add your logic to retry or handle the error
+      // Add your logic to retry or handle the error
     } else {
       console.log('An error occured in the transaction, performing a data rollback:' + error);
     }
     await session.abortTransaction();
   } finally {
-    // End the session so that no further calls can be made on it
+    // End the session
     await session.endSession();
   }
 }
@@ -148,13 +139,14 @@ async function run() {
   // Call a method that creates sample inventory data for this example
   await setup(client);
 
-  // Create sample data for a customer's shopping cart that includes "sunblock" and "beach towel" items
+  // Create sample data for a customer's shopping cart
   const cart = [
-    { name: 'sunblock', sku: 5432, qty: 1, price: 5.19 },
-    { name: 'beach towel', sku: 7865, qty: 2, price: 15.99 }
+    { item: 'sunblock', item_id: 5432, qty: 1, price: 5.19 },
+    { item: 'beach towel', item_id: 7865, qty: 2, price: 15.99 }
   ];
 
-  // Create sample data for a customer's payment, calculated from the contents of their cart
+  /* Create sample data for a customer's payment based on the contents
+  of their cart */
   const payment = { customer: 98765, total: 37.17 };
 
   try {
@@ -164,7 +156,7 @@ async function run() {
     // Call a method that removes data from prior runs of this example
     await cleanUp(client);
 
-    // Close the database connection
+    // Close your connection
     await client.close();
   }
 }
