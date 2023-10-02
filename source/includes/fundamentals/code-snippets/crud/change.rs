a1a0c3df3afe33d60ed0dac91e1af7f5ad152caf
Nora Reidy <nora.reidy@mongodb.com>
2023-10-02 10:53:04 -0400

    DOCSP-30541: Change guide tech review (#47)

--- source/includes/fundamentals/code-snippets/crud/change.rs
+++ source/includes/fundamentals/code-snippets/crud/change.rs
@@ -1,5 +1,5 @@
 use bson::Document;
-use mongodb::{ bson::doc, Client, Collection, options::UpdateOptions };
+use mongodb::{bson::doc, options::UpdateOptions, Client, Collection};
 use std::env;
 
 #[tokio::main]
@@ -10,30 +10,31 @@ async fn main() -> mongodb::error::Result<()> {
     let my_coll: Collection<Document> = client.database("db").collection("employees");
 
     // begin-update
-    let update_doc =
-        doc! {
+    let update_doc = doc! {
             "$set": doc!{ "department": "Business Operations",
                           "role": "Analytics Specialist" },
             "$inc": doc!{ "bonus": 500 }
     };
 
-    let res = my_coll.update_many(doc! { "department": "Marketing" }, update_doc, None).await?;
+    let res = my_coll
+        .update_many(doc! { "department": "Marketing" }, update_doc, None)
+        .await?;
     println!("Modified {} document(s)", res.modified_count);
     // end-update
 
     // begin-replace
-    let replace_doc =
-        doc! {
+    let replace_doc = doc! {
         "name": "Susan Lee",
         "role": "Lead Consultant",
         "team_members": vec! [ "Jill Gillison" ]
     };
 
-    let res = my_coll.replace_one(doc! { "_id": 4501 }, replace_doc, None).await?;
+    let res = my_coll
+        .replace_one(doc! { "_id": 4501 }, replace_doc, None)
+        .await?;
     println!(
         "Matched {} document(s)\nModified {} document(s)",
-        res.matched_count,
-        res.modified_count
+        res.matched_count, res.modified_count
     );
     // end-replace
 
