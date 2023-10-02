47ce994c52b1128a2f11e0498deb4296dbc5ef54
Nora Reidy <nora.reidy@mongodb.com>
2023-10-02 12:04:36 -0400

    DOCSP-30546: Aggregation tech review (#48)

--- source/includes/fundamentals/code-snippets/aggregation.rs
+++ source/includes/fundamentals/code-snippets/aggregation.rs
@@ -1,29 +1,29 @@
-use chrono::{ Utc };
-use mongodb::{ Client, Collection };
-use serde::{ Deserialize, Serialize };
+use bson::{doc, DateTime};
+use mongodb::{Client, Collection};
+use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
     let uri: &str = "<connection string>";
 
-    let client: Client = Client::with_uri_str(uri).await?;
+    let client = Client::with_uri_str(uri).await?;
 
     // begin-insert
     let my_coll: Collection<Document> = client.database("db").collection("site_users");
 
     let docs = vec![
-        doc! { "name": "Sonya Mehta", "age": 23, "genre_interests": vec!["fiction", "mystery", "memoir"], "last_active": Utc.with_ymd_and_hms(2019, 5, 13, 0, 0, 0).unwrap() },
-        doc! { "name": "Selena Sun", "age": 45, "genre_interests": vec!["fiction", "literary", "theory"], "last_active": Utc.with_ymd_and_hms(2019, 5, 25, 0, 0, 0).unwrap() },
-        doc! { "name": "Carter Johnson", "age": 56, "genre_interests": vec!["literary", "self help"], "last_active": Utc.with_ymd_and_hms(2019, 5, 31, 0, 0, 0).unwrap() },
-        doc! { "name": "Rick Cortes", "age": 18, "genre_interests": vec!["sci-fi", "fantasy", "memoir"], "last_active": Utc.with_ymd_and_hms(2019, 7, 1, 0, 0, 0).unwrap() },
-        doc! { "name": "Belinda James", "age": 76, "genre_interests": vec!["literary", "nonfiction"], "last_active": Utc.with_ymd_and_hms(2019, 6, 11, 0, 0, 0).unwrap() },
-        doc! { "name": "Corey Saltz", "age": 29, "genre_interests": vec!["fiction", "sports", "memoir"], "last_active": Utc.with_ymd_and_hms(2019, 1, 23, 0, 0, 0).unwrap() },
-        doc! { "name": "John Soo", "age": 16, "genre_interests": vec!["fiction", "sports"], "last_active": Utc.with_ymd_and_hms(2019, 1, 3, 0, 0, 0).unwrap() },
-        doc! { "name": "Lisa Ray", "age": 39, "genre_interests": vec!["poetry", "art", "memoir"], "last_active": Utc.with_ymd_and_hms(2019, 5, 30, 0, 0, 0).unwrap() },
-        doc! { "name": "Kiran Murray", "age": 20, "genre_interests": vec!["mystery", "fantasy", "memoir"], "last_active": Utc.with_ymd_and_hms(2019, 1, 30, 0, 0, 0).unwrap() },
-        doc! { "name": "Beth Carson", "age": 31, "genre_interests": vec!["mystery", "nonfiction"], "last_active": Utc.with_ymd_and_hms(2019, 8, 4, 0, 0, 0).unwrap() },
-        doc! { "name": "Thalia Dorn", "age": 21, "genre_interests": vec!["theory", "literary", "fiction"], "last_active": Utc.with_ymd_and_hms(2019, 8, 19, 0, 0, 0).unwrap() },
-        doc! { "name": "Arthur Ray", "age": 66, "genre_interests": vec!["sci-fi", "fantasy", "fiction"], "last_active": Utc.with_ymd_and_hms(2019, 11, 27, 0, 0, 0).unwrap() }
+        doc! { "name": "Sonya Mehta", "age": 23, "genre_interests": vec!["fiction", "mystery", "memoir"], "last_active": DateTime::builder().year(2019).month(5).day(13).build().unwrap() },
+        doc! { "name": "Selena Sun", "age": 45, "genre_interests": vec!["fiction", "literary", "theory"], "last_active": DateTime::builder().year(2019).month(5).day(25).build().unwrap() },
+        doc! { "name": "Carter Johnson", "age": 56, "genre_interests": vec!["literary", "self help"], "last_active": DateTime::builder().year(2019).month(5).day(31).build().unwrap() },
+        doc! { "name": "Rick Cortes", "age": 18, "genre_interests": vec!["sci-fi", "fantasy", "memoir"], "last_active": DateTime::builder().year(2019).month(7).day(1).build().unwrap() },
+        doc! { "name": "Belinda James", "age": 76, "genre_interests": vec!["literary", "nonfiction"], "last_active": DateTime::builder().year(2019).month(6).day(11).build().unwrap() },
+        doc! { "name": "Corey Saltz", "age": 29, "genre_interests": vec!["fiction", "sports", "memoir"], "last_active": DateTime::builder().year(2019).month(1).day(23).build().unwrap() },
+        doc! { "name": "John Soo", "age": 16, "genre_interests": vec!["fiction", "sports"], "last_active": DateTime::builder().year(2019).month(1).day(3).build().unwrap() },
+        doc! { "name": "Lisa Ray", "age": 39, "genre_interests": vec!["poetry", "art", "memoir"], "last_active": DateTime::builder().year(2019).month(5).day(30).build().unwrap() },
+        doc! { "name": "Kiran Murray", "age": 20, "genre_interests": vec!["mystery", "fantasy", "memoir"], "last_active": DateTime::builder().year(2019).month(1).day(30).build().unwrap() },
+        doc! { "name": "Beth Carson", "age": 31, "genre_interests": vec!["mystery", "nonfiction"], "last_active": DateTime::builder().year(2019).month(8).day(4).build().unwrap() },
+        doc! { "name": "Thalia Dorn", "age": 21, "genre_interests": vec!["theory", "literary", "fiction"], "last_active": DateTime::builder().year(2019).month(8).day(19).build().unwrap() },
+        doc! { "name": "Arthur Ray", "age": 66, "genre_interests": vec!["sci-fi", "fantasy", "fiction"], "last_active": DateTime::builder().year(2019).month(11).day(27).build().unwrap() },
     ];
 
     my_coll.insert_many(docs, None).await?;
@@ -37,7 +37,7 @@ async fn main() -> mongodb::error::Result<()> {
             "avg_age": doc! { "$avg": "$age" },
             "min_age": doc! { "$min": "$age" },
             "max_age": doc! { "$max": "$age" }
-        } }
+        } },
     ];
 
     let mut results = my_coll.aggregate(age_pipeline, None).await?;
@@ -50,9 +50,9 @@ async fn main() -> mongodb::error::Result<()> {
     // begin-lastactive-agg
     let last_active_pipeline = vec![
         doc! { "$project": { "month_last_active" : doc! { "$month" : "$last_active" } } },
-        doc! { "$group": doc! { "_id" : doc! {"month_last_active": "$month_last_active"} , 
-                                "number" : doc! { "$sum" : 1 } } },
-        doc! { "$sort": { "_id.month_last_active" : 1 } }
+        doc! { "$group": doc! { "_id" : doc! {"month_last_active": "$month_last_active"} ,
+        "number" : doc! { "$sum" : 1 } } },
+        doc! { "$sort": { "_id.month_last_active" : 1 } },
     ];
 
     let mut results = my_coll.aggregate(last_active_pipeline, None).await?;
@@ -67,7 +67,7 @@ async fn main() -> mongodb::error::Result<()> {
         doc! { "$unwind" : "$genre_interests" },
         doc! { "$group" : doc! { "_id" : "$genre_interests" , "number" : doc! { "$sum" : 1 } } },
         doc! { "$sort" : doc! { "number" : -1 } },
-        doc! { "$limit": 3 }
+        doc! { "$limit": 3 },
     ];
 
     let mut results = my_coll.aggregate(popularity_pipeline, None).await?;
