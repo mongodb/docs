6521c25503f9aa98a094bc1e36a023030e5cf396
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2023-10-03 13:48:47 -0400

    DOCSP-30505: quickstart tech review IA (#51)

--- source/includes/quick-start/code-snippets/connect-async.rs
+++ source/includes/quick-start/code-snippets/connect-async.rs
@@ -1,21 +1,21 @@
 use mongodb::{ bson::doc, Client, Collection };
-use bson:: Document;
+use bson::Document;
 
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
-   // Replace the placeholder with your Atlas connection string
-   let uri = "<connection string>";
-   
-   // Create a new client and connect to the server
-   let client = Client::with_uri_str(uri).await?;
+    // Replace the placeholder with your Atlas connection string
+    let uri = "<connection string>";
 
-   // Get a handle on the "movies" collection in the "sample_mflix" database
-   let my_coll: Collection<Document> = client.database("sample_mflix").collection("movies");
+    // Create a new client and connect to the server
+    let client = Client::with_uri_str(uri).await?;
 
-   // Find the movie "The Perils of Pauline" in the "movies" collection
-   let my_movie = my_coll.find_one(doc! { "title": "The Perils of Pauline" }, None).await?;
+    // Get a handle on the movies collection
+    let my_coll: Collection<Document> = client.database("sample_mflix").collection("movies");
 
-   // Print the document that contains the movie found
-   println!("{}", serde_json::to_string_pretty(&my_movie).unwrap());
-   Ok(())
+    // Find a movie based on the title value
+    let my_movie = my_coll.find_one(doc! { "title": "The Perils of Pauline" }, None).await?;
+
+    // Print the document
+    println!("Found a movie:\n{:#?}", my_movie);
+    Ok(())
 }
--- source/includes/quick-start/code-snippets/connect-sync.rs
+++ source/includes/quick-start/code-snippets/connect-sync.rs
@@ -1,23 +1,21 @@
-use mongodb::{ bson::doc, sync::{Client} };
-use bson:: Document;
+use mongodb::{ bson::doc, sync::{ Client } };
+use bson::Document;
 
 fn main() -> mongodb::error::Result<()> {
-   // Replace the placeholder with your Atlas connection string
-   let uri = "<connection string>";
-   
-   // Create a new client and connect to the server
-   let client = Client::with_uri_str(uri);
+    // Replace the placeholder with your Atlas connection string
+    let uri = "<connection string>";
 
-   // Get a handle on the "movies" collection in the "sample_mflix" database
-   let database = client?.database("sample_mflix");
-   let my_coll = database.collection::<Document>("movies");
+    // Create a new client and connect to the server
+    let client = Client::with_uri_str(uri)?;
 
-   // Find the movie "The Perils of Pauline" in the "movies" collection
-   let my_movie = my_coll.find_one(doc! { "title": "The Perils of Pauline" }, None)?;
+    // Get a handle on the movies collection
+    let database = client.database("sample_mflix");
+    let my_coll = database.collection::<Document>("movies");
 
-   // Print the document that contains the movie found
-   println!("{}", serde_json::to_string_pretty(&my_movie).unwrap());
-   Ok(())
+    // Find a movie based on the title value
+    let my_movie = my_coll.find_one(doc! { "title": "The Perils of Pauline" }, None)?;
+
+    // Print the document
+    println!("Found a movie:\n{:#?}", my_movie);
+    Ok(())
 }
- 
-
