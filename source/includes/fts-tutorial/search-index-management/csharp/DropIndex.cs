610f11181e5fd9c38416e4ce04cd94425964b861
Mike Woofter <108414937+mongoKart@users.noreply.github.com>
2023-10-03 11:11:18 -0500

    docsp-33048 - delete indexes c# (#4913)

---
+++ source/includes/fts-tutorial/search-index-management/csharp/DropIndex.cs
@@ -0,0 +1,13 @@
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+// connect to your Atlas deployment
+var uri = "<connection-string>";
+
+var client = new MongoClient(uri);
+
+var db = client.GetDatabase("<databaseName>");
+var collection = db.GetCollection<BsonDocument>("<collectionName");
+
+// drop your Atlas Search index
+collection.SearchIndexes.DropOne("<index name>");
