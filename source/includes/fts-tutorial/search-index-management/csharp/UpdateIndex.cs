e810bdfb82cff10dc62f6436075d4ad1e5c0a66e
Mike Woofter <108414937+mongoKart@users.noreply.github.com>
2023-10-03 11:11:34 -0500

    DOCSP-33047 – C# Edit Index Example (#4924)

---
+++ source/includes/fts-tutorial/search-index-management/csharp/UpdateIndex.cs
@@ -0,0 +1,32 @@
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+// connect to your Atlas deployment
+var uri = "<connection-string>";
+
+var client = new MongoClient(uri);
+
+var db = client.GetDatabase("<databaseName>");
+var collection = db.GetCollection<BsonDocument>("<collectionName");
+
+// define your Atlas Search index
+var index = new BsonDocument
+{
+  // updated search index definition
+  { "mappings", new BsonDocument
+    {
+      { "dynamic", false },
+      { "fields", new BsonDocument
+        {
+          { "<field-name>", new BsonDocument
+            {
+              { "type", "<field-type>" }
+            }
+          }
+        }
+      }
+    }
+  }
+};
+
+guitarsCollection.SearchIndexes.Update("<index-name>", index);
