dcb634513e23d97448380a158fcf31c0b00a4dec
Mike Woofter <108414937+mongoKart@users.noreply.github.com>
2023-10-03 11:10:58 -0500

    docsp-33046 - C# list indexes example (#4900)

---
+++ source/includes/fts-tutorial/search-index-management/csharp/ListIndexes.cs
@@ -0,0 +1,17 @@
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+// connect to your Atlas deployment
+var uri = "<connection-string>";
+
+var client = new MongoClient(uri);
+
+var db = client.GetDatabase("<databaseName>");
+var collection = db.GetCollection<BsonDocument>("<collectionName");
+
+// list your Atlas Search indexes
+var result = collection.SearchIndexes.List().ToList();
+foreach (var index in result)
+{
+    Console.WriteLine(index);
+}
