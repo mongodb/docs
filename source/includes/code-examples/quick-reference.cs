2163ea5d37d0202e091421ba23d7395bce917b38
Jordan Smith <jordan.smith@mongodb.com>
2023-10-10 12:34:49 -0700

    spacing

--- source/includes/code-examples/quick-reference.cs
+++ source/includes/code-examples/quick-reference.cs
@@ -148,14 +148,17 @@ void SkipExample()
 internal class PlanetDbContext : DbContext
 {
     public DbSet<Planet> Planets { get; init; }
+
     public static PlanetDbContext Create(IMongoDatabase database) =>
         new(new DbContextOptionsBuilder<PlanetDbContext>()
             .UseMongoDB(database.Client, database.DatabaseNamespace.DatabaseName)
             .Options);
+
     public PlanetDbContext(DbContextOptions options)
         : base(options)
     {
     }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
