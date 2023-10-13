9284a1fdd070f7fe7baddc252b6e741028641cc1
Mike Woofter <108414937+mongoKart@users.noreply.github.com>
2023-10-13 15:51:33 -0500

    add await (#921)

--- source/includes/connection-snippets/scram/py-motor-connection-no-stableapi.py
+++ source/includes/connection-snippets/scram/py-motor-connection-no-stableapi.py
@@ -10,7 +10,7 @@ async def ping_server():
 
   # Send a ping to confirm a successful connection
   try:
-      client.admin.command('ping')
+      await client.admin.command('ping')
       print("Pinged your deployment. You successfully connected to MongoDB!")
   except Exception as e:
       print(e)
--- source/includes/connection-snippets/scram/py-motor-connection.py
+++ source/includes/connection-snippets/scram/py-motor-connection.py
@@ -11,7 +11,7 @@ async def ping_server():
 
   # Send a ping to confirm a successful connection
   try:
-      client.admin.command('ping')
+      await client.admin.command('ping')
       print("Pinged your deployment. You successfully connected to MongoDB!")
   except Exception as e:
       print(e)
