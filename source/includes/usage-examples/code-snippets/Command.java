3b23db45376aa2a2d233c3a870d0cea43de0ed6b
Chris Cho <chris.cho@mongodb.com>
2023-11-02 16:05:13 -0400

    DOCSP-34069: fix imports for run command usage example (#482)
    * DOCSP-34069: fix imports for run command usage example

--- source/includes/usage-examples/code-snippets/Command.java
+++ source/includes/usage-examples/code-snippets/Command.java
@@ -6,12 +6,11 @@ import org.bson.BsonDocument;
 import org.bson.BsonInt64;
 import org.bson.Document;
 
-import com.mongodb.ConnectionString;
-import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoException;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
+import org.bson.conversions.Bson;
 
 
 public class RunCommand {
@@ -28,12 +27,12 @@ public class RunCommand {
 
                 // Retrieves statistics about the specified database
                 Document commandResult = database.runCommand(command);
-                
+
                 // Prints the database statistics
                 System.out.println("dbStats: " + commandResult.toJson());
-                
-            // Prints a message if any exceptions occur during the command execution
+
             } catch (MongoException me) {
+                // Prints a message if any exceptions occur during the command execution
                 System.err.println("An error occurred: " + me);
             }
         }
