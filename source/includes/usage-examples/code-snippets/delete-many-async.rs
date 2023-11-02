a981d27ba5171e74da070fd65c7b8c022d777850
Nora Reidy <nora.reidy@mongodb.com>
2023-11-02 16:19:08 -0400

    DOCSP-30518: Delete Many Usage Example (#67)

---
+++ source/includes/usage-examples/code-snippets/delete-many-async.rs
@@ -0,0 +1,40 @@
+use mongodb::{ 
+    bson::doc,
+    Client,
+    Collection 
+};
+use serde::{ Deserialize, Serialize };
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Restaurant {
+    address: Address,
+    borough: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Address {
+    street: String,
+}
+
+#[tokio::main]
+async fn main() -> mongodb::error::Result<()> {
+    let uri = "<connection string>";
+    let client = Client::with_uri_str(uri).await?;
+
+    let my_coll: Collection<Restaurant> = client
+        .database("sample_restaurants")
+        .collection("restaurants");
+
+    let filter =
+        doc! { "$and": [
+           doc! { "borough": "Manhattan" },
+           doc! { "address.street": "Broadway" }
+       ]
+    };
+
+    let result = my_coll.delete_many(filter, None).await?;
+
+    println!("Deleted documents: {}", result.deleted_count);
+
+    Ok(())
+}
