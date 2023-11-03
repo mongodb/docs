4eca7e19a35a6b912bd2780f5e82232cdc195d84
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2023-11-03 13:55:47 -0400

    DOCSP-30512: insert one usage ex (#75)
    (cherry picked from commit ef51d9aeaafee1b7ea79679d342ebf7030a22291)

---
+++ source/includes/usage-examples/code-snippets/insert-one-async.rs
@@ -0,0 +1,31 @@
+use std::env;
+use mongodb::{ bson::doc, Client, Collection };
+use serde::{ Deserialize, Serialize };
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Restaurant {
+    borough: String,
+    cuisine: String,
+    name: String,
+}
+
+#[tokio::main]
+async fn main() -> mongodb::error::Result<()> {
+    let uri = "<connection string>";
+
+    let client = Client::with_uri_str(uri).await?;
+    let my_coll: Collection<Restaurant> = client
+        .database("sample_restaurants")
+        .collection("restaurants");
+
+    let doc = Restaurant {
+        name: "Sea Stone Tavern".to_string(),
+        cuisine: "Greek".to_string(),
+        borough: "Queens".to_string(),
+    };
+
+    let res = my_coll.insert_one(doc, None).await?;
+    println!("Inserted a document with _id: {}", res.inserted_id);
+
+    Ok(())
+}
