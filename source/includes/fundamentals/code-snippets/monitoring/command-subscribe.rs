09678954b44d14150829f72fe4926c79c5f1a2f8
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2023-11-03 17:31:54 -0400

    DOCSP-30548: monitoring (command, connection pool, cluster topology) (#69)

---
+++ source/includes/fundamentals/code-snippets/monitoring/command-subscribe.rs
@@ -0,0 +1,35 @@
+use std::{ env, sync::Arc };
+
+use bson::Document;
+use mongodb::{
+    Client,
+    Collection,
+    event::command::{ CommandEventHandler, CommandStartedEvent },
+    options::ClientOptions,
+};
+
+fn main() -> mongodb::error::Result<()> {
+    let uri = "<connection string>";
+
+    let mut client_options = ClientOptions::parse(uri).await?;
+
+    // begin-command
+    struct CommandStartHandler;
+
+    impl CommandEventHandler for CommandStartHandler {
+        fn handle_command_started_event(&self, event: CommandStartedEvent) {
+            eprintln!("Command started: {:?}", event);
+        }
+    }
+
+    let handler: Arc<dyn CommandEventHandler> = Arc::new(CommandStartHandler);
+    client_options.command_event_handler = Some(handler);
+
+    let client = Client::with_options(client_options)?;
+
+    // ... perform actions with the client to generate events
+
+    // end-command
+
+    Ok(())
+}
