4f1165e79d41ba6c2ce55f3fed9c4fb0b1c49173
Nora Reidy <nora.reidy@mongodb.com>
2023-11-10 12:12:33 -0500

    DOCSP-34338: Update Quick Start code (#84)

--- source/includes/quick-start/code-snippets/connect-sync.rs
+++ source/includes/quick-start/code-snippets/connect-sync.rs
@@ -1,5 +1,7 @@
-use mongodb::{ bson::doc, sync::{ Client } };
-use bson::Document;
+use mongodb::{ 
+	bson::{Document, doc},
+	sync::{Client, Collection} 
+};
 
 fn main() -> mongodb::error::Result<()> {
     // Replace the placeholder with your Atlas connection string
@@ -10,7 +12,7 @@ fn main() -> mongodb::error::Result<()> {
 
     // Get a handle on the movies collection
     let database = client.database("sample_mflix");
-    let my_coll = database.collection::<Document>("movies");
+    let my_coll: Collection<Document> = database.collection("movies");
 
     // Find a movie based on the title value
     let my_movie = my_coll.find_one(doc! { "title": "The Perils of Pauline" }, None)?;
