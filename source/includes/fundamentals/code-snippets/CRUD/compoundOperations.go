d3f3855c1f6352ba6eb419f834f3c7da2085d3a2
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2023-11-30 11:24:41 -0500

    DOCSP-34174: codewhisperer pt 9 (#328)

--- source/includes/fundamentals/code-snippets/CRUD/compoundOperations.go
+++ source/includes/fundamentals/code-snippets/CRUD/compoundOperations.go
@@ -1,3 +1,4 @@
+// Performs compound operations to find and modify data by using the Go driver
 package main
 
 import (
@@ -55,15 +56,19 @@ func main() {
 
 	fmt.Println("\nFindOneAndDelete:\n")
 	{
+		// Creates a filter to match documents where the "enrollment"
+		// value is less than 20
 		//begin FindOneAndDelete
 		filter := bson.D{{"enrollment", bson.D{{"$lt", 20}}}}
 
+		// Finds and deletes the first matching document in one action
 		var deletedDoc Course
 		err := coll.FindOneAndDelete(context.TODO(), filter).Decode(&deletedDoc)
 		if err != nil {
 			panic(err)
 		}
 
+		// Prints the contents of the deleted document
 		res, _ := bson.MarshalExtJSON(deletedDoc, false, false)
 		fmt.Println(string(res))
 		//end FindOneAndDelete
@@ -71,17 +76,23 @@ func main() {
 
 	fmt.Println("\nFindOneAndUpdate:\n")
 	{
+		// Creates a filter to match documents where the "title"
+		// value includes the string "Modern"
 		//begin FindOneAndUpdate
 		filter := bson.D{{"title", bson.D{{"$regex", "Modern"}}}}
+
+		// Creates instructions to set the "enrollment" field to 32
 		update := bson.D{{"$set", bson.D{{"enrollment", 32}}}}
 		opts := options.FindOneAndUpdate().SetReturnDocument(options.After)
 
+		// Finds and updates the first matching document in one action
 		var updatedDoc Course
 		err := coll.FindOneAndUpdate(context.TODO(), filter, update, opts).Decode(&updatedDoc)
 		if err != nil {
 			panic(err)
 		}
 
+		// Prints the contents of the document after the update
 		res, _ := bson.MarshalExtJSON(updatedDoc, false, false)
 		fmt.Println(string(res))
 		//end FindOneAndUpdate
@@ -89,16 +100,22 @@ func main() {
 
 	fmt.Println("\nFindOneAndReplace:\n")
 	{
+		// Creates a filter to match documents where the "title"
+		// value is "Representation Theory"
 		//begin FindOneAndReplace
 		filter := bson.D{{"title", "Representation Theory"}}
+
+		// Creates a new document to replace the matched document
 		replacement := Course{Title: "Combinatorial Theory", Enrollment: 35}
 
+		// Finds and replaces the first matching document in one action
 		var previousDoc Course
 		err := coll.FindOneAndReplace(context.TODO(), filter, replacement).Decode(&previousDoc)
 		if err != nil {
 			panic(err)
 		}
 
+		// Prints the contents of the document before the replacement
 		res, _ := bson.MarshalExtJSON(previousDoc, false, false)
 		fmt.Println(string(res))
 		//end FindOneAndReplace
