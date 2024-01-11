ec51e2a8dd41b7c4537d886a53060c46e9e9f0cf
Jordan Smith <45415425+jordan-smith721@users.noreply.github.com>
2024-01-11 10:26:36 -0800

    DOCSP-32169 Add configuration page (#11)

---
+++ source/includes/fundamentals/code-examples/configure/ConfigureEFProvider.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Microsoft.Extensions.Configuration;
+using MongoDB.EntityFrameworkCore.Extensions;
+
+var mongoClient = new MongoClient("<Your MongoDB Connection URI>");
+
+var dbContextOptions =
+    new DbContextOptionsBuilder<MyDbContext>().UseMongoDB(mongoClient, "<Database Name>");
+
+var db = new MyDbContext(dbContextOptions.Options);
+
+// Add a new customer and save it to the database
+db.Customers.Add(new Customer() { name = "John Doe", Order = "1 Green Tea" });
+db.SaveChanges();
+
+internal class Customer
+{
+    public ObjectId Id { get; set; }
+    public String Name { get; set; }
+    public String Order { get; set; }
+}
+
+internal class MyDbContext : DbContext
+{
+    public DbSet<Customer> Customers { get; init; }
+
+    public MyDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Customer>().ToCollection("customers");
+    }
+}
