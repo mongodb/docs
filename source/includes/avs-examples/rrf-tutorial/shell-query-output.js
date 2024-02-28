9ad091d1dadafad414387602a195f88a41028880
kanchana-mongodb <54281287+kanchana-mongodb@users.noreply.github.com>
2024-02-28 09:02:44 -0800

    DOCSP-34453 Add example of how to perform reciprocal rank fusion (#5259)
    * DOCSP-34453 Add example of how to perform reciprocal rank fusion
    
    * DOCSP-34453 updates for HW's feedback
    
    * DOCSP-34453 updates for DH's feedback
    
    * DOCSP-34453 updates for HW's feedback
    
    * DOCSP-34453 updates for JU's feedback
    
    * Apply suggestions from code review
    
    Co-authored-by: davidhou17 <55004296+davidhou17@users.noreply.github.com>
    
    * DOCSP-34453 updates for DH's feedback
    
    * Apply suggestions from code review
    
    Co-authored-by: davidhou17 <55004296+davidhou17@users.noreply.github.com>
    
    * DOCSP-34453 updates for HW's feedback
    
    * DOCSP-34453 adds JU's video
    
    ---------
    
    Co-authored-by: davidhou17 <55004296+davidhou17@users.noreply.github.com>

---
+++ source/includes/avs-examples/rrf-tutorial/shell-query-output.js
@@ -0,0 +1,47 @@
+[
+  { _id: 'Pixels', vs_score: 0.5, fts_score: 0, score: 0.5 },
+  {
+    _id: 'Fighting',
+    vs_score: 0.3333333333333333,
+    fts_score: 0,
+    score: 0.3333333333333333
+  },
+  {
+    _id: 'A Most Violent Year',
+    vs_score: 0.25,
+    fts_score: 0,
+    score: 0.25
+  },
+  {
+    _id: 'Mistletoe Over Manhattan',
+    vs_score: 0.2,
+    fts_score: 0,
+    score: 0.2
+  },
+  {
+    _id: 'End of Days',
+    vs_score: 0.16666666666666666,
+    fts_score: 0,
+    score: 0.16666666666666666
+  },
+  { _id: 'Sam', vs_score: 0.125, fts_score: 0, score: 0.125 },
+  {
+    _id: 'Rumble in the Bronx',
+    vs_score: 0.1111111111111111,
+    fts_score: 0,
+    score: 0.1111111111111111
+  },
+  {
+    _id: 'Escape from New York',
+    vs_score: 0.047619047619047616,
+    fts_score: 0.05555555555555555,
+    score: 0.10317460317460317
+  },
+  { _id: 'The Other Guys', vs_score: 0.1, fts_score: 0, score: 0.1 },
+  {
+    _id: 'Sky Captain and the World of Tomorrow',
+    vs_score: 0.09090909090909091,
+    fts_score: 0,
+    score: 0.09090909090909091
+  }
+]
