fdb67ca660528eef53666041abb2bab2fb5692d4
rustagir <rea.rustagi@mongodb.com>
2024-03-12 13:42:47 -0400

    add case insensitive example

---
+++ source/examples/atlas-examples/CaseInsensitiveQuery.kt
@@ -0,0 +1,46 @@
+import com.mongodb.client.model.Aggregates.limit
+import com.mongodb.client.model.Aggregates.project
+import com.mongodb.client.model.Projections.*
+import com.mongodb.kotlin.client.coroutine.MongoClient
+import kotlinx.coroutines.runBlocking
+import org.bson.Document
+
+fun main() {
+    val uri = "<connection-string>"
+    val mongoClient = MongoClient.create(uri)
+    val database = mongoClient.getDatabase("sample_mflix")
+    val collection = database.getCollection<Document>("movies")
+
+    runBlocking {
+        val agg = Document(
+            "\$search",
+            Document("index", "case-insensitive-sort")
+                .append(
+                    "text",
+                    Document("path", "title")
+                        .append("query", "train")
+                )
+                .append(
+                    "sort",
+                    Document("title", 1)
+                )
+        )
+
+        val resultsFlow = collection.aggregate<Document>(
+            listOf(
+                agg,
+                limit(5),
+                project(fields(
+                    excludeId(),
+                    include("title", "awards"),
+                    computed("score", Document("\$meta", "searchScore"))
+                ))
+            )
+        )
+
+        resultsFlow.collect { println(it) }
+    }
+
+    mongoClient.close()
+}
+
