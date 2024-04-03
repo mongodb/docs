9f4a38662c4db143d383010a0d5c52ed274e5d1f
Jordan Smith <45415425+jordan-smith721@users.noreply.github.com>
2024-04-03 09:41:01 -0700

    DOCSP-37511 Indexes (#59)

---
+++ source/includes/indexes/indexes.py
@@ -0,0 +1,79 @@
+# start-index-single
+
+# end-index-single
+
+# start-index-single-query
+
+# end-index-single-query
+
+# start-compound-index
+
+# end-compound-index
+
+# start-index-compound-query
+
+# end-index-compound-query
+
+# start-index-multikey
+
+# end-index-multikey
+
+# start-index-multikey-query
+
+# end-index-multikey-query
+
+# start-index-text-single
+
+# end-index-text-single
+
+# start-index-text-single-query
+
+# end-index-text-single-query
+
+# start-index-text-multi
+
+# end-index-text-multi
+
+# start-index-text-multi-query
+
+# end-index-text-multi-query
+
+# start-index-geo
+
+# end-index-geo
+
+# start-index-geo-query
+
+# end-index-geo-query
+
+# start-index-wildcard
+
+# end-index-wildcard
+
+# start-index-clustered
+
+# end-index-clustered
+
+# start-remove-index
+
+# end-remove-index
+
+# start-create-search-index
+
+# end-create-search-index
+
+# start-create-search-indexes
+
+# end-create-search-indexes
+
+# start-list-search-indexes
+
+# end-list-search-indexes
+
+# start-update-search-indexes
+
+# end-update-search-indexes
+
+# start-delete-search-indexes
+
+# end-delete-search-indexes
