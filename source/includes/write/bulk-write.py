b3c1fa39ca2705e99adef57dc6097934ee988c7e
Jordan Smith <45415425+jordan-smith721@users.noreply.github.com>
2024-04-03 07:52:06 -0700

    DOCSP-37536 Bulk Write Operations (#48)

---
+++ source/includes/write/bulk-write.py
@@ -0,0 +1,31 @@
+# start-bulk-insert-one
+
+# end-bulk-insert-one
+
+# start-bulk-update-one
+
+# end-bulk-update-one
+
+# start-bulk-update-many
+
+# end-bulk-update-many
+
+# start-bulk-replace-one
+
+# end-bulk-replace-one
+
+# start-bulk-delete-one
+
+# end-bulk-delete-one
+
+# start-bulk-delete-many
+
+# end-bulk-delete-many
+
+# start-bulk-write-mixed
+
+# end-bulk-write-mixed
+
+# start-bulk-write-unordered
+
+# end-bulk-write-unordered
