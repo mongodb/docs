14b9726b1cb04254d7ad4abe3a7076ba33496e07
Jordan Smith <45415425+jordan-smith721@users.noreply.github.com>
2024-04-11 10:08:33 -0700

    read and write code examples (#73)

--- source/includes/write/bulk-write.py
+++ source/includes/write/bulk-write.py
@@ -1,31 +1,80 @@
 # start-bulk-insert-one
-
+operation = pymongo.InsertOne(
+    {
+        "name": "Mongo's Deli",
+        "cuisine": "Sandwiches",
+        "borough": "Manhattan",
+        "restaurant_id": "1234"
+    }
+)
 # end-bulk-insert-one
 
 # start-bulk-update-one
-
+operation = pymongo.UpdateOne(
+    { "name": "Mongo's Deli" },
+    { "$set": { "cuisine": "Sandwiches and Salads" }},
+)
 # end-bulk-update-one
 
 # start-bulk-update-many
-
+operation = pymongo.UpdateMany(
+    { "name": "Mongo's Deli" },
+    { "$set": { "cuisine": "Sandwiches and Salads" }},
+)
 # end-bulk-update-many
 
 # start-bulk-replace-one
-
+operation = pymongo.ReplaceOne(
+    { "restaurant_id": "1234" },
+    {
+        "name": "Mongo's Pizza",
+        "cuisine": "Pizza",
+        "borough": "Brooklyn",
+        "restaurant_id": "5678"
+    }
+)
 # end-bulk-replace-one
 
 # start-bulk-delete-one
-
+operation = pymongo.DeleteOne({ "restaurant_id": "5678" })
 # end-bulk-delete-one
 
 # start-bulk-delete-many
-
+operation = pymongo.DeleteMany({ "name": "Mongo's Deli" })
 # end-bulk-delete-many
 
 # start-bulk-write-mixed
+operations = [
+    pymongo.InsertOne(
+        {
+            "name": "Mongo's Deli",
+            "cuisine": "Sandwiches",
+            "borough": "Manhattan",
+            "restaurant_id": "1234"
+        }
+    ),
+    pymongo.InsertOne(
+        {
+            "name": "Mongo's Deli",
+            "cuisine": "Sandwiches",
+            "borough": "Brooklyn",
+            "restaurant_id": "5678"
+        }
+    ),
+    pymongo.UpdateMany(
+        { "name": "Mongo's Deli" },
+        { "$set": { "cuisine": "Sandwiches and Salads" }},
+    ),
+    pymongo.DeleteOne(
+        { "restaurant_id": "1234" }
+    )
+]
 
+results = restaurants.bulk_write(operations)
+
+print(results)
 # end-bulk-write-mixed
 
 # start-bulk-write-unordered
-
+results = restaurants.bulk_write(operations, ordered=False)
 # end-bulk-write-unordered
--- source/includes/write/delete.py
+++ source/includes/write/delete.py
@@ -1,11 +1,17 @@
 # start-delete-one
+query_filter = { "name": "Ready Penny Inn" }
 
+result = restaurants.delete_one(query_filter)
 # end-delete-one
 
 # start-delete-many
+query_filter = { "borough": "Brooklyn" }
 
+result = restaurants.delete_many(query_filter)
 # end-delete-many
 
 # start-delete-options
+query_filter = { 'name': {'$regex': 'Mongo' }}
 
+result = restaurants.delete_many(query_filter, comment="Deleting Mongo restaurants")
 # end-delete-options
