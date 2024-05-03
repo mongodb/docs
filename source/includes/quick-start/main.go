f1dfc7cb6e5c4bc3292c3ea1eb92c6034de57c9b
Nora Reidy <nora.reidy@mongodb.com>
2024-05-03 10:30:35 -0400

    DOCSP-39153: Reduce horizontal scroll in Quick Start (#361)
    * 1.14 Code Fixes (#353)
    
    (cherry picked from commit f1993edd6531e0e00e698a9f81d4cd08b5f64ef1)
    
    * remove context
    
    * DOCSP-39153: Reduce horizontal scroll in Quick Start
    
    * shorten
    
    (cherry picked from commit 43b13a5a6419cd995c8f7d1d96bdc8e0df1b1b6f)

--- source/includes/quick-start/main.go
+++ source/includes/quick-start/main.go
@@ -20,9 +20,12 @@ func main() {
 
 	uri := os.Getenv("MONGODB_URI")
 	if uri == "" {
-		log.Fatal("You must set your 'MONGODB_URI' environment variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
+		log.Fatal("Set your 'MONGODB_URI' environment variable. " +
+			"See: " +
+			"www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
 	}
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	client, err := mongo.Connect(context.TODO(), options.Client().
+		ApplyURI(uri))
 	if err != nil {
 		panic(err)
 	}
@@ -37,7 +40,8 @@ func main() {
 	title := "Back to the Future"
 
 	var result bson.M
-	err = coll.FindOne(context.TODO(), bson.D{{"title", title}}).Decode(&result)
+	err = coll.FindOne(context.TODO(), bson.D{{"title", title}}).
+		Decode(&result)
 	if err == mongo.ErrNoDocuments {
 		fmt.Printf("No document was found with the title %s\n", title)
 		return
