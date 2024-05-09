9fd6dcc23dc54e2f16086186b26102118cfac901
Nora Reidy <nora.reidy@mongodb.com>
2024-05-09 15:43:47 -0400

    DOCSP-34305: Delete search index (Python) (#5221)
    * DOCSP-34305: Delete search index (Python
    
    * small fix
    
    * snooty build

---
+++ source/includes/fts-tutorial/search-index-management/python/delete-index.py
@@ -0,0 +1,13 @@
+from pymongo.mongo_client import MongoClient
+
+def edit_index():
+    # Connect to your Atlas deployment
+    uri = "<connectionString>"
+    client = MongoClient(uri)
+
+    # Access your database and collection
+    database = client["<databaseName>"]
+    collection = database["<collectionName>"]
+
+    # Delete your search index
+    collection.drop_search_index("<indexName>")
