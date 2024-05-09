e2cd43ab8803f2679f49e5f89a182878a1eb06c0
Nora Reidy <nora.reidy@mongodb.com>
2024-05-09 16:27:29 -0400

    DOCSP-34304: Edit search index (Python) (#5216)
    * DOCSP-34304: Edit search index (Python)
    
    * code edits, rewording
    
    * add tab
    
    * snooty build
    
    * AJ feedback

---
+++ source/includes/fts-tutorial/search-index-management/python/edit-index.py
@@ -0,0 +1,19 @@
+from pymongo.mongo_client import MongoClient
+
+def edit_index():
+    # Connect to your Atlas deployment
+    uri = "<connectionString>"
+    client = MongoClient(uri)
+
+    # Access your database and collection
+    database = client["<databaseName>"]
+    collection = database["<collectionName>"]
+
+    definition = {
+            "mappings": {
+                "dynamic": True
+            },
+    }
+    
+    # Update your search index
+    collection.update_search_index("<indexName>", definition)
