4f87a9762b0fbd9293e37e8af71c913eccaaf176
kanchana-mongodb <54281287+kanchana-mongodb@users.noreply.github.com>
2024-06-20 07:15:38 -0700

    DOCSP-38808 Create Customer-facing docs for Exact Vector Search (#5585)
    * DOCSP-38808 Create Customer-facing docs for Exact Vector Search
    
    * DOCSP-38808 updates for WZ's feedback
    
    * DOCSP-38808 updates for HW's feedback
    
    * DOCSP-38808 version fix as requested by WZ
    
    * DOCSP-38808 updates for DH's feedback
    
    * DOCSP-38808 updates for DH's feedback
    
    * Apply suggestions from code review
    
    Co-authored-by: hweller1 <121510832+hweller1@users.noreply.github.com>
    
    * DOCSP-38808 updates for HW's feedback
    
    * DOCSP-38808 remove v1.0.16 support
    
    ---------
    
    Co-authored-by: hweller1 <121510832+hweller1@users.noreply.github.com>

---
+++ source/includes/avs-examples/pipeline-stage-examples/enn-basic-query-nodejs-output.js
@@ -0,0 +1,50 @@
+{
+  "plot":"It is the dawn of World War III. In mid-western America, a group of teenagers bands together to defend their town, and their country, from invading Soviet forces.",
+  "title":"Red Dawn",
+  "score":0.7700583338737488
+}
+{
+  "plot":"A dramatization of the World War II Battle of Iwo Jima.",
+  "title":"Sands of Iwo Jima",
+  "score":0.7581185102462769
+}
+{
+  "plot":"Great Patriotic War, early 1940s. After barely surviving a battle with a mysterious, ghostly-white Tiger tank, Red Army Sergeant Ivan Naydenov becomes obsessed with its destruction.",
+  "title":"White Tiger",
+  "score":0.750884473323822
+}
+{
+  "plot":"As World War Two rages on, the allies are about to push the Nazis out of North Africa. That's when the Nazis turn up the heat, unleashing their secret Weapon - dragons.",
+  "title":"P-51 Dragon Fighter",
+  "score":0.749922513961792
+}
+{
+  "plot":"A private in the latter days of WWII on the German front struggles between his will to survive and what his superiors perceive as a battlefield instinct.",
+  "title":"When Trumpets Fade",
+  "score":0.7498313188552856
+}
+{
+  "plot":"Post World War III futuristic tale of collapsed governments & bankrupt countries heralding a new lawless age.",
+  "title":"Battletruck",
+  "score":0.7497193217277527
+}
+{
+  "plot":"It is post-World War III. War is outlawed. In its place, are matches between large Robots called Robot Jox. These matches take place between two large superpowers over disputed territories....",
+  "title":"Robot Jox",
+  "score":0.7495121955871582
+}
+{
+  "plot":"During World War II, an American destroyer meets a German U-Boat. Both captains are good ones, and the engagement lasts for a considerable time.",
+  "title":"The Enemy Below",
+  "score":0.746050238609314
+}
+{
+  "plot":"Four American soldiers and one Brit fighting in Europe during World War II struggle to return to Allied territory after being separated from U.S. forces during the historic Malmedy Massacre.",
+  "title":"Saints and Soldiers",
+  "score":0.7435222864151001
+}
+{
+  "plot":"Four American soldiers and one Brit fighting in Europe during World War II struggle to return to Allied territory after being separated from U.S. forces during the historic Malmedy Massacre.",
+  "title":"Saints and Soldiers",
+  "score":0.743497371673584
+}
