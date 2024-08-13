4ef65b75456593d752512b537f7d9815660ee3cb
Michael Morisi <michael.morisi@mongodb.com>
2024-08-13 10:48:34 -0400

    Merge pull request #29 from mcmorisi/DOCSP-42457-PascalFollow

--- source/includes/quick-start/Program.cs
+++ source/includes/quick-start/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Driver;
 using MongoDB.EntityFrameworkCore.Extensions;
 
@@ -41,8 +42,15 @@ internal class MflixDbContext : DbContext
 
 internal class Movie
 {
+    [BsonId]
     public ObjectId _id { get; set; }
+    
+    [BsonElement("title")]
     public string Title { get; set; }
+    
+    [BsonElement("rated")]
     public string Rated { get; set; }
+    
+    [BsonElement("plot")]
     public string Plot { get; set; }
 }
