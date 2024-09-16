9f8cccb9f0cf8edce081e05e391e1829978f903f
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2024-09-16 13:32:35 -0400

    DOCSP-43473: oidc (#382)
    * DOCSP-43473: oidc
    
    * vale
    
    * fix
    
    * wip
    
    * log error
    
    * dedent
    
    * emphasis
    
    * fix
    
    * MD tech review 1
    
    (cherry picked from commit 8a67edffaf8670840d0e1ac310736760e63f7011)

---
+++ source/includes/authentication/eks-custom-callback.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	// start-custom-callback
+	eksCallback := func(_ context.Context,
+		_ *options.OIDCArgs) (*options.OIDCCredential, error) {
+		accessToken, err := os.ReadFile(
+			os.Getenv("AWS_WEB_IDENTITY_TOKEN_FILE"))
+		if err != nil {
+			return nil, err
+		}
+		return &options.OIDCCredential{
+			AccessToken: string(accessToken),
+		}, nil
+	}
+	// end-custom-callback
+
+	// start-credential-callback
+	uri := "mongodb://<hostname>:<port>"
+	opts := options.Client().ApplyURI(uri)
+	opts.SetAuth(
+		options.Credential{
+			AuthMechanism:       "MONGODB-OIDC",
+			OIDCMachineCallback: eksCallback,
+		},
+	)
+	client, err := mongo.Connect(opts)
+	if err != nil {
+		panic(err)
+	}
+	// end-credential-callback
+}
---
+++ source/includes/authentication/gke-callback.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+	// start-callback
+	gkeCallback := func(_ context.Context,
+		_ *options.OIDCArgs) (*options.OIDCCredential, error) {
+		accessToken, err := os.ReadFile(
+			"/var/run/secrets/kubernetes.io/serviceaccount/token")
+		if err != nil {
+			return nil, err
+		}
+		return &options.OIDCCredential{
+			AccessToken: string(accessToken),
+		}, nil
+	}
+	// end-callback
+
+	// start-credential-callback
+	uri := "mongodb://<hostname>:<port>"
+	opts := options.Client().ApplyURI(uri)
+	opts.SetAuth(
+		options.Credential{
+			AuthMechanism:       "MONGODB-OIDC",
+			OIDCMachineCallback: gkeCallback,
+		},
+	)
+	client, err := mongo.Connect(opts)
+	if err != nil {
+		panic(err)
+	}
+	// end-credential-callback
+}
