3c74e694fa49e2c91d215da6f953c65c546e917c
Dachary <dachary.carey@mongodb.com>
2024-09-24 08:36:12 -0400

    (DOCSP-42862): Add Local RAG JS examples (#6087)
    * Draft local RAG JS examples
    
    * Minor fixups
    
    * Add details to create index programmatically for Cloud deployments
    
    * Apply review feedback
    
    * Apply review feedback
    
    * Remove Python-specific description
    
    * Apply review suggestions
    
    * Fix the error in the generate-embeddings file
    
    * Fix Atlas connection string references after change to env file
    
    * Consistent syntax highlgihting for connection strings

---
+++ source/includes/avs-examples/local-rag/llm-output.sh
@@ -0,0 +1,45 @@
+Found mistral-7b-openorca.gguf2.Q4_0.gguf at /Users/dachary.carey/.cache/gpt4all/mistral-7b-openorca.gguf2.Q4_0.gguf
+Creating LLModel: {
+  llmOptions: {
+    model_name: 'mistral-7b-openorca.gguf2.Q4_0.gguf',
+    model_path: '/Users/dachary.carey/.cache/gpt4all',
+    library_path: '/Users/dachary.carey/temp/local-rag-mongodb/node_modules/gpt4all/runtimes/darwin/native;/Users/dachary.carey/temp/local-rag-mongodb',
+    device: 'cpu',
+    nCtx: 2048,
+    ngl: 100
+  },
+  modelConfig: {
+    systemPrompt: '<|im_start|>system\n' +
+      'You are MistralOrca, a large language model trained by Alignment Lab AI.\n' +
+      '<|im_end|>',
+    promptTemplate: '<|im_start|>user\n%1<|im_end|>\n<|im_start|>assistant\n%2<|im_end|>\n',
+    order: 'e',
+    md5sum: 'f692417a22405d80573ac10cb0cd6c6a',
+    name: 'Mistral OpenOrca',
+    filename: 'mistral-7b-openorca.gguf2.Q4_0.gguf',
+    filesize: '4108928128',
+    requires: '2.7.1',
+    ramrequired: '8',
+    parameters: '7 billion',
+    quant: 'q4_0',
+    type: 'Mistral',
+    description: '<strong>Strong overall fast chat model</strong><br><ul><li>Fast responses</li><li>Chat based model</li><li>Trained by Mistral AI<li>Finetuned on OpenOrca dataset curated via <a href="https://atlas.nomic.ai/">Nomic Atlas</a><li>Licensed for commercial use</ul>',
+    url: 'https://gpt4all.io/models/gguf/mistral-7b-openorca.gguf2.Q4_0.gguf',
+    path: '/Users/dachary.carey/.cache/gpt4all/mistral-7b-openorca.gguf2.Q4_0.gguf'
+  }
+}
+Output:  Yes, here are a few AirBnB beach houses with links to the listings:
+
+1. Ocean Living! Secluded Secret Beach! Less than 20 steps to the Ocean! - https://www.airbnb.com/rooms/10317142
+2. 2 Bedrooms and a living room in the house. 1 Bathroom. 1 Kitchen. Friendly neighbourhood. Close to sea side and Historical places - https://www.airbnb.com/rooms/10488837
+3. Gorgeous home just off the main rd, with lots of sun and new amenities. Room has own entrance with small deck, close proximity to the beach - https://www.airbnb.com/rooms/11719579
+4. This favourite home offers a huge balcony, lots of space, easy life, all the comfort you need and a fantastic location! The beach is only 3 minutes away. Metro is 2 blocks away (starting august 2016) - https://www.airbnb.com/rooms/12657285
+5. 5 minutes to seaside where you can swim, and 5 minutes to the woods, this two floors single house contains a cultivated garden with fruit trees, two large bedrooms and a big living room with a large sea view - https://www.airbnb.com/rooms/10985735
+
+Source documents:
+
+Summary: Ocean Living! Secluded Secret Beach! Less than 20 steps to the Ocean! This spacious 4 Bedroom and 4 Bath house has all you need for your family or group. Perfect for Family Vacations and executive retreats. We are in a gated beachfront estate, with lots of space for your activities. Link: https://www.airbnb.com/rooms/10317142.
+Summary: There are 2 bedrooms and a living room in the house. 1 Bathroom. 1 Kitchen. Friendly neighbourhood. Close to sea side and Historical places. Link: https://www.airbnb.com/rooms/10488837.
+Summary: This is a gorgeous home just off the main rd, with lots of sun and new amenities. room has own entrance with small deck, close proximity to the beach , bus to the junction , around the corner form all the cafes, bars and restaurants (2 mins). Link: https://www.airbnb.com/rooms/11719579.
+Summary: This favourite home offers a huge balcony, lots of space, easy life, all the comfort you need and a fantastic location! The beach is only 3 minutes away. Metro is 2 blocks away (starting august 2016). Link: https://www.airbnb.com/rooms/12657285.
+Summary: 5 minutes to seaside where you can swim, and 5 minutes to the woods, this two floors single house contains a cultivated garden with fruit trees, two large bedrooms and a big living room with a large sea view. Link: https://www.airbnb.com/rooms/10985735.
---
+++ source/includes/avs-local-rag-set-up-environment-javascript.rst
@@ -0,0 +1,82 @@
+.. procedure:: 
+   :style: normal 
+
+   .. step:: Initialize your Node.js project.
+
+      Run the following commands in your terminal 
+      to create a new directory named ``local-rag-mongodb`` and
+      initialize your project:
+
+      .. code-block:: console
+
+         mkdir local-rag-mongodb
+         cd local-rag-mongodb
+         npm init -y
+
+   .. step:: Install and import dependencies.
+
+      Run the following command:
+
+      .. code-block:: console
+
+         npm install mongodb @xenova/transformers node-gyp gpt4all
+
+   .. step:: Update your ``package.json`` file.
+
+      In your project's ``package.json`` file, specify the 
+      ``type`` field as shown in the following example,
+      and then save the file.
+
+      .. code-block:: json
+         :emphasize-lines: 3
+
+         {
+            "name": "local-rag-mongodb",
+            "type": "module",
+            ...
+         }
+
+   .. step:: Create a ``.env`` file.
+
+      In your project, create a ``.env`` file to store your connection string.
+      
+      .. code-block:: javascript
+
+         ATLAS_CONNECTION_STRING = "<connection-string>"
+      
+      Replace the ``<connection-string>`` placeholder value with your |service|
+      connection string.
+
+      .. tabs::
+
+         .. tab:: Local {+Deployment+}
+            :tabid: local
+
+            If you're using a local |service| {+deployment+}, 
+            your connection string follows this format, replacing
+            ``<port-number>`` with the port for your local {+deployment+}.
+            
+            .. code-block:: javascript
+
+               ATLAS_CONNECTION_STRING = "mongodb://localhost:<port-number>/?directConnection=true";
+
+         .. tab:: Cloud {+Deployment+}
+            :tabid: cloud
+
+            If you're using an |service| {+cluster+}, your connection string
+            follows this format, replacing ``"<connection-string>";``
+            with your |service| {+cluster+}'s |srv| :manual:`connection string 
+            </reference/connection-string/#find-your-mongodb-atlas-connection-string>`:
+            
+            .. code-block:: javascript
+
+               ATLAS_CONNECTION_STRING = "<connection-string>";
+
+            .. note:: 
+
+               Your connection string should use the following format:
+
+               .. code-block::
+
+                  mongodb+srv://<db_username>:<db_password>@<clusterName>.<hostname>.mongodb.net
+                  
