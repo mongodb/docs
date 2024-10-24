a328163723fe71b9dfaa1bf42790238ce65cbd28
cbullinger <115956901+cbullinger@users.noreply.github.com>
2024-10-24 15:31:14 -0400

    DOCSP-42859: Add Java examples for creating a Vector Search index (#6267)
    * Add Java example to Create Vector Search Index page
    
    * Add Java example to Create Vector Search Index page
    
    * Update java examples for consistency with other PRs
    
    * Fix malformed links
    
    * Update to use dachary's version of the polling messages
    
    * Delete unused duplicated includes
    
    * Apply suggestions from code review
    
    Co-authored-by: davidhou17 <55004296+davidhou17@users.noreply.github.com>
    
    * Add missing imports
    
    * Apply tech review feedback
    
    ---------
    
    Co-authored-by: davidhou17 <55004296+davidhou17@users.noreply.github.com>

---
+++ source/includes/avs-examples/index-management/create-index/basic-example.java
@@ -0,0 +1,84 @@
+import com.mongodb.client.ListSearchIndexesIterable;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.SearchIndexModel;
+import com.mongodb.client.model.SearchIndexType;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.util.Collections;
+import java.util.List;
+
+public class VectorIndex {
+
+    public static void main(String[] args) {
+
+        // Replace the placeholder with your Atlas connection string
+        String uri = "<connectionString>";
+
+        // Connect to your Atlas cluster
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+
+            // Set the namespace
+            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+            MongoCollection<Document> collection = database.getCollection("embedded_movies");
+
+            // Define the index details
+            String indexName = "vector_index";
+            Bson definition = new Document(
+                "fields",
+                Collections.singletonList(
+                    new Document("type", "vector")
+                        .append("path", "plot_embedding")
+                        .append("numDimensions", 1536)
+                        .append("similarity", "euclidean")));
+
+            // Define the index model
+            SearchIndexModel indexModel = new SearchIndexModel(
+                indexName,
+                definition,
+                SearchIndexType.vectorSearch());
+
+            // Create the index
+            try {
+                List<String> result = collection.createSearchIndexes(Collections.singletonList(indexModel));
+                System.out.println("Successfully created a vector index named: " + result);
+                System.out.println("It may take up to a minute for the index to build before you can query using it.");
+            } catch (Exception e) {
+                throw new RuntimeException("Error creating index: " + e);
+            }
+
+            // Wait for Atlas to build the index
+            System.out.println("Polling to confirm the index has completed building.");
+
+            ListSearchIndexesIterable<Document> searchIndexes = collection.listSearchIndexes();
+            Document doc = null;
+            while (doc == null) {
+                try (MongoCursor<Document> cursor = searchIndexes.iterator()) {
+                    if (!cursor.hasNext()) {
+                        break;
+                    }
+                    Document current = cursor.next();
+                    String name = current.getString("name");
+                    // When the index completes building, it becomes `queryable`
+                    boolean queryable = current.getBoolean("queryable");
+                    if (name.equals(indexName) && queryable) {
+                        doc = current;
+                    } else {
+                        Thread.sleep(500);
+                    }
+                } catch (Exception e) {
+                    throw new RuntimeException("Failed to list search indexes: " + e);
+                    mongoClient.close();
+                }
+            }
+            System.out.println(indexName + " index is ready to query");
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error connecting to MongoDB: " + e);
+        }
+    }
+}
