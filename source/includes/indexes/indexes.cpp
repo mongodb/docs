2b832f66c6a0e55e412ac4856cabc7c8178d58b5
Elyse Foreman <elyse.foreman@mongodb.com>
2024-10-29 13:38:50 -0400

    (DOCSP-40807) Indexes > Compound indexes (#63)
    * (DOCSP-40807) Add compound indexes to work-with-indexes page and code examples page.
    
    * (DOCSP-40807) Fix toctree error.
    
    * (DOCSP-40807) Fixing toctree error and deleting copies of example code.
    
    * (DOCSP-40807) Fixes.
    
    * (DOCSP-40807) Fixes.
    
    * (DOCSP_40807) Migrating single index section to its own page.
    
    * (DOCSP-40807) edits.
    
    * (DOCSP-40807) Added pages to toctree
    
    * (DOCSP-40807) Added pages to toctree
    
    * (DOCSP-40807) Added to compound index page.
    ggp
    
    * (DOCSP-40807) Edits.
    
    * (DOCSP-40807) Edits for indexes landing page.
    
    * (DOCSP-40807) Add output to code examples.
    
    * (DOCSP-40807) Dedent all code examples
    
    * (DOCSP-40807) Edited output for compound index.
    ggp
    
    * (DOCSP-40807) Changed introduction to remove index section.
    
    * (Docsp-40807) @norareidy Introducing lists with a full sentence.
    
    * (Docsp-40807) @norareidy No articles when starting list.
    
    * (DOCSP-40807) @norareidy Review changes.
    
    * (DOCSP-40807) Fix reference.
    
    * (DOCSP-40807) Reduce wordiness.
    
    * (DOCSP-40807) @norareidy Review changes.
    
    * (DOCSP-40807) @kevinAlbs Correcting programming language in code examples.
    
    ---------

--- source/includes/indexes/indexes.cpp
+++ source/includes/indexes/indexes.cpp
@@ -1,16 +1,69 @@
-// start-index-single
-auto index_specification = make_document(kvp("title", 1));
-collection.create_index(index_specification.view());
-// end-index-single
+#include <iostream>
 
-// start-remove-index
-collection.indexes().drop_one("title_1");
-// end-remove-index
+#include <bsoncxx/builder/basic/document.hpp>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/uri.hpp>
 
-// start-remove-all-indexes
-collection.indexes().drop_all();
-// end-remove-all-indexes
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_document;
 
-// start-remove-all-wildcard
-collection.indexes().drop_one("*");
-// end-remove-all-wildcard
+
+int main(){
+
+    mongocxx::instance instance;
+    mongocxx::uri uri("<connection string>");
+    mongocxx::client client(uri);
+
+    // start-db-coll
+    auto db = client["sample_mflix"];
+    auto collection = db["movies"];
+    // end-db-coll
+
+    {
+        // start-index-single
+        auto index_specification = make_document(kvp("title", 1));
+        auto result = collection.create_index(index_specification.view());
+        // end-index-single
+    }
+    {
+        // start-index-single-query
+        auto document = collection.find_one(make_document(kvp("title","Peter Pan")));
+        std::cout << bsoncxx::to_json(*document) << std::endl;
+        // end-index-single-query 
+    }
+
+    {
+        // start-index-compound
+        auto index_specification = make_document(kvp("title", 1), kvp("year", 1));
+        auto result = collection.create_index(index_specification.view());
+        // end-index-compound
+    }
+
+    {
+        // start-index-compound-query
+        auto document = collection.find_one(make_document(kvp("title","Peter Pan"), kvp("year", 1924)));
+        std::cout << bsoncxx::to_json(*document) << std::endl;
+        // end-index-compound-query 
+    }
+
+    {
+        // start-remove-index
+        collection.indexes().drop_one("title_1");
+        // end-remove-index
+    }
+
+    {
+        // start-remove-all-indexes
+        collection.indexes().drop_all();
+        // end-remove-all-indexes
+    }
+    
+    {
+        // start-remove-all-wildcard
+        collection.indexes().drop_one("*");
+        // end-remove-all-wildcard    
+    }
+
+}
