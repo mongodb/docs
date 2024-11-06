a728f4d67beae904b0d7c11a80526e5365c048f3
Nora Reidy <nora.reidy@mongodb.com>
2024-11-06 10:30:18 -0500

    DOCSP-44963: Data modeling in find usage examples (#137)
    (cherry picked from commit 592712dea5e9a75a5783c7e6b9f0f8b471157add)

--- source/includes/usage-examples/code-snippets/find-one-async.rs
+++ source/includes/usage-examples/code-snippets/find-one-async.rs
@@ -16,7 +16,8 @@ async fn main() -> mongodb::error::Result<()> {
     let uri = "<connection string>";
     let client = Client::with_uri_str(uri).await?;
 
-    let my_coll: Collection<Restaurant> = client
+    // Replace <T> with the <Document> or <Restaurant> type parameter
+    let my_coll: Collection<T> = client
         .database("sample_restaurants")
         .collection("restaurants");
 
