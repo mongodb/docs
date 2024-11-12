428549baf6bc673979e96db2a027382bc140f349
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2024-11-12 17:55:36 -0500

    Update build.sh to master

--- build.sh
+++ build.sh
@@ -3,6 +3,10 @@ if [ -f build-site.sh ]; then
   rm build-site.sh
 fi 
 
-
+if [ -d "source" ]; then
+  rm -rf source
+fi
+git submodule update --init --recursive
+mkdir source && cp -r laravel-mongodb/docs/* source
 curl https://raw.githubusercontent.com/mongodb/docs-worker-pool/netlify-poc/scripts/build-site.sh -o build-site.sh 
 sh build-site.sh
