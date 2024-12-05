6d8f6ce00091cba01b265566ef136489b40bb097
Nora Reidy <nora.reidy@mongodb.com>
2024-12-05 10:04:46 -0500

    DOCSP-44969: Modify insert usage examples (#154)

--- source/includes/usage-examples/code-snippets/insert-one-async.rs
+++ source/includes/usage-examples/code-snippets/insert-one-async.rs
@@ -1,5 +1,9 @@
 use std::env;
-use mongodb::{ bson::doc, Client, Collection };
+use mongodb::{ 
+    bson::{doc, Document},
+    Client,
+    Collection 
+};
 use serde::{ Deserialize, Serialize };
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -14,17 +18,25 @@ async fn main() -> mongodb::error::Result<()> {
     let uri = "<connection string>";
 
     let client = Client::with_uri_str(uri).await?;
-    let my_coll: Collection<Restaurant> = client
+
+    // Replace <T> with the <Document> or <Restaurant> type parameter
+    let my_coll: Collection<T> = client
         .database("sample_restaurants")
         .collection("restaurants");
 
-    let doc = Restaurant {
+    let insert_doc = doc! { 
+        "name": "Sea Stone Tavern",
+        "cuisine": "Greek",
+        "borough": "Queens",
+    };
+    let insert_struct = Restaurant {
         name: "Sea Stone Tavern".to_string(),
         cuisine: "Greek".to_string(),
         borough: "Queens".to_string(),
     };
 
-    let res = my_coll.insert_one(doc).await?;
+    // Replace <struct or doc> with the insert_struct or insert_doc variable
+    let res = my_coll.insert_one(<struct or doc>).await?;
     println!("Inserted a document with _id: {}", res.inserted_id);
 
     Ok(())
