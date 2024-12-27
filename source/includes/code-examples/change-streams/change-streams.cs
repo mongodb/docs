1121be7928bac97f4fcba9183f843441757b637d
Nora Reidy <nora.reidy@mongodb.com>
2024-12-27 11:14:13 -0500

    DOCSP-34008: Split large change events (#374)
    * DOCSP-34008: Split large change events
    
    * fixes
    
    * edits
    
    * fixes to async code
    
    * fixes to sync code
    
    * wording
    
    * admonition
    
    * small fix
    
    * MM feedback
    
    * FP feedback
    
    * tech feedback
    
    * edits
    
    * final tech feedback
    
    (cherry picked from commit e721d24b9091621ad943e208462b8fe053916b3f)

--- source/includes/code-examples/change-streams/change-streams.cs
+++ source/includes/code-examples/change-streams/change-streams.cs
@@ -42,7 +42,7 @@ var pipeline = new EmptyPipelineDefinition<ChangeStreamDocument<Restaurant>>()
             .Match(change => change.OperationType == ChangeStreamOperationType.Update);
 
 // Opens a change stream and prints the changes as they're received
-using (var cursor = await _restaurantsCollection.WatchAsync(pipeline))
+using (var cursor = await collection.WatchAsync(pipeline))
 {
     await cursor.ForEachAsync(change =>
     {
@@ -56,7 +56,7 @@ var pipeline = new EmptyPipelineDefinition<ChangeStreamDocument<Restaurant>>()
             .Match(change => change.OperationType == ChangeStreamOperationType.Update);
 
 // Opens a change streams and print the changes as they're received
-using (var cursor = _restaurantsCollection.Watch(pipeline))
+using (var cursor = collection.Watch(pipeline))
 {
     foreach (var change in cursor.ToEnumerable())
     {
@@ -64,6 +64,115 @@ using (var cursor = _restaurantsCollection.Watch(pipeline))
     }
 }
 // end-change-stream-pipeline
+
+// start-split-event-helpers-sync
+// Fetches the next complete change stream event
+private static IEnumerable<ChangeStreamDocument<TDocument>> GetNextChangeStreamEvent<TDocument>(
+IEnumerator<ChangeStreamDocument<TDocument>> changeStreamEnumerator)
+{
+    while (changeStreamEnumerator.MoveNext())
+    {
+        var changeStreamEvent = changeStreamEnumerator.Current;
+        if (changeStreamEvent.SplitEvent != null)
+        {
+            var fragment = changeStreamEvent;
+            while (fragment.SplitEvent.Fragment < fragment.SplitEvent.Of)
+            {
+                changeStreamEnumerator.MoveNext();
+                fragment = changeStreamEnumerator.Current;
+                MergeFragment(changeStreamEvent, fragment);
+            }
+        }
+        yield return changeStreamEvent;
+    }
+}
+
+// Merges a fragment into the base event
+private static void MergeFragment<TDocument>(
+    ChangeStreamDocument<TDocument> changeStreamEvent,
+    ChangeStreamDocument<TDocument> fragment)
+{
+    foreach (var element in fragment.BackingDocument)
+    {
+        if (element.Name != "_id" && element.Name != "splitEvent")
+        {
+            changeStreamEvent.BackingDocument[element.Name] = element.Value; 
+        }
+    }
+}
+// end-split-event-helpers-sync
+
+// start-split-event-helpers-async
+// Fetches the next complete change stream event
+private static async IAsyncEnumerable<ChangeStreamDocument<TDocument>> GetNextChangeStreamEventAsync<TDocument>(
+    IAsyncCursor<ChangeStreamDocument<TDocument>> changeStreamCursor)
+{
+    var changeStreamEnumerator = GetNextChangeStreamEventFragmentAsync(changeStreamCursor).GetAsyncEnumerator();
+    while (await changeStreamEnumerator.MoveNextAsync())
+    {
+        var changeStreamEvent = changeStreamEnumerator.Current;
+        if (changeStreamEvent.SplitEvent != null)
+        {
+            var fragment = changeStreamEvent;
+            while (fragment.SplitEvent.Fragment < fragment.SplitEvent.Of)
+            {
+                await changeStreamEnumerator.MoveNextAsync();
+                fragment = changeStreamEnumerator.Current;
+                MergeFragment(changeStreamEvent, fragment);
+            }
+        }
+        yield return changeStreamEvent;
+    }
+}
+
+private static async IAsyncEnumerable<ChangeStreamDocument<TDocument>> GetNextChangeStreamEventFragmentAsync<TDocument>(
+    IAsyncCursor<ChangeStreamDocument<TDocument>> changeStreamCursor)
+{
+    while (await changeStreamCursor.MoveNextAsync())
+    {
+        foreach (var changeStreamEvent in changeStreamCursor.Current)
+        {
+            yield return changeStreamEvent;
+        }
+    }
+}
+
+// Merges a fragment into the base event
+private static void MergeFragment<TDocument>(
+    ChangeStreamDocument<TDocument> changeStreamEvent,
+    ChangeStreamDocument<TDocument> fragment)
+{
+    foreach (var element in fragment.BackingDocument)
+    {
+        if (element.Name != "_id" && element.Name != "splitEvent")
+        {
+            changeStreamEvent.BackingDocument[element.Name] = element.Value;
+        }
+    }
+}
+// end-split-event-helpers-async
+
+// start-split-change-event-sync
+var pipeline = new EmptyPipelineDefinition<ChangeStreamDocument<Restaurant>>()
+    .ChangeStreamSplitLargeEvent();
+
+using var cursor = collection.Watch(pipeline);
+foreach (var completeEvent in GetNextChangeStreamEvent(cursor.ToEnumerable().GetEnumerator()))
+{
+    Console.WriteLine("Received the following change: " + completeEvent.BackingDocument);
+}
+// end-split-change-event-sync
+
+// start-split-change-event-async
+var pipeline = new EmptyPipelineDefinition<ChangeStreamDocument<Restaurant>>()
+    .ChangeStreamSplitLargeEvent();
+
+using var cursor = await collection.WatchAsync(pipeline);
+await foreach (var completeEvent in GetNextChangeStreamEventAsync(cursor))
+{
+    Console.WriteLine("Received the following change: " + completeEvent.BackingDocument);
+}
+// end-split-change-event-async
 
 // start-change-stream-post-image
 var pipeline = new EmptyPipelineDefinition<ChangeStreamDocument<Restaurant>>()
@@ -74,7 +183,7 @@ var options = new ChangeStreamOptions
     FullDocument = ChangeStreamFullDocumentOption.UpdateLookup,
 };
 
-using (var cursor = _restaurantsCollection.Watch(pipeline, options))
+using (var cursor = collection.Watch(pipeline, options))
 {
     foreach (var change in cursor.ToEnumerable())
     {
@@ -92,7 +201,7 @@ var options = new ChangeStreamOptions
     FullDocument = ChangeStreamFullDocumentOption.UpdateLookup,
 };
 
-using var cursor = await _restaurantsCollection.WatchAsync(pipeline, options);
+using var cursor = await collection.WatchAsync(pipeline, options);
 await cursor.ForEachAsync(change =>
 {
     Console.WriteLine(change.FullDocument.ToBsonDocument());
