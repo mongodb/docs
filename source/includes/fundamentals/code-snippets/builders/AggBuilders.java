4929b3c2ca62503babb89741b9980b2dd1c14ee5
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2025-01-13 11:20:23 -0500

    DOCSP-45096: binaryvector type for avs (#608)
    * DOCSP-45096: binaryvector type for avs
    
    * wip
    
    * cross link
    
    * RM PR fixes 1
    
    * add api links

--- source/includes/fundamentals/code-snippets/builders/AggBuilders.java
+++ source/includes/fundamentals/code-snippets/builders/AggBuilders.java
@@ -314,12 +314,24 @@ public class AggBuilders {
     private void vectorSearchPipeline() {
 
         // begin vectorSearch
-        List<Double> queryVector = (asList(-0.0072121937, -0.030757688, -0.012945653));
-        String indexName = "mflix_movies_embedding_index";
-        FieldSearchPath fieldSearchPath = fieldPath("plot_embedding");
-        int limit = 1;
-        VectorSearchOptions options = exactVectorSearchOptions().filter(gte("year", 2016));
+        // Create an instance of the BinaryVector class as the query vector
+        BinaryVector queryVector = BinaryVector.floatVector(
+            new float[]{0.0001f, 1.12345f, 2.23456f, 3.34567f, 4.45678f});
 
+        // Specify the index name for the vector embedding index
+        String indexName = "mflix_movies_embedding_index";
+
+        // Specify the path of the field to search on
+        FieldSearchPath fieldSearchPath = fieldPath("plot_embedding");
+
+        // Limit the number of matches to 1
+        int limit = 1;
+
+        // Create a pre-filter to only search within a subset of documents
+        VectorSearchOptions options = exactVectorSearchOptions()
+            .filter(gte("year", 2016));
+
+        // Create the vectorSearch pipeline stage
         List<Bson> pipeline = asList(
                 vectorSearch(
                         fieldSearchPath,
