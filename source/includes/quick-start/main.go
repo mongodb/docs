ce7d83ddcec2847cc6be82e2aa05084a7eef5567
Lindsey Moore <lindsey.moore@mongodb.com>
2025-01-23 15:09:08 -0500

    DOCSP-46748 Update Quick Start Imports to v2
    (cherry picked from commit d71da56593b5b797e4d03b3ad44fa8943311f9db)

--- source/includes/quick-start/main.go
+++ source/includes/quick-start/main.go
@@ -8,9 +8,9 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/bson"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 func main() {
