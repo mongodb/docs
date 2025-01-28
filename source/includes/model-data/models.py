5765e26006feb1f77d21c7c91ff61f68d61922de
Nora Reidy <nora.reidy@mongodb.com>
2025-01-28 10:34:43 -0500

    DOCSP-46325: Models (#9)
    * DOCSP-46325: Models
    
    * edits
    
    * more info
    
    * edit
    
    * fixes
    
    * toc
    
    * snooty
    
    * remove unsupported fields
    
    * jib feedback
    
    * meta options
    
    * RM feedback
    
    * SR feedback

---
+++ source/includes/model-data/models.py
@@ -0,0 +1,85 @@
+# start-models
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    plot = models.TextField(blank=True)
+    runtime = models.IntegerField(default=0)
+    released = models.DateTimeField("release date", null=True, blank=True)
+
+    class Meta:
+        db_table = "movies"
+        managed = False
+    
+    def __str__(self):
+        return self.title
+# end-models
+
+# start-json-field
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    plot = models.TextField(blank=True)
+    runtime = models.IntegerField(default=0)
+    released = models.DateTimeField("release date", null=True, blank=True)
+    imdb = models.JSONField(null=True, blank=True)
+
+    class Meta:
+        db_table = "movies"
+        managed = False
+    
+    def __str__(self):
+        return self.title
+# end-json-field
+
+# start-array-field
+from django.db import models
+from django_mongodb_backend.fields import ArrayField
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    plot = models.TextField(blank=True)
+    runtime = models.IntegerField(default=0)
+    released = models.DateTimeField("release date", null=True, blank=True)
+    genres = ArrayField(
+             models.CharField(max_length=100),
+             size=5,
+             null=True,
+             blank=True)
+
+    class Meta:
+        db_table = "movies"
+        managed = False
+    
+    def __str__(self):
+        return self.title
+# end-array-field
+
+# start-embedded-field
+from django.db import models
+from django_mongodb_backend.models import EmbeddedModel
+from django_mongodb_backend.fields import EmbeddedModelField
+
+class Award(EmbeddedModel):
+    wins = models.IntegerField(default=0)
+    nominations = models.IntegerField(default=0)
+    text = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    plot = models.TextField(blank=True)
+    runtime = models.IntegerField(default=0)
+    released = models.DateTimeField("release date", null=True, blank=True)
+    awards = EmbeddedModelField(Award, null=True, blank=True)
+
+    class Meta:
+        db_table = "movies"
+        managed = False
+    
+    def __str__(self):
+        return self.title
+# end-embedded-field
