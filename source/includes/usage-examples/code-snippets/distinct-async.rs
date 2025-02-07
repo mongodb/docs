c2614e66f63ab58ed29c03e771abf9e48494aabc
Nora Reidy <nora.reidy@mongodb.com>
2025-02-07 11:56:12 -0500

    DOCSP-44971: Count & distinct usage ex updates (#163)

--- source/includes/usage-examples/code-snippets/distinct-async.rs
+++ source/includes/usage-examples/code-snippets/distinct-async.rs
@@ -3,13 +3,22 @@ use mongodb::{
     bson::{ Document, doc },
     Client,
     Collection };
+use serde::{ Deserialize, Serialize };
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Restaurant {
+    name: String,
+    cuisine: String,
+    borough: String,
+}
 
 #[tokio::main]
 async fn main() -> mongodb::error::Result<()> {
     let uri = "<connection string>";
-
     let client = Client::with_uri_str(uri).await?;
-    let my_coll: Collection<Document> = client
+
+    // Replace <T> with the <Document> or <Restaurant> type parameter
+    let my_coll: Collection<T> = client
         .database("sample_restaurants")
         .collection("restaurants");
 
