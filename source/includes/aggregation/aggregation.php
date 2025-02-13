bc9c0e295cad6a0203721d7dffdc0ba6b2e7173d
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2025-02-13 15:48:36 -0500

    DOCSP-43919: aggregation builder (#203)
    * DOCSP-43919: aggregation builder
    
    * indentation fix
    
    * remov Pipeline type
    
    * LM PR fixes 1
    
    * fp fixes
    
    * todo fixes
    
    * small fixes
    
    * JM tech review 1

--- source/includes/aggregation/aggregation.php
+++ source/includes/aggregation/aggregation.php
@@ -1,4 +1,5 @@
 <?php
+
 require 'vendor/autoload.php';
 
 $uri = getenv('MONGODB_URI') ?: throw new RuntimeException('Set the MONGODB_URI variable to your Atlas URI that connects to the sample dataset');
@@ -8,7 +9,7 @@ $collection = $client->sample_restaurants->restaurants;
 
 // Retrieves documents with a cuisine value of "Bakery", groups them by "borough", and
 // counts each borough's matching documents
-// start-match-group
+// start-array-match-group
 $pipeline = [
     ['$match' => ['cuisine' => 'Bakery']],
     ['$group' => ['_id' => '$borough', 'count' => ['$sum' => 1]]],
@@ -19,10 +20,10 @@ $cursor = $collection->aggregate($pipeline);
 foreach ($cursor as $doc) {
     echo json_encode($doc), PHP_EOL;
 }
-// end-match-group
+// end-array-match-group
 
 // Performs the same aggregation operation as above but asks MongoDB to explain it
-// start-explain
+// start-array-explain
 $pipeline = [
     ['$match' => ['cuisine' => 'Bakery']],
     ['$group' => ['_id' => '$borough', 'count' => ['$sum' => 1]]],
@@ -36,5 +37,79 @@ $aggregate = new MongoDB\Operation\Aggregate(
 
 $result = $collection->explain($aggregate);
 echo json_encode($result), PHP_EOL;
-// end-explain
+// end-array-explain
 
+// start-builder-match-group
+$pipeline = [
+    MongoDB\Builder\Stage::match(
+        date: [
+            MongoDB\Builder\Query::gte(new MongoDB\BSON\UTCDateTime(new DateTimeImmutable('2014-01-01'))),
+            MongoDB\Builder\Query::lt(new MongoDB\BSON\UTCDateTime(new DateTimeImmutable('2015-01-01'))),
+        ],
+    ),
+    MongoDB\Builder\Stage::group(
+        _id: MongoDB\Builder\Expression::dateToString(MongoDB\Builder\Expression::dateFieldPath('date'), '%Y-%m-%d'),
+        totalSaleAmount: MongoDB\Builder\Accumulator::sum(
+            MongoDB\Builder\Expression::multiply(
+                MongoDB\Builder\Expression::numberFieldPath('price'),
+                MongoDB\Builder\Expression::numberFieldPath('quantity'),
+            ),
+        ),
+        averageQuantity: MongoDB\Builder\Accumulator::avg(
+            MongoDB\Builder\Expression::numberFieldPath('quantity'),
+        ),
+        count: MongoDB\Builder\Accumulator::sum(1),
+    ),
+    MongoDB\Builder\Stage::sort(
+        totalSaleAmount: MongoDB\Builder\Type\Sort::Desc,
+    ),
+];
+
+$cursor = $collection->aggregate($pipeline);
+
+foreach ($cursor as $doc) {
+    echo json_encode($doc), PHP_EOL;
+}
+// end-builder-match-group
+
+// start-builder-unwind
+$pipeline = [
+    MongoDB\Builder\Stage::unwind(MongoDB\Builder\Expression::arrayFieldPath('items')),
+    MongoDB\Builder\Stage::unwind(MongoDB\Builder\Expression::arrayFieldPath('items.tags')),
+    MongoDB\Builder\Stage::group(
+        _id: MongoDB\Builder\Expression::fieldPath('items.tags'),
+        totalSalesAmount: MongoDB\Builder\Accumulator::sum(
+            MongoDB\Builder\Expression::multiply(
+                MongoDB\Builder\Expression::numberFieldPath('items.price'),
+                MongoDB\Builder\Expression::numberFieldPath('items.quantity'),
+            ),
+        ),
+    ),
+];
+
+$cursor = $collection->aggregate($pipeline);
+
+foreach ($cursor as $doc) {
+    echo json_encode($doc), PHP_EOL;
+}
+// end-builder-unwind
+
+$collection = $client->db->orders;
+
+// start-builder-lookup
+$pipeline = [
+    MongoDB\Builder\Stage::lookup(
+        from: 'inventory',
+        localField: 'item',
+        foreignField: 'sku',
+        as: 'inventory_docs',
+    ),
+];
+
+/* Performs the aggregation on the orders collection */
+$cursor = $collection->aggregate($pipeline);
+
+foreach ($cursor as $doc) {
+    echo json_encode($doc), PHP_EOL;
+}
+// end-builder-lookup
