98a175d0705f8476627d06de402393eac2e0b3eb
Nora Reidy <nora.reidy@mongodb.com>
2025-02-24 13:19:22 -0500

    DOCSP-43420: Client bulk write (#177)
    * DOCSP-43420: Client bulk write
    
    * versions
    
    * output
    
    * error type
    
    * wording
    
    * RM feedback
    
    * admonition
    
    * CC feedback
    
    (cherry picked from commit 7609c5a404734214041e3cd84866c57e1c737d2a)

--- source/includes/write/bulk-write.py
+++ source/includes/write/bulk-write.py
@@ -1,6 +1,7 @@
 # start-bulk-insert-one
-operation = pymongo.InsertOne(
-    {
+operation = InsertOne(
+    namespace="sample_restaurants.restaurants",
+    document={
         "name": "Mongo's Deli",
         "cuisine": "Sandwiches",
         "borough": "Manhattan",
@@ -10,23 +11,26 @@ operation = pymongo.InsertOne(
 # end-bulk-insert-one
 
 # start-bulk-update-one
-operation = pymongo.UpdateOne(
-    { "name": "Mongo's Deli" },
-    { "$set": { "cuisine": "Sandwiches and Salads" }},
+operation = UpdateOne(
+    namespace="sample_restaurants.restaurants",
+    filter={ "name": "Mongo's Deli" },
+    update={ "$set": { "cuisine": "Sandwiches and Salads" }}
 )
 # end-bulk-update-one
 
 # start-bulk-update-many
-operation = pymongo.UpdateMany(
-    { "name": "Mongo's Deli" },
-    { "$set": { "cuisine": "Sandwiches and Salads" }},
+operation = UpdateMany(
+    namespace="sample_restaurants.restaurants",
+    filter={ "name": "Mongo's Deli" },
+    update={ "$set": { "cuisine": "Sandwiches and Salads" }}
 )
 # end-bulk-update-many
 
 # start-bulk-replace-one
-operation = pymongo.ReplaceOne(
-    { "restaurant_id": "1234" },
-    {
+operation = ReplaceOne(
+    namespace="sample_restaurants.restaurants",
+    filter={ "restaurant_id": "1234" },
+    replacement={
         "name": "Mongo's Pizza",
         "cuisine": "Pizza",
         "borough": "Brooklyn",
@@ -36,45 +40,82 @@ operation = pymongo.ReplaceOne(
 # end-bulk-replace-one
 
 # start-bulk-delete-one
-operation = pymongo.DeleteOne({ "restaurant_id": "5678" })
+operation = DeleteOne(
+    namespace="sample_restaurants.restaurants",
+    filter={ "restaurant_id": "5678" }
+)
 # end-bulk-delete-one
 
 # start-bulk-delete-many
-operation = pymongo.DeleteMany({ "name": "Mongo's Deli" })
+operation = DeleteMany(
+    namespace="sample_restaurants.restaurants",
+    filter={ "name": "Mongo's Deli" }
+)
 # end-bulk-delete-many
 
-# start-bulk-write-mixed
+# start-bulk-write-mixed-collection
 operations = [
-    pymongo.InsertOne(
-        {
+    InsertOne(
+        document={
             "name": "Mongo's Deli",
             "cuisine": "Sandwiches",
             "borough": "Manhattan",
             "restaurant_id": "1234"
         }
     ),
-    pymongo.InsertOne(
-        {
+    InsertOne(
+        document={
             "name": "Mongo's Deli",
             "cuisine": "Sandwiches",
             "borough": "Brooklyn",
             "restaurant_id": "5678"
         }
     ),
-    pymongo.UpdateMany(
-        { "name": "Mongo's Deli" },
-        { "$set": { "cuisine": "Sandwiches and Salads" }},
+    UpdateMany(
+        filter={ "name": "Mongo's Deli" },
+        update={ "$set": { "cuisine": "Sandwiches and Salads" }}
     ),
-    pymongo.DeleteOne(
-        { "restaurant_id": "1234" }
+    DeleteOne(
+        filter={ "restaurant_id": "1234" }
     )
 ]
 
 results = restaurants.bulk_write(operations)
 
 print(results)
-# end-bulk-write-mixed
+# end-bulk-write-mixed-collection
+
+
+# start-bulk-write-mixed-client
+operations = [
+    InsertOne(
+        namespace="sample_mflix.movies",
+        document={
+            "title": "Minari",
+            "runtime": 217,
+            "genres": ["Drama", "Comedy"]
+        }
+    ),
+    UpdateOne(
+        namespace="sample_mflix.movies",
+        filter={ "title": "Minari" },
+        update={ "$set": { "runtime": 117 }}
+    ),
+    DeleteMany(
+        namespace="sample_restaurants.restaurants",
+        filter={ "cuisine": "French" }
+    )
+]
+
+results = client.bulk_write(operations)
+
+print(results)
+# end-bulk-write-mixed-client
 
 # start-bulk-write-unordered
 results = restaurants.bulk_write(operations, ordered=False)
 # end-bulk-write-unordered
+
+# start-bulk-write-verbose
+results = client.bulk_write(operations, verbose_results=True)
+# end-bulk-write-verbose
