cdbce599c689c11905474630cffdc55988a90135
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2025-03-12 16:35:30 -0400

    DOCSP-46923: go v2 - aws lambda code ex (#6891)

--- source/includes/drivers-examples/_example_aws_lambda_handler.go
+++ source/includes/drivers-examples/_example_aws_lambda_handler.go
@@ -11,13 +11,13 @@ import (
 	"os"
 
 	runtime "github.com/aws/aws-lambda-go/lambda"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 // Start AWS Lambda Example 1
 
-var client, err = mongo.Connect(context.TODO(), options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
+var client, err = mongo.Connect(options.Client().ApplyURI(os.Getenv("MONGODB_URI")))
 
 func HandleRequest(ctx context.Context) error {
 	if err != nil {
