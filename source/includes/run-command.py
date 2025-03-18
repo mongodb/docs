d4ee483f775ad7b717cf779c3a3b84efbc148604
Michael Morisi <michael.morisi@mongodb.com>
2025-03-18 10:51:29 -0400

    DOCSP-48168: Async examples for Databases and Collections/Run Command

--- source/includes/run-command.py
+++ source/includes/run-command.py
@@ -1,15 +1,12 @@
 # start-hello
-
 database = client.get_database("my_db")
 
 hello = database.command("hello")
 
 print(hello)
-
 # end-hello
 
 # start-readpref
-
 from pymongo.read_preferences import Secondary
 
 database = client.get_database("my_db")
@@ -17,25 +14,20 @@ database = client.get_database("my_db")
 hello = database.command("hello", read_preference=Secondary())
 
 print(hello)
-
 # end-readpref
 
 # start-cursor-command
-
 database = client.get_database("sample_mflix")
 
 result = database.cursor_command("find", "movies", filter={"runtime": 11})
 
 print(result.to_list())
-
 # end-cursor-command
 
 # start-runcommand
-
 database = client.get_database("sample_mflix")
 
 result = database.command("dbStats")
 
 print(result)
-
 # end-runcommand
