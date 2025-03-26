2d25f4f830efd4e47197ae6a27e5aa2c9af1a86d
Rea Rustagi <85902999+rustagir@users.noreply.github.com>
2025-03-26 16:24:00 -0400

    DOCSP-48677: v5.4 release (#206)
    * DOCSP-48677: v5.4 release
    
    * fix extensions dependency

--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -1,4 +1,4 @@
-val kotlin_mongodb_version: String by project
+val kotlinMongoDbDriverVersion: String by project
 
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.25"
@@ -17,7 +17,11 @@ repositories {
 }
 
 dependencies {
-    implementation("org.mongodb:mongodb-driver-kotlin-coroutine:$kotlin_mongodb_version")
+    implementation(platform("org.mongodb:mongodb-driver-bom:$kotlinMongoDbDriverVersion"))
+    implementation("org.mongodb:mongodb-driver-kotlin-coroutine")
+    implementation("org.mongodb:bson-kotlinx")
+    implementation("org.mongodb:mongodb-driver-kotlin-extensions:$kotlinMongoDbDriverVersion")
+
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.8.10")
     implementation("org.slf4j:slf4j-api:2.0.5")
@@ -29,9 +33,8 @@ dependencies {
     implementation("com.github.luben:zstd-jni:1.5.5-4")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.6.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
-    implementation("org.mongodb:bson-kotlinx:$kotlin_mongodb_version")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.1")
-    implementation("org.mongodb:mongodb-driver-kotlin-extensions:$kotlin_mongodb_version")
+
 }
 
 tasks.test {
--- source/includes/language-compatibility-table-kotlin.rst
+++ source/includes/language-compatibility-table-kotlin.rst
@@ -7,5 +7,5 @@
    * - Kotlin Driver Version
      - Kotlin 1.8
 
-   * - 4.10 to 5.3
+   * - 4.10 to 5.4
      - ✓
