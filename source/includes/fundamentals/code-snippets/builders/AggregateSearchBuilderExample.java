a83d0cd6fcb3c995b900b757eb47ed7e451042f4
Rachel Mackintosh <148898879+rachel-mack@users.noreply.github.com>
2025-04-02 12:36:10 -0400

    [Java Sync] Add additional SearchOperator helper methods for the rest of the Atlas Search operators (#658) (#669)
    * atlas search operators
    
    * aggregation page
    
    * formatting
    
    * table format
    
    * references
    
    * JS feedback
    
    * use shared doc
    
    * fix for new core-api variable
    
    * eg updates
    
    * rebase
    
    * eg format
    
    * format
    
    * release reference
    
    * WIP
    
    * use List.of()
    
    ---------
    
    Co-authored-by: rustagir <rea.rustagi@mongodb.com>

--- source/includes/fundamentals/code-snippets/builders/AggregateSearchBuilderExample.java
+++ source/includes/fundamentals/code-snippets/builders/AggregateSearchBuilderExample.java
@@ -1,6 +1,5 @@
-package fundamentals.builders;
+package org.example;
 
-import java.util.Arrays;
 import java.util.List;
 
 import org.bson.Document;
@@ -14,7 +13,8 @@ import com.mongodb.client.model.Aggregates;
 import com.mongodb.client.model.Filters;
 import com.mongodb.client.model.Projections;
 import com.mongodb.client.model.search.SearchOperator;
-import com.mongodb.client.model.search.SearchPath;
+import static com.mongodb.client.model.search.SearchPath.fieldPath;
+
 public class AggregateSearchBuilderExample {
 
     private static final String CONNECTION_URI = "<connection URI>";
@@ -24,14 +24,14 @@ public class AggregateSearchBuilderExample {
         Bson matchStage = Aggregates.match(Filters.eq("title", "Future"));
         Bson projection = Aggregates.project(Projections.fields(Projections.include("title", "released")));
 
-        List<Bson> aggregateStages = Arrays.asList(matchStage, projection);
+        List<Bson> aggregateStages = List.of(matchStage, projection);
         System.out.println("aggregateStages: " + aggregateStages);
         collection.aggregate(
                 aggregateStages
-                ).forEach(result -> System.out.println(result));
+        ).forEach(result -> System.out.println(result));
     }
 
-    /* 
+    /*
      * Atlas text search aggregation
      * Requires Atlas cluster and full text search index
      * See https://www.mongodb.com/docs/atlas/atlas-search/tutorial/ for more info on requirements
@@ -40,13 +40,65 @@ public class AggregateSearchBuilderExample {
         // begin atlasTextSearch
         Bson textSearch = Aggregates.search(
                 SearchOperator.text(
-                        SearchPath.fieldPath("title"), "Future"));
+                        fieldPath("title"), "Future"));
         // end atlasTextSearch
 
         // To condense result data, add this projection into the pipeline
         // Bson projection = Aggregates.project(Projections.fields(Projections.include("title", "released")));
 
-        List<Bson> aggregateStages = Arrays.asList(textSearch);
+        List<Bson> aggregateStages = List.of(textSearch);
+        System.out.println("aggregateStages: " + aggregateStages);
+
+        System.out.println("explain:\n" + collection.aggregate(aggregateStages).explain());
+        collection.aggregate(aggregateStages).forEach(result -> System.out.println(result));
+    }
+
+    /*
+     * Atlas search aggregation
+     * Requires Atlas cluster and full text search index
+     * See https://www.mongodb.com/docs/atlas/atlas-search/tutorial/ for more info on requirements
+     */
+    private static void runAtlasSearchWithSearchHelperMethods(MongoCollection<Document> collection) {
+        // begin atlasHelperMethods
+        List<Bson> pipeline = new ArrayList<>();
+
+        pipeline.add(Aggregates.search(
+                SearchOperator.compound()
+                        .filter(
+                                List.of(
+                                        SearchOperator.in(fieldPath("genres"), "Comedy"),
+                                        SearchOperator.phrase(fieldPath("fullplot"), "new york"),
+                                        SearchOperator.numberRange(fieldPath("year")).gtLt(1950, 2000),
+                                        SearchOperator.wildcard(fieldPath("title"), "Love *")
+                                ))));
+
+        pipeline.add(Aggregates.project(
+                Projections.include("title", "year", "genres")
+        ));
+
+        AggregateIterable<Document> results = collection.aggregate(pipeline);
+        results.forEach(doc -> System.out.println(doc.toJson()));
+        // end atlasHelperMethods
+    }
+
+    /*
+     * Atlas search aggregation
+     * Requires Atlas cluster and full text search index
+     * See https://www.mongodb.com/docs/atlas/atlas-search/tutorial/ for more info on requirements
+     */
+    private static void runAtlasSearch(MongoCollection<Document> collection) {
+        // begin atlasSearch
+        Bson search_stage = Aggregates.search(
+                SearchOperator.compound()
+                        .filter(List.of(SearchOperator.text(fieldPath("genres"), "Drama")))
+                        .must(List.of(SearchOperator.phrase(fieldPath("cast"), "keanu reeves")))
+        );
+        // end atlasSearch
+
+        // To condense result data, add this projection into the pipeline
+        // Bson projection = Aggregates.project(Projections.fields(Projections.include("title", "released")));
+
+        List<Bson> aggregateStages = List.of(search_stage);
         System.out.println("aggregateStages: " + aggregateStages);
 
         System.out.println("explain:\n" + collection.aggregate(aggregateStages).explain());
@@ -55,12 +107,12 @@ public class AggregateSearchBuilderExample {
 
     private static void runAtlasTextSearchMeta(MongoCollection<Document> collection) {
         Bson textSearchMeta =
-        // begin atlasSearchMeta
-        Aggregates.searchMeta(
-                SearchOperator.near(2010, 1, SearchPath.fieldPath("year")));
+                // begin atlasSearchMeta
+                Aggregates.searchMeta(
+                        SearchOperator.near(2010, 1, fieldPath("year")));
         // end atlasSearchMeta
 
-        List<Bson> aggregateStages = Arrays.asList(textSearchMeta);
+        List<Bson> aggregateStages = List.of(textSearchMeta);
         System.out.println("aggregateStages: " + aggregateStages);
 
         collection.aggregate(aggregateStages).forEach(result -> System.out.println(result));
@@ -77,7 +129,9 @@ public class AggregateSearchBuilderExample {
             // Uncomment the methods that correspond to what you're testing
             // runMatch(collection);
             // runAtlasTextSearch(collection);
-            runAtlasTextSearchMeta(collection);
+            // runAtlasSearch(collection);
+            // runAtlasTextSearchMeta(collection);
+            // runAtlasSearchWithSearchHelperMethods(collection);
         }
     }
 }
