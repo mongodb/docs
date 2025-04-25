76d920d581a127881f7a83a316dc4b3a12dd0059
Jérôme Tamarelle <jerome@tamarelle.net>
2025-04-25 12:30:53 +0200

    Create QE tutorial in PHP

---
+++ source/includes/qe-tutorials/php/queryable-encryption-tutorial.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace MongoDB\Tutorials\QueryableEncryption;
+
+use MongoDB\Exception\RuntimeException;
+use Symfony\Component\Dotenv\Dotenv;
+
+require __DIR__.'/vendor/autoload.php';
+require __DIR__.'/queryable-encryption-helpers.php';
+
+(new Dotenv())->usePutenv()->loadEnv(__DIR__.'/.env');
+
+// start-setup-application-variables
+// KMS provider name should be one of the following: "aws", "gcp", "azure", "kmip" or "local"
+$kmsProviderName = '<KMS provider name>';
+
+$uri = getenv('MONGODB_URI'); // Your connection URI
+
+$keyVaultDatabaseName = 'encryption';
+$keyVaultCollectionName = '__keyVault';
+$keyVaultNamespace = $keyVaultDatabaseName . '.' . $keyVaultCollectionName;
+$encryptedDatabaseName = 'medicalRecords';
+$encryptedCollectionName = 'patients';
+// end-setup-application-variables
+
+// Override the KMS provider name with the environment variable if set
+$kmsProviderName = getenv('KMS_PROVIDER') ?: throw new RuntimeException('Variable KMS_PROVIDER not set.');
+
+$kmsProviderCredentials = getKMSProviderCredentials($kmsProviderName);
+$customerMasterKeyCredentials = getCustomerMasterKeyCredentials($kmsProviderName);
+
+$autoEncryptionOptions = getAutoEncryptionOptions(
+    $kmsProviderName,
+    $keyVaultNamespace,
+    $kmsProviderCredentials
+);
+
+// start-create-client
+$encryptedClient = new \MongoDB\Client($uri, [], [
+    'autoEncryption' => $autoEncryptionOptions,
+]);
+// end-create-client
+
+dropExistingCollection($encryptedClient, $encryptedDatabaseName);
+dropExistingCollection($encryptedClient, $keyVaultDatabaseName);
+
+// start-encrypted-fields-map
+$encryptedFieldsMap = [
+    'encryptedFields' => [
+        'fields' => [
+            [
+                'path' => 'patientRecord.ssn',
+                'bsonType' => 'string',
+                'queries' => ['queryType' => 'equality'],
+            ],
+            [
+                'path' => 'patientRecord.billing',
+                'bsonType' => 'object',
+            ],
+        ],
+    ],
+];
+// end-encrypted-fields-map
+
+$clientEncryption = getClientEncryption(
+    $encryptedClient,
+    $autoEncryptionOptions
+);
+
+createEncryptedCollection(
+    $encryptedClient,
+    $clientEncryption,
+    $encryptedDatabaseName,
+    $encryptedCollectionName,
+    $kmsProviderName,
+    $encryptedFieldsMap,
+    $customerMasterKeyCredentials
+);
+
+// start-insert-document
+$patientDocument = [
+    'patientName' => 'Jon Doe',
+    'patientId' => 12345678,
+    'patientRecord' => [
+        'ssn' => '987-65-4320',
+        'billing' => [
+            'type' => 'Visa',
+            'number' => '4111111111111111',
+        ],
+    ],
+];
+
+$encryptedCollection = $encryptedClient
+    ->getDatabase($encryptedDatabaseName)
+    ->getCollection($encryptedCollectionName);
+
+$result = $encryptedCollection->insertOne($patientDocument);
+// end-insert-document
+
+if ($result->isAcknowledged()) {
+    echo "Successfully inserted the patient document.\n";
+}
+
+// start-find-document
+$findResult = $encryptedCollection->findOne([
+    'patientRecord.ssn' => '987-65-4320',
+]);
+
+print_r($findResult);
+// end-find-document
