f9a37d505400f8dfeea9d26c4881d15feddaec26
Stephanie <52582720+stephmarie17@users.noreply.github.com>
2025-04-30 07:21:06 -0700

    DOCSP-48859 Add RESTful API tutorial (#617)
    (cherry picked from commit 7de5c71ad852fb851b2a5729d9283c437f21ee44)

---
+++ source/includes/fundamentals/code-examples/restful-api-tutorial/MongoDBSettingsSetup.cs
@@ -0,0 +1,9 @@
+namespace MongoExample.Models;
+
+public class MongoDBSettings {
+
+    public string ConnectionURI { get; set; } = null!;
+    public string DatabaseName { get; set; } = null!;
+    public string CollectionName { get; set; } = null!;
+
+}
