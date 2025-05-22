14e097d71a411c6d13b196dca89a19b3c2ac0acc
Jordan Smith <45415425+jordan-smith721@users.noreply.github.com>
2025-05-22 12:57:03 -0400

    DOCSP-36867 Perform operations with driver (#57)
    (cherry picked from commit 41f81547d1040d7f0fe65b494328686724c34316)

--- source/includes/code-examples/faq.cs
+++ source/includes/code-examples/faq.cs
@@ -14,3 +14,20 @@ async Task CreateIndexesAsync(IMongoDatabase database)
           .Indexes.CreateOneAsync(moviesIndex);
 }
 // end-create-index
+
+// start-atlas-search
+// Client used to set up your DbContext 
+var client = new MongoClient("<connection string>");
+
+var clientDB = client.GetDatabase("sample_guides");
+var collection = clientDB.GetCollection<Planet>("planets");
+
+var searchResult = collection.Aggregate()
+    .Search(Builders<Planet>.Search.Equals(p => p.hasRings, true))
+    .ToList();
+
+foreach (var p in searchResult)
+{
+    Console.WriteLine(p.name);
+}
+// end-atlas-search
