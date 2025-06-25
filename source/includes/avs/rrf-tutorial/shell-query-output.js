de7b5648e802a6b95bdda511d22c97c194376298
kanchana-mongodb <54281287+kanchana-mongodb@users.noreply.github.com>
2025-06-25 08:08:33 -0700

    DOCSP-34084 Doc for Hybrid Search Rank Fusion (#7015)
    * DOCSP-34084 Hybrid Search Rank Fusion tutorial update
    
    * DOCSP-34084 doc for Hybrid Search Rank Fusion
    
    * DOCSP-34084 updates for HW's feedback
    
    * DOCSP-34084 updates for HW's feedback
    
    * DOCSP-34084 updates for HW's feedback
    
    * Update source/atlas-vector-search/hybrid-search.txt
    
    Co-authored-by: hweller1 <121510832+hweller1@users.noreply.github.com>
    
    * DOCSP-34084 updates for HW's feedback
    
    * Apply suggestions from code review
    
    Co-authored-by: davidhou17 <55004296+davidhou17@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: davidhou17 <55004296+davidhou17@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: davidhou17 <55004296+davidhou17@users.noreply.github.com>
    
    * DOCSP-34084 added constant value to doc
    
    * DOCSP-34084 addressing merge conflicts
    
    * DOCSP-34084 updates for broken link to rankFusion
    
    ---------
    
    Co-authored-by: hweller1 <121510832+hweller1@users.noreply.github.com>
    Co-authored-by: davidhou17 <55004296+davidhou17@users.noreply.github.com>

--- source/includes/avs/rrf-tutorial/shell-query-output.js
+++ source/includes/avs/rrf-tutorial/shell-query-output.js
@@ -1,70 +1,424 @@
-{
-  _id: new ObjectId('573a13c0f29313caabd62f62'),
-  title: 'Star Wars: The Clone Wars',
-  vs_score: 0.0015625,
-  fts_score: 0.015,
-  score: 0.0165625
-}
-{
-  _id: new ObjectId('573a1397f29313caabce68f6'),
-  title: 'Star Wars: Episode IV - A New Hope',
-  vs_score: 0.0016666666666666668,
-  fts_score: 0.014754098360655738,
-  score: 0.016420765027322405
-}
-{
-  _id: new ObjectId('573a139af29313caabcf0f5f'),
-  title: 'Star Wars: Episode I - The Phantom Menace',
-  vs_score: 0.0016393442622950822,
-  fts_score: 0.014516129032258065,
-  score: 0.016155473294553146
-}
-{
-  _id: new ObjectId('573a1397f29313caabce77d9'),
-  title: 'Star Wars: Episode V - The Empire Strikes Back',
-  vs_score: 0.0016129032258064516,
-  fts_score: 0.014285714285714285,
-  score: 0.015898617511520736
-}
-{
-  _id: new ObjectId('573a1397f29313caabce8cdb'),
-  title: 'Star Wars: Episode VI - Return of the Jedi',
-  vs_score: 0.0015873015873015873,
-  fts_score: 0.0140625,
-  score: 0.015649801587301587
-}
-{
-  _id: new ObjectId('573a139af29313caabcf124d'),
-  title: 'Star Wars: Episode III - Revenge of the Sith',
-  vs_score: 0.0013698630136986301,
-  fts_score: 0.013846153846153847,
-  score: 0.015216016859852476
-}
-{
-  _id: new ObjectId('573a139af29313caabcf1258'),
-  title: 'Star Wars: Episode II - Attack of the Clones',
-  vs_score: 0.0014925373134328358,
-  fts_score: 0.013636363636363637,
-  score: 0.015128900949796473
-}
-{
-  _id: new ObjectId('573a1397f29313caabce6f53'),
-  title: 'Message from Space',
-  vs_score: 0.0015384615384615387,
-  fts_score: 0,
-  score: 0.0015384615384615387
-}
-{
-  _id: new ObjectId('573a139df29313caabcfa90b'),
-  title: 'Message from Space',
-  vs_score: 0.0015151515151515154,
-  fts_score: 0,
-  score: 0.0015151515151515154
-}
-{
-  _id: new ObjectId('573a13d9f29313caabda92ff'),
-  title: 'Guardians of the Galaxy',
-  vs_score: 0.0014705882352941176,
-  fts_score: 0,
-  score: 0.0014705882352941176
-}
+[
+  {
+    _id: ObjectId('573a1397f29313caabce68f6'),
+    plot: "Luke Skywalker joins forces with a Jedi Knight, a cocky pilot, a wookiee and two droids to save the universe from the Empire's world-destroying battle-station, while also attempting to rescue Princess Leia from the evil Darth Vader.",
+    title: 'Star Wars: Episode IV - A New Hope',
+    scoreDetails: {
+      value: 0.01626123744050767,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        {
+          inputPipelineName: 'fullTextPipeline',
+          rank: 2,
+          weight: 0.5,
+          value: 2.9629626274108887,
+          details: []
+        },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 1,
+          weight: 0.5,
+          value: 0.9400461912155151,
+          details: []
+        }
+      ]
+    }
+  },
+  {
+    _id: ObjectId('573a139af29313caabcf0f5f'),
+    plot: 'Two Jedi Knights escape a hostile blockade to find allies and come across a young boy who may bring balance to the Force, but the long dormant Sith resurface to reclaim their old glory.',
+    title: 'Star Wars: Episode I - The Phantom Menace',
+    scoreDetails: {
+      value: 0.016129032258064516,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        {
+          inputPipelineName: 'fullTextPipeline',
+          rank: 2,
+          weight: 0.5,
+          value: 2.9629626274108887,
+          details: []
+        },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 2,
+          weight: 0.5,
+          value: 0.9308199286460876,
+          details: []
+        }
+      ]
+    }
+  },
+  {
+    _id: ObjectId('573a13c0f29313caabd62f62'),
+    plot: 'Anakin Skywalker and Ahsoka Tano must rescue the kidnapped son of Jabba the Hutt, but political intrigue complicates their mission.',
+    title: 'Star Wars: The Clone Wars',
+    scoreDetails: {
+      value: 0.015889029003783105,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        {
+          inputPipelineName: 'fullTextPipeline',
+          rank: 1,
+          weight: 0.5,
+          value: 3.6164965629577637,
+          details: []
+        },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 5,
+          weight: 0.5,
+          value: 0.9257078170776367,
+          details: []
+        }
+      ]
+    }
+  },
+  {
+    _id: ObjectId('573a1397f29313caabce77d9'),
+    plot: 'After the rebels have been brutally overpowered by the Empire on their newly established base, Luke Skywalker takes advanced Jedi training with Master Yoda, while his friends are pursued by Darth Vader as part of his plan to capture Luke.',
+    title: 'Star Wars: Episode V - The Empire Strikes Back',
+    scoreDetails: {
+      value: 0.015749007936507936,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        {
+          inputPipelineName: 'fullTextPipeline',
+          rank: 4,
+          weight: 0.5,
+          value: 2.7174296379089355,
+          details: []
+        },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 3,
+          weight: 0.5,
+          value: 0.9270252585411072,
+          details: []
+        }
+      ]
+    }
+  },
+  {
+    _id: ObjectId('573a1397f29313caabce8cdb'),
+    plot: 'After rescuing Han Solo from the palace of Jabba the Hutt, the rebels attempt to destroy the second Death Star, while Luke struggles to make Vader return from the dark side of the Force.',
+    title: 'Star Wars: Episode VI - Return of the Jedi',
+    scoreDetails: {
+      value: 0.015625,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        {
+          inputPipelineName: 'fullTextPipeline',
+          rank: 4,
+          weight: 0.5,
+          value: 2.7174296379089355,
+          details: []
+        },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 4,
+          weight: 0.5,
+          value: 0.9263832569122314,
+          details: []
+        }
+      ]
+    }
+  },
+  {
+    _id: ObjectId('573a139af29313caabcf1258'),
+    plot: 'Ten years after initially meeting, Anakin Skywalker shares a forbidden romance with Padmè, while Obi-Wan investigates an assassination attempt on the Senator and discovers a secret clone army crafted for the Jedi.',
+    title: 'Star Wars: Episode II - Attack of the Clones',
+    scoreDetails: {
+      value: 0.015165441176470588,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        {
+          inputPipelineName: 'fullTextPipeline',
+          rank: 4,
+          weight: 0.5,
+          value: 2.7174296379089355,
+          details: []
+        },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 8,
+          weight: 0.5,
+          value: 0.9229284524917603,
+          details: []
+        }
+      ]
+    }
+  },
+  {
+    _id: ObjectId('573a139af29313caabcf124d'),
+    plot: 'As the Clone Wars near an end, the Sith Lord Darth Sidious steps out of the shadows, at which time Anakin succumbs to his emotions, becoming Darth Vader and putting his relationships with Obi-Wan and Padme at risk.',
+    title: 'Star Wars: Episode III - Revenge of the Sith',
+    scoreDetails: {
+      value: 0.014569256756756757,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        {
+          inputPipelineName: 'fullTextPipeline',
+          rank: 4,
+          weight: 0.5,
+          value: 2.7174296379089355,
+          details: []
+        },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 14,
+          weight: 0.5,
+          value: 0.9172446727752686,
+          details: []
+        }
+      ]
+    }
+  },
+  {
+    _id: ObjectId('573a1397f29313caabce6f53'),
+    plot: 'In this Star Wars take-off, the peaceful planet of Jillucia has been nearly wiped out by the Gavanas, whose leader takes orders from his mother (played a comic actor in drag) rather than ...',
+    title: 'Message from Space',
+    scoreDetails: {
+      value: 0.007575757575757576,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        { inputPipelineName: 'fullTextPipeline', rank: 0, weight: 0.5 },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 6,
+          weight: 0.5,
+          value: 0.9238690137863159,
+          details: []
+        }
+      ]
+    }
+  },
+  {
+    _id: ObjectId('573a139df29313caabcfa90b'),
+    plot: 'In this Star Wars take-off, the peaceful planet of Jillucia has been nearly wiped out by the Gavanas, whose leader takes orders from his mother (played a comic actor in drag) rather than ...',
+    title: 'Message from Space',
+    scoreDetails: {
+      value: 0.007575757575757576,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        { inputPipelineName: 'fullTextPipeline', rank: 0, weight: 0.5 },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 6,
+          weight: 0.5,
+          value: 0.9238690137863159,
+          details: []
+        }
+      ]
+    }
+  },
+  {
+    _id: ObjectId('573a13d9f29313caabda92ff'),
+    plot: 'A group of intergalactic criminals are forced to work together to stop a fanatical warrior from taking control of the universe.',
+    title: 'Guardians of the Galaxy',
+    scoreDetails: {
+      value: 0.007246376811594203,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        { inputPipelineName: 'fullTextPipeline', rank: 0, weight: 0.5 },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 9,
+          weight: 0.5,
+          value: 0.9183617234230042,
+          details: []
+        }
+      ]
+    }
+  },
+  {
+    _id: ObjectId('573a13d5f29313caabd9c312'),
+    plot: 'A group of intergalactic criminals are forced to work together to stop a fanatical warrior from taking control of the universe.',
+    title: 'Guardians of the Galaxy',
+    scoreDetails: {
+      value: 0.007142857142857143,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        { inputPipelineName: 'fullTextPipeline', rank: 0, weight: 0.5 },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 10,
+          weight: 0.5,
+          value: 0.9183551073074341,
+          details: []
+        }
+      ]
+    }
+  },
+  {
+    _id: ObjectId('573a13d4f29313caabd9887f'),
+    plot: 'In this breathtaking science fiction spectacle, a strange mechanical device lands on a desolate world and uses the planet to undergo a startling transformation, that has profound implications for an entire galaxy.',
+    title: 'Abiogenesis',
+    scoreDetails: {
+      value: 0.007042253521126761,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        { inputPipelineName: 'fullTextPipeline', rank: 0, weight: 0.5 },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 11,
+          weight: 0.5,
+          value: 0.918245792388916,
+          details: []
+        }
+      ]
+    }
+  },
+  {
+    _id: ObjectId('573a1398f29313caabce9091'),
+    plot: "A Duke's son leads desert warriors against the galactic emperor and his father's evil nemesis when they assassinate his father and free their desert world from the emperor's rule.",
+    title: 'Dune',
+    scoreDetails: {
+      value: 0.006944444444444444,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        { inputPipelineName: 'fullTextPipeline', rank: 0, weight: 0.5 },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 12,
+          weight: 0.5,
+          value: 0.9180452823638916,
+          details: []
+        }
+      ]
+    }
+  },
+  {
+    _id: ObjectId('573a1398f29313caabce90bd'),
+    plot: 'Wicket the Ewok and his friends agree to help two shipwrecked human children on a quest to find their parents.',
+    title: 'The Ewok Adventure',
+    scoreDetails: {
+      value: 0.00684931506849315,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        { inputPipelineName: 'fullTextPipeline', rank: 0, weight: 0.5 },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 13,
+          weight: 0.5,
+          value: 0.9176332354545593,
+          details: []
+        }
+      ]
+    }
+  },
+  {
+    _id: ObjectId('573a1398f29313caabce8d67'),
+    plot: 'A futuristic, sensitive tale of adventure and confrontation when a 10 year old boy is accidentally kidnapped by a spaceship filled with a motley crew of space pirates.',
+    title: 'Space Raiders',
+    scoreDetails: {
+      value: 0.006666666666666667,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        { inputPipelineName: 'fullTextPipeline', rank: 0, weight: 0.5 },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 15,
+          weight: 0.5,
+          value: 0.916418731212616,
+          details: []
+        }
+      ]
+    }
+  },
+  {
+    _id: ObjectId('573a13b0f29313caabd33d15'),
+    plot: 'Two young brothers are drawn into an intergalactic adventure when their house is hurled through the depths of space by the magical board game they are playing.',
+    title: 'Zathura: A Space Adventure',
+    scoreDetails: {
+      value: 0.006578947368421052,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        { inputPipelineName: 'fullTextPipeline', rank: 0, weight: 0.5 },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 16,
+          weight: 0.5,
+          value: 0.9160659909248352,
+          details: []
+        }
+      ]
+    }
+  },
+  {
+    _id: ObjectId('573a1397f29313caabce7509'),
+    plot: 'An outlaw smuggler and her alien companion are recruited by the Emperor of the Galaxy to rescue his son and destroy a secret weapon by the evil Count Zarth Arn.',
+    title: 'Starcrash',
+    scoreDetails: {
+      value: 0.006493506493506494,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        { inputPipelineName: 'fullTextPipeline', rank: 0, weight: 0.5 },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 17,
+          weight: 0.5,
+          value: 0.915353000164032,
+          details: []
+        }
+      ]
+    }
+  },
+  {
+    _id: ObjectId('573a139bf29313caabcf3d4b'),
+    plot: 'An Air Force astronaut crash lands on a mysterious planet where evolved, talking apes dominate a race of primitive humans.',
+    title: 'Planet of the Apes',
+    scoreDetails: {
+      value: 0.00641025641025641,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        { inputPipelineName: 'fullTextPipeline', rank: 0, weight: 0.5 },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 18,
+          weight: 0.5,
+          value: 0.9140176773071289,
+          details: []
+        }
+      ]
+    }
+  },
+  {
+    _id: ObjectId('573a13a3f29313caabd0ec59'),
+    plot: 'An Air Force astronaut crash lands on a mysterious planet where evolved, talking apes dominate a race of primitive humans.',
+    title: 'Planet of the Apes',
+    scoreDetails: {
+      value: 0.00641025641025641,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        { inputPipelineName: 'fullTextPipeline', rank: 0, weight: 0.5 },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 18,
+          weight: 0.5,
+          value: 0.9140176773071289,
+          details: []
+        }
+      ]
+    }
+  },
+  {
+    _id: ObjectId('573a13b0f29313caabd34a3e'),
+    plot: 'A spaceship arrives in Arizona, 1873, to take over the Earth, starting with the Wild West region. A posse of cowboys and natives are all that stand in their way.',
+    title: 'Cowboys & Aliens',
+    scoreDetails: {
+      value: 0.00625,
+      description: 'value output by reciprocal rank fusion algorithm, computed as sum of (weight * (1 / (60 + rank))) across input pipelines from which this document is output, from:',
+      details: [
+        { inputPipelineName: 'fullTextPipeline', rank: 0, weight: 0.5 },
+        {
+          inputPipelineName: 'vectorPipeline',
+          rank: 20,
+          weight: 0.5,
+          value: 0.9139271974563599,
+          details: []
+        }
+      ]
+    }
+  }
+]
--- source/includes/avs/rrf-tutorial/steps-avs-rrf-tutorial-create-index-mongosh.rst
+++ source/includes/avs/rrf-tutorial/steps-avs-rrf-tutorial-create-index-mongosh.rst
@@ -1,22 +1,41 @@
 .. procedure:: 
    :style: normal
 
+   .. step:: Connect to the |service| {+cluster+} using {+mongosh+}.
+
+      To learn more, see :ref:`connect-mongo-shell`.
+
+   .. step:: Switch to the database that contains the collection for which you want to create the index.
+
+      .. io-code-block:: 
+         :copyable: true 
+
+         .. input:: 
+            :language: shell
+               
+            use sample_mflix 
+
+         .. output:: 
+            :language: shell 
+
+            switched to db sample_mflix
+
    .. step:: Define the {+avs+} index.
 
-      The following index definition indexes the ``plot_embedding`` field as
+      Run the following command. This index definition indexes the ``plot_embedding`` field as
       the {+avs+} field when querying the collection.
 
-      To create an {+avs+} index for a collection using {+mongosh+}
-      v2.1.2 or later, perform the following steps:
-      
-      .. include:: /includes/avs/rrf-tutorial/avs-rrf-tutorial-create-vector-index-mongosh.rst
+      .. literalinclude:: /includes/avs/rrf-tutorial/create-avs-index-mongosh.sh
+         :language: shell
+         :copyable: true 
+         :linenos:
 
    .. step:: Define the |fts| index. 
 
       The following index definition automatically indexes all the
       dynamically indexable fields in the collection.
 
-      To create a |fts| index for a collection using {+mongosh+}
-      v2.1.2 or later, perform the following steps:
-      
-      .. include:: /includes/avs/rrf-tutorial/avs-rrf-tutorial-create-fts-index-mongosh.rst
+      .. literalinclude:: /includes/avs/rrf-tutorial/create-fts-index-mongosh.sh
+         :language: shell
+         :copyable: true 
+         :linenos:
