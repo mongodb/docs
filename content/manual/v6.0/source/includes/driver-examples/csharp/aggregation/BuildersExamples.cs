efc0608e746565c0979a394086bacb51cce95386
Mike Woofter <108414937+mongoKart@users.noreply.github.com>
2025-07-08 13:57:25 -0500

    DOCSP-51590 - Fix build errors (#12759)

--- content/manual/v6.0/source/includes/driver-examples/csharp/aggregation/BuildersExamples.cs
+++ content/manual/v6.0/source/includes/driver-examples/csharp/aggregation/BuildersExamples.cs
@@ -154,7 +154,37 @@ public static class AggregationBuilders
             .Facet(bucketFacet, countFacet);
         // end facet
     }
- 
+
+    public static void GeoNearStage()
+    {
+        // start geoNear
+        var pipeline = new EmptyPipelineDefinition<Theater>()
+            .GeoNear(
+                GeoJson.Point(GeoJson.Geographic(-74.1, 40.95)),
+                new GeoNearOptions<Theater, Theater>
+                {
+                  DistanceField = "distance",
+                  MaxDistance = 8000,
+                  Key = "location.geo",
+                  Query = Builders<Theater>.Filter.Eq(t => t.Location.Address.State, "NJ"),
+                });
+        // end geoNear
+    
+        // start geoNear min
+        var pipeline = new EmptyPipelineDefinition<Theater>()
+            .GeoNear(
+                GeoJson.Point(GeoJson.Geographic(-74.1, 40.95)),
+                new GeoNearOptions<Theater, Theater>
+                {
+                  DistanceField = "distance",
+                  MinDistance = 8000,
+                  Key = "location.geo",
+                  Query = Builders<Theater>.Filter.Eq(t => t.Location.Address.State, "NJ"),
+                })
+            .Limit(4);
+        // end geoNear min
+    }
+
     public static void GraphLookupStage()
     {
         // start graphLookupBasic
