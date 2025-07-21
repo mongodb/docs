9a624456fb297469d4df5595fe7a1cfb71f742b0
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2025-07-21 11:13:43 -0400

    remove old files from entity framework

--- content/entity-framework/upcoming/build.sh
+++
@@ -1,8 +0,0 @@
-# ensures that we always use the latest version of the script
-if [ -f build-site.sh ]; then
-  rm build-site.sh
-fi 
-
-
-curl https://raw.githubusercontent.com/mongodb/docs-worker-pool/netlify-poc/scripts/build-site.sh -o build-site.sh 
-sh build-site.sh
