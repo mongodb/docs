bf951520227f4883225e6b49fcc4d6c65a64cb96
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2025-07-25 16:14:43 -0400

    Clean repo root

--- source/includes/subscriber-note.rst
+++
@@ -1,5 +0,0 @@
-.. important::
-
-   This guide uses custom ``Subscriber`` implementations, which are
-   described in the :ref:`Sample Custom Subscriber Implementations
-   <java-rs-custom-implementations>` guide.
--- source/includes/write/run-command.java
+++
@@ -1,5 +0,0 @@
-Document command = new Document("ping", 1);
-
-Publisher<Document> commandPublisher = database.runCommand(command);
-Document result = Mono.from(commandPublisher).block();
-System.out.println(result);
