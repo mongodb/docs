6f2e8c67066ed062c1a95b3711a60b6d01635a2a
shuangela <angela.shu@mongodb.com>
2025-07-28 14:08:15 -0400

    DOCSP-48715-username-pw (#238) (#239)
    * username pw
    
    * small change
    
    (cherry picked from commit 3878890e0d1777e201f947e2e8ba32e65c6517a7)

--- examples/src/test/kotlin/MongoClientSettingsTest.kt
+++ examples/src/test/kotlin/MongoClientSettingsTest.kt
@@ -22,7 +22,7 @@ import kotlin.test.Ignore
 // }
 
 /* NOTE: These tests are not run by default because they require a MongoDB deployment
-with a username and password. To run these tests locally, you need to set up your .env
+with a database username and database password. To run these tests locally, you need to set up your .env
 and replace the @Ignore annotation with @Test on the tests you want to run.
  */
 class MongoClientSettingsTest {
--- source/examples/generated/NetworkCompressionTest.snippet.connection-string-compression-example.kt
+++ source/examples/generated/NetworkCompressionTest.snippet.connection-string-compression-example.kt
@@ -1,5 +1,5 @@
 // Replace the placeholders with values from your MongoDB deployment's connection string
-val connectionString = ConnectionString("mongodb+srv://<user>:<password>@<cluster-url>/?compressors=snappy,zlib,zstd")
+val connectionString = ConnectionString("mongodb+srv://<db_username>:<db_password>@<cluster-url>/?compressors=snappy,zlib,zstd")
 
 // Create a new client with your settings
 val mongoClient = MongoClient.create(connectionString)
