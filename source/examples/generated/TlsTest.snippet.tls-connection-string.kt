ed73bd22125c14bafc2ab8956bdb6185fd627587
shuangela <angela.shu@mongodb.com>
2025-07-28 14:08:23 -0400

    DOCSP-48715-username-pw (#238) (#240)
    * username pw
    
    * small change
    
    (cherry picked from commit 3878890e0d1777e201f947e2e8ba32e65c6517a7)

--- source/examples/generated/TlsTest.snippet.tls-connection-string.kt
+++ source/examples/generated/TlsTest.snippet.tls-connection-string.kt
@@ -1,1 +1,1 @@
-val mongoClient = MongoClient.create("mongodb+srv://<user>:<password>@<cluster-url>?tls=true")
+val mongoClient = MongoClient.create("mongodb+srv://<db_username>:<db_password>@<cluster-url>?tls=true")
