2d8bf8eedebb420b6633b5dfce1312c6d4b8d64c
biancalaube <bianca.laube@gmail.com>
2025-09-17 17:07:53 -0400

    fix toc.ts comment

--- content/table-of-contents/toc.ts
+++ content/table-of-contents/toc.ts
@@ -5,7 +5,7 @@ import ManagementData from './L1-data/management';
 import ClientLibData from './L1-data/client-libraries';
 import type { TocItem } from './types';
 
-// These are all the L1's for the ToC - BIANCA
+// These are all the L1's for the ToC
 export const toc: TocItem[] = [
   {
     label: 'Get Started',
