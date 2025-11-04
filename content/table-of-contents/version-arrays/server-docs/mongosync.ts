b2495f7bea3a37975bf916c9f883530991451dc9
Marco Del Toro <marco.deltoro@mongodb.com>
2025-11-04 14:15:55 -0600

    DOCSP-54290: Archive mongosync v1.9 version (#15724)

--- content/table-of-contents/version-arrays/server-docs/mongosync.ts
+++ content/table-of-contents/version-arrays/server-docs/mongosync.ts
@@ -1,20 +1,12 @@
 import Versions from '../versions';
 
 // allVersions and deprecatedVersions must be in order: oldest -> newest
-const allVersions = [
-  'v1.9',
-  'v1.10',
-  'v1.11',
-  'v1.12',
-  'v1.13',
-  'v1.14',
-  'v1.15',
-  'v1.16',
-  'v1.17',
-];
+const allVersions: string[] = ["v1.9", "v1.10", "v1.11", "v1.12", "v1.13", "v1.14", "v1.15", "v1.16", "v1.17"];
+const deprecatedVersions: string[] = ["v1.9"];
 
-const deprecatedVersions: string[] = [];
-const namedVersions = new Map<string, string>([['v1.17', 'current']]);
+const namedVersions = new Map<string, string>([
+  ["v1.17", "current"],
+]);
 
 const mongosyncVersions = new Versions(
   allVersions,
