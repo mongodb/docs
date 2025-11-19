fca879a89f2c0aabc45b6bb4cbd041994c7251fc
John Williams <55147273+jwilliams-mongo@users.noreply.github.com>
2025-11-19 11:07:20 -0600

    DOCSP-55579: fix getting started (#16274)
    https://deploy-preview-16274--mongodb-landing.netlify.app/docs/get-started/
    
    ---------
    
    Co-authored-by: Melissa Mahoney <melissa.mahoney@mongodb.com>

--- content/landing/source/get-started/includes/deploy-commands.sh
+++ content/landing/source/get-started/includes/deploy-commands.sh
@@ -1,11 +1,11 @@
 # start-deploy-local
-atlas deployments setup testing --type local \
+atlas deployments setup myTestDb --type local \
 --mdbVersion 8.0 --port 27017 --connectWith skip
 # end-deploy-local
 
 # start-deploy-cloud
-atlas deployments setup testing --type atlas \
+atlas deployments setup myTestDb --type atlas \
 --provider AWS -r us-east-1 --skipSampleData \
---username <Your username> --password <Your password> \
+--username <database user name> --password <database user password> \
 --connectWith skip --force
 # end-deploy-cloud
