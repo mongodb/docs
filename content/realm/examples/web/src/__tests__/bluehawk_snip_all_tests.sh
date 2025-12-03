951ea9c74af0649c9135144673d1de8b1cc8b460
Dachary <dachary.carey@mongodb.com>
2025-12-03 16:49:27 -0500

    (DOCSP-56123): Remove unneeded Realm Web example app (#16564)
    Jira: https://jira.mongodb.org/browse/DOCSP-56123
    
    This Web app is no longer needed, as Realm is EOL. Removing it to
    mitigate the reported security vulnerabilities from this Dependabot
    alert:
    https://github.com/10gen/docs-mongodb-internal/security/dependabot/525

--- content/realm/examples/web/src/__tests__/bluehawk_snip_all_tests.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-REPO=$(git rev-parse --show-toplevel)
-bluehawk snip $(echo $REPO)/examples/web/src/__tests__ \
--d $(echo $REPO)/source/examples/generated/web
-
-echo 'deposited the snippets to `/source/examples/generated/web`'
--- content/realm/examples/web/src/setupTests.ts
+++
@@ -1,8 +0,0 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
-import { TextEncoder, TextDecoder } from "util";
-global.TextEncoder = TextEncoder;
-(global as any).TextDecoder = TextDecoder;
