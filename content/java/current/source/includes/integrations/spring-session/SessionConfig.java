428196ff95cfad3cc1b950b38e2a12dfa344c951
Nora Reidy <nora.reidy@mongodb.com>
2026-01-05 09:36:44 -0500

    DOCSP-56021: Spring Session docs updates-main-backport (16947) (#16983)
    # Backport
    
    This will backport the following commits from `main` to `main`:
    - [DOCSP-56021: Spring Session docs updates
    (#16947)](https://github.com/10gen/docs-mongodb-internal/pull/16947)
    
    <!--- Backport version: 9.6.6 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sorenlouv/backport)

---
+++ content/java/current/source/includes/integrations/spring-session/SessionConfig.java
@@ -0,0 +1,9 @@
+package com.mongodb.examples.springdatabulkinsert;
+
+import org.mongodb.spring.session.config.annotation.web.http.EnableMongoHttpSession;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableMongoHttpSession
+public class SessionConfig {
+}
