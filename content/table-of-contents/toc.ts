418a1b95815cb81d5c7353d0ad3e5808bfc3eec8
anabellabuckvar <41971124+anabellabuckvar@users.noreply.github.com>
2026-02-19 15:10:30 -0500

    update toc comment to trigger rebuild of all sites (#18081)
    Very small update to comment on toc.ts to trigger rebuild of all content
    sites. Can be reverted or not after merge.

--- content/table-of-contents/toc.ts
+++ content/table-of-contents/toc.ts
@@ -6,7 +6,7 @@ import ToolsData from './L1-data/tools';
 import VoyageData from './L1-data/voyageai';
 import type { L1TocItem } from './types';
 
-// The L1 items for the Unified ToC
+// All the L1 items for the Unified ToC
 export const toc: L1TocItem[] = [
   {
     label: 'Get Started',
