abe800c4737acd2383faafa709e6b31b594831a8
Michael Morisi <michael.morisi@mongodb.com>
2026-02-25 12:50:43 -0500

    DOCSP-48912: Spring Microservice App tutorial-main-backport (17762) (#18174)
    # Backport
    
    This will backport the following commits from `main` to `main`:
    - [DOCSP-48912: Spring Microservice App tutorial
    (#17762)](https://github.com/10gen/docs-mongodb-internal/pull/17762)
    
    <!--- Backport version: 9.6.6 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sorenlouv/backport)

---
+++ content/java/current/source/includes/integrations/spring-microservice/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.mongodb.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
