d7e1be19d924397aec2152209080214a087ca52e
Mark Mwangi <Markmneutron@gmail.com>
2026-02-26 17:11:51 -0500

    DOP-6503: prevent global landing page styles (#18232)
    ## Objective
    
    We want to ensure that headers in our NextJS App have the right color
    relative to the theme. (Dark/Light)
    
    ## Solution
    
    No longer injecting landing page template styles globally. Having them
    injected globally led to them persisting across other pages when
    navigating.
    
    ## How to replicate 
    - Navigate to landing page
    https://mongodbcom-cdn.staging.corp.mongodb.com/docs/
    - Ensure page is set to light theme
    - Click "Get Started" link in table of contents
        - If the page header looks correct, try again in a new incognito tab
    
    ## Testing the fix
    - Follow above steps but start in the landing page for the deploy
    preview tied to this PR

--- platform/docs-nextjs/src/components/templates/landing.tsx
+++ platform/docs-nextjs/src/components/templates/landing.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import type { ReactNode } from 'react';
-import { Global, css as emotionCss } from '@emotion/react';
 import { cx, css } from '@leafygreen-ui/emotion';
 import { useDarkMode } from '@leafygreen-ui/leafygreen-provider';
 import { palette } from '@leafygreen-ui/palette';
@@ -56,126 +55,124 @@ const wrapperStyles = css`
 `;
 
 // The Landing template exclusively represents mongodb.com/docs. All other landings use the ProductLanding template
-const LandingTemplate = ({ children }: BaseTemplateProps & { children: ReactNode }) => {
+const getLandingTemplateStyles = (darkMode: boolean) => {
   const { fontSize, screenSize, size } = theme;
+  return css`
+    & h1,
+    & h2,
+    & h3,
+    & h4 {
+      color: ${darkMode ? palette.gray.light2 : palette.black};
+    }
+
+    & h1,
+    & h2 {
+      font-size: 32px;
+      margin-bottom: ${size.default};
+    }
+    & h2 {
+      margin-top: ${size.large};
+    }
+    & p {
+      color: ${palette.black};
+      font-size: ${fontSize.default};
+      letter-spacing: normal;
+      margin-bottom: ${size.default};
+    }
+    & a {
+      color: ${palette.blue.base};
+      font-size: ${fontSize.small};
+      letter-spacing: normal;
+    }
+    & a:hover {
+      text-decoration: none;
+    }
+    & h1 {
+      align-self: end;
+      grid-column: 2 / 8;
+      grid-row: 1 / 2;
+
+      @media ${screenSize.upToMedium} {
+        grid-column: 2 / 11;
+      }
+
+      @media ${screenSize.upToSmall} {
+        grid-column: 2 / -2;
+      }
+    }
+    & main h1:first-of-type {
+      color: ${palette.white};
+      grid-column: 2/-1;
+      margin-top: 44px;
+      font-size: 48px;
+      line-height: 62px;
+      margin-bottom: 23px;
+      align-self: end;
+      @media ${screenSize.upToMedium} {
+        font-size: 32px;
+        line-height: 40px;
+      }
+    }
+    & main > section > section:first-of-type h2 {
+      color: ${darkMode ? palette.gray.light2 : palette.gray.dark4};
+      font-size: 32px;
+      font-family: 'MongoDB Value Serif';
+      font-weight: 400;
+      margin-top: ${size.medium};
+      margin-bottom: 0px;
+    }
+    & .span-columns {
+      grid-column: 3 / -3 !important;
+      margin: ${size.xlarge} 0;
+    }
+    & section > * {
+      grid-column-start: 2;
+      grid-column-end: 8;
+
+      @media ${screenSize.upToMedium} {
+        grid-column: 2 / -2;
+      }
+    }
+    & .hero-img {
+      grid-column: 1 / -1;
+      grid-row: 1 / 3;
+      width: 100%;
+      object-fit: cover;
+      z-index: -1;
+      object-position: right;
+      height: 100%;
+
+      @media ${screenSize.upTo2XLarge} {
+        object-position: center;
+      }
+    }
+
+    & .introduction {
+      grid-column: 2 / -4;
+      grid-row: 2 / 3;
+      p {
+        color: ${palette.white};
+      }
+
+      @media ${screenSize.upToMedium} {
+        grid-column: 2 / -2;
+      }
+
+      @media ${screenSize.xLargeAndUp} {
+        grid-column: 2 / -4;
+      }
+    }
+  `;
+};
+
+// The Landing template exclusively represents mongodb.com/docs. All other landings use the ProductLanding template
+const LandingTemplate = ({ children }: BaseTemplateProps & { children: ReactNode }) => {
   const { darkMode } = useDarkMode();
   return (
     <>
-      <div>
+      <div className={cx(getLandingTemplateStyles(darkMode))}>
         <main className={cx(wrapperStyles)}>{children}</main>
       </div>
-      <Global
-        styles={emotionCss`
-          h1,
-          h2,
-          h3,
-          h4 {
-            color: ${darkMode ? palette.gray.light2 : palette.black};
-          }
-
-          h1,
-          h2 {
-            font-size: 32px;
-            margin-bottom: ${size.default};
-          }
-          h2 {
-            margin-top: ${size.large};
-          }
-          p {
-            color: ${palette.black};
-            font-size: ${fontSize.small};
-            letter-spacing: normal;
-            margin-bottom: ${size.default};
-          }
-          a {
-            color: ${palette.blue.base};
-            font-size: ${fontSize.small};
-            letter-spacing: normal;
-          }
-          a:hover {
-            text-decoration: none;
-          }
-          h1 {
-            align-self: end;
-            grid-column: 2 / 8;
-            grid-row: 1 / 2;
-
-            @media ${screenSize.upToMedium} {
-              grid-column: 2 / 11;
-            }
-
-            @media ${screenSize.upToSmall} {
-              grid-column: 2 / -2;
-            }
-          }
-          main h1:first-of-type {
-            color: ${palette.white};
-            grid-column: 2/-1;
-            margin-top: 44px;
-            font-size: 48px;
-            line-height: 62px;
-            margin-bottom: 23px;
-            align-self: end;
-            @media ${screenSize.upToMedium} {
-              font-size: 32px;
-              line-height: 40px;
-            }
-          }
-          ${
-            '' /* :first-of-type selector used for precedence
-            above LeafyGreen class selector */
-          }
-          main>section>section:first-of-type h2 {
-            color: ${darkMode ? palette.gray.light2 : palette.gray.dark4};
-            font-size: 32px;
-            font-family: 'MongoDB Value Serif';
-            font-weight: 400;
-            margin-top: ${size.medium};
-            margin-bottom: 0px;
-          }
-          .span-columns {
-            grid-column: 3 / -3 !important;
-            margin: ${size.xlarge} 0;
-          }
-          section > * {
-            grid-column-start: 2;
-            grid-column-end: 8;
-
-            @media ${screenSize.upToMedium} {
-              grid-column: 2 / -2;
-            }
-          }
-          .hero-img {
-            grid-column: 1 / -1;
-            grid-row: 1 / 3;
-            width: 100%;
-            object-fit: cover;
-            z-index: -1;
-            object-position: right;
-            height: 100%;
-
-            @media ${screenSize.upTo2XLarge} {
-              object-position: center;
-            }
-          }
-
-          .introduction {
-            grid-column: 2 / -4;
-            grid-row: 2 / 3;
-            p {
-              color: ${palette.white};
-            }
-
-            @media ${screenSize.upToMedium} {
-              grid-column: 2 / -2;
-            }
-
-            @media ${screenSize.xLargeAndUp} {
-              grid-column: 2 / -4;
-            }
-          }
-        `}
-      />
     </>
   );
 };
