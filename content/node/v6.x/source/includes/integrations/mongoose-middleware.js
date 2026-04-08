2aff6e4e2d567fba14da1549a72705dd45fa4698
shuangela <angelashu103@gmail.com>
2026-04-08 13:41:25 -0400

    Fix Node Typo -main-backport (19291) (#19298)
    # Backport
    
    This will backport the following commits from `main` to `main`:
    - [Fix Node Typo
    (#19291)](https://github.com/10gen/docs-mongodb-internal/pull/19291)
    
    <!--- Backport version: 9.6.6 -->
    
    ### Questions ?
    Please refer to the [Backport tool
    documentation](https://github.com/sorenlouv/backport)

--- content/node/v6.x/source/includes/integrations/mongoose-middleware.js
+++ content/node/v6.x/source/includes/integrations/mongoose-middleware.js
@@ -8,14 +8,14 @@ mongoose.connect("<connection string>");
 
 // start-create-user-improper-email
 const user = await User.create({
-  name: 'Jess Garica',
+  name: 'Jess Garcia',
   email: 'jgarciaemail.com',
 });
 // end-create-user-improper-email
 
 // start-create-user-ok-email
 const user = await User.create({
-  name: 'Jess Garica',
+  name: 'Jess Garcia',
   email: 'jgarcia@email.com',
 });
 // end-create-user-ok-email
