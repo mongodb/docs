44e4f49f1136503683c050cd321db862cc377dc6
corryroot <72401712+corryroot@users.noreply.github.com>
2026-04-17 11:47:47 -0400

    (DOCSP-57626)(DOCSP-57555)(DOCSP-57468)(DOCSP-59067)(DOCSP-57466): Deprecated atlas deployments in tutorials. (#19523)
    @jeroenvervaeke and @lmkerbey-mdb, I deprecated the `atlas deployments`
    command in the Atlas CLI tutorials.
    
    Atlas CLI:
    https://jira.mongodb.org/browse/DOCSP-57626
    https://jira.mongodb.org/browse/DOCSP-57555
    https://jira.mongodb.org/browse/DOCSP-57468
    https://jira.mongodb.org/browse/DOCSP-59067
    
    
    Getting Started Landing:
    https://jira.mongodb.org/browse/DOCSP-57466
    
    
    [STAGE](https://deploy-preview-19523--mongodb-atlas-cli.netlify.app/docs/atlas/cli/upcoming/atlas-cli-local-cloud/)
    [STAGE: Getting
    Started](https://deploy-preview-19523--mongodb-landing.netlify.app/docs/get-started/?)

--- content/landing/source/get-started/includes/deploy-commands.sh
+++ content/landing/source/get-started/includes/deploy-commands.sh
@@ -1,11 +1,11 @@
 # start-deploy-local
-atlas deployments setup myDeployment --type local \
---mdbVersion 8.0 --port <port number> --connectWith skip
+atlas local setup myDeployment \
+--mdbVersion 8.0 --port <port number> --connectWith connectionString
 # end-deploy-local
 
 # start-deploy-cloud
-atlas deployments setup myDeployment --type atlas \
---provider AWS -r us-east-1 --skipSampleData \
+atlas setup --clusterName myDeployment \
+--provider AWS --region us-east-1 --skipSampleData \
 --username <database username> --password <database user password> \
 --connectWith skip --force
 # end-deploy-cloud
