3c84f05c6136f617fe73c8fad26a8d426b5135f8
antellezr-mdb <angel.tellez@mongodb.com>
2026-04-20 18:46:54 -0600

    (DOCSP-58128) EOL Ops Manager v7 (#19446)
    Ops Manager v7.0 EOL
    
    ## Staging Links
    
    - [v7.0 OM
    changelog](https://deploy-preview-19446--mongodb-ops-manager.netlify.app/docs/ops-manager/v7.0/release-notes/application/)
    - [v7.0 —
    Overview](https://deploy-preview-19446--mongodb-ops-manager.netlify.app/docs/ops-manager/v7.0/application/)

--- content/table-of-contents/docset-data/ops-manager.ts
+++ content/table-of-contents/docset-data/ops-manager.ts
@@ -2521,12 +2521,6 @@ const tocData: TocItem[] = [
                     ],
                   },
                   {
-                    label: 'Checkpoints',
-                    contentSite: 'ops-manager',
-                    url: '/docs/ops-manager/:version/reference/api/checkpoints',
-                    versions: { includes: ['v7.0'] },
-                  },
-                  {
                     label: 'Restore Jobs',
                     contentSite: 'ops-manager',
                     url: '/docs/ops-manager/:version/reference/api/restore-jobs',
@@ -2677,37 +2671,31 @@ const tocData: TocItem[] = [
                 contentSite: 'ops-manager',
                 url: '/docs/ops-manager/:version/reference/api/import-deployments',
                 collapsible: true,
-                versions: { excludes: ['v7.0'] },
                 items: [
                   {
                     label: 'Create Import Deployment Request',
                     contentSite: 'ops-manager',
                     url: '/docs/ops-manager/:version/reference/api/import-deployments/create',
-                    versions: { excludes: ['v7.0'] },
                   },
                   {
                     label: 'Get Import Deployment Requests',
                     contentSite: 'ops-manager',
                     url: '/docs/ops-manager/:version/reference/api/import-deployments/get-all',
-                    versions: { excludes: ['v7.0'] },
                   },
                   {
                     label: 'Get Import Deployment Request Status',
                     contentSite: 'ops-manager',
                     url: '/docs/ops-manager/:version/reference/api/import-deployments/get-status',
-                    versions: { excludes: ['v7.0'] },
                   },
                   {
                     label: 'Cancel Import Deployment Request',
                     contentSite: 'ops-manager',
                     url: '/docs/ops-manager/:version/reference/api/import-deployments/cancel',
-                    versions: { excludes: ['v7.0'] },
                   },
                   {
                     label: 'Delete Import Deployment Request',
                     contentSite: 'ops-manager',
                     url: '/docs/ops-manager/:version/reference/api/import-deployments/delete',
-                    versions: { excludes: ['v7.0'] },
                   },
                 ],
               },
