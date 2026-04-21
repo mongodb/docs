72982bf2a9725be4c27b0edcc46f7f9ba8cfd2d1
Andrew Steinheiser <me@iamandrew.io>
2026-04-21 10:24:06 -0700

    DOP-6762 - fix procedure heading styles (#19618)
    [DOP-6762](https://jira.mongodb.org/browse/DOP-6762)
    
    The procedure headings should now be bold (expected behavior)
    
    I tested with http://localhost:8888/docs/atlas/billing

--- platform/docs-nextjs/src/mdx-components/Procedure/StepHeading.tsx
+++ platform/docs-nextjs/src/mdx-components/Procedure/StepHeading.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Body } from '@leafygreen-ui/typography';
+import { SkipPTagContext } from '@/mdx-components/Paragraph';
 
 type StepHeadingProps = {
   children: React.ReactNode;
@@ -8,8 +9,9 @@ type StepHeadingProps = {
 
 export const StepHeading = ({ children }: StepHeadingProps) => {
   return (
-    <Body as="h4" weight="medium">
-      {children}
+    <Body as="h4" weight="bold">
+      {/* Step titles are a single MDX paragraph; skip inner Body so weight/color inherit from this h4 (matches Snooty). */}
+      <SkipPTagContext.Provider value={true}>{children}</SkipPTagContext.Provider>
     </Body>
   );
 };
