32556000dc84c783344f3d83aa471b247768620f
Bianca <76073842+biancalaube@users.noreply.github.com>
2026-05-26 11:28:00 -0400

    DOP-6922: fix docsets data in search (#20445)
    ## Ticket
    
    DOP-6922: Add docsets data to search page 
    
    ### Current behaviour 
    https://docs-on-nextjs.netlify.app/docs/search/?q=test&page=1
    
    ### Staging behaviour 
    * Refine your search is built with the correct data 
    
    
    https://deploy-preview-20445--docs-on-nextjs.netlify.app/docs/search/?q=test&page=1
    <img width="1264" height="497" alt="Screenshot 2026-05-26 at 9 03 06 AM"
    src="https://github.com/user-attachments/assets/61e717f1-7196-47b1-8552-47039d2e9472"
    />
    
    ### Testing
    
    * You will need the CORS UnBlock chrome extension and you will need to
    enable the follow values and select `start`
    <img width="851" height="597" alt="Screenshot 2026-05-26 at 9 04 50 AM"
    src="https://github.com/user-attachments/assets/4fe3226b-adff-448a-806f-f030018537ab"
    />

--- platform/docs-nextjs/src/app/docs/search/page.tsx
+++ platform/docs-nextjs/src/app/docs/search/page.tsx
@@ -1,14 +1,16 @@
 import { cookies as nextCookies } from 'next/headers';
 import envConfig from '@/utils/env-config';
+import { getAllDocsetsWithVersionsCached } from '@/services/db/docsets';
 import { SearchPageContent } from './SearchPageContent';
 
-export default function SearchPage() {
+export default async function SearchPage() {
   const cookieStore = nextCookies();
   const cookieValues = cookieStore.getAll().reduce((acc, cookie) => {
     acc[cookie.name] = cookie.value;
     return acc;
   }, {} as Record<string, string>);
 
-  // TODO: Fetch real docsets so the version switcher works on the search page
-  return <SearchPageContent docsets={[]} env={envConfig.DB_ENV} cookies={cookieValues} />;
+  const docsets = await getAllDocsetsWithVersionsCached();
+
+  return <SearchPageContent docsets={docsets} env={envConfig.DB_ENV} cookies={cookieValues} />;
 }
