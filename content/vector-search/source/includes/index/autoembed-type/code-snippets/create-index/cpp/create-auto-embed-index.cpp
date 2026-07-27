88dcbba60e52260a7b1be1d2d07b2926dce85035
aabourezkDB <ahmed.abourezk@mongodb.com>
2026-07-27 10:16:22 +0100

    DOCSP-62105 [CPP] Create Vector Search Index Page  (#22134)
    # Pull Request Info
    - Created Vector Search Index for C++
    
    ### JIRA Ticket
    
    [DOCSP-62105](https://jira.mongodb.org/browse/DOCSP-62105)
    
    ### Staging Links
    
    <!-- start insert-links --><!-- end insert-links -->
    
    ## Self-Review Checklist
    
    - [x] Is the build log free of new errors and warnings?
    - [x] Does the content render properly in staging (including links)?
    - [ ] Did you check the [Style
    Guide](https://www.mongodb.com/docs/meta/) for any style questions?
    - [ ] Are [facets and meta
    keywords](https://wiki.corp.mongodb.com/display/DE/Docs+Taxonomy)
    accurate?
    - [ ] Are page titles 20+ characters long and [SEO
    relevant](https://docs.google.com/spreadsheets/d/1Wkt0-5z04KmcMNscN5bjUKnzwWAtMq9VESp-Lz6r2o8/edit?usp=sharing)?
    - [ ] If you deleted/renamed files, did you add redirects?
    - [ ] If you added/deleted/moved files, did you update the offline and
    TypeScript TOCs?

---
+++ content/vector-search/source/includes/index/autoembed-type/code-snippets/create-index/cpp/create-auto-embed-index.cpp
@@ -0,0 +1,65 @@
+#include <bsoncxx/builder/basic/document.hpp>
+#include <iostream>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/search_index_view.hpp>
+#include <mongocxx/uri.hpp>
+#include <thread>
+
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_array;
+using bsoncxx::builder::basic::make_document;
+
+int main() {
+  try {
+    mongocxx::instance inst{};
+
+    // Connect to your deployment
+    const auto uri = mongocxx::uri{"<connection-string>"};
+    mongocxx::client conn{uri};
+
+    // Access your database and collection
+    auto db = conn["<database-name>"];
+    auto collection = db["<collection-name>"];
+
+    auto siv = collection.search_indexes();
+    std::string name = "<index-name>";
+
+    // Define the index with automated embedding and filter fields
+    auto definition = make_document(
+        kvp("fields",
+            make_array(make_document(
+                kvp("type", "autoEmbed"), kvp("modality", "text"),
+                kvp("path", "<field-to-index>"),
+                kvp("model", "<embedding-model>")))));
+    auto model =
+        mongocxx::search_index_model(name, definition.view())
+            .type("vectorSearch");
+
+    // Create the search index
+    siv.create_one(model);
+    std::cout << "New search index named " << name << " is building."
+              << std::endl;
+
+    // Wait for initial sync to complete
+    std::cout << "Polling to check if the index is ready. This may take up to "
+                 "a minute."
+              << std::endl;
+    bool queryable = false;
+    while (!queryable) {
+      auto indexes = siv.list();
+      for (const auto& index : indexes) {
+        if (index["name"].get_value() == name) {
+          queryable = index["queryable"].get_bool();
+        }
+      }
+      if (!queryable) {
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+      }
+    }
+    std::cout << name << " is ready for querying." << std::endl;
+  } catch (const std::exception& e) {
+    std::cout << "Exception: " << e.what() << std::endl;
+  }
+  return 0;
+}
