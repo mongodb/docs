9947d8d0fbdb988ed1cbf1944d23595db6a0f0be
oliviacaulfield <olivia.caulfield@mongodb.com>
2026-09-08 16:32:00 -0400

    DOCSP-42509-asp-source-sample-solar (#23321)
    ## Description 
    Adds a Solar Stream example to the $source aggregation stage reference
    page, demonstrating the built-in sample_stream_solar sample connection
    with a $match/$merge pipeline.
    
    Testing
    [here](https://docs.google.com/document/d/1Lbz8Tz7Y08-HodHMXUJbCaZY3q2bgRxaUY_0KzQuMpA/edit?tab=t.0#heading=h.7crpuxgcsu83).
    
    ## Staging
    -
    https://deploy-preview-23321--atlas-mdb-docs.netlify.app/docs/atlas/atlas-stream-processing/sp-agg-source/#solar-stream-example
    
    ## Jira
    https://jira.mongodb.org/browse/DOCSP-42509

---
+++ content/atlas/source/includes/examples/sp-agg-source/solar-find-result.sh
@@ -0,0 +1,11 @@
+[
+  {
+    _id: ObjectId('6a8f5f0352c6ffe64c8884ad'),
+    device_id: 'device_3',
+    event_type: 0,
+    group_id: 8,
+    max_watts: 450,
+    obs: { watts: 428, temp: 10 },
+    timestamp: '2024-08-12T21:47:47.287+00:00'
+  }
+]
---
+++ content/atlas/source/includes/examples/sp-agg-source/solar-find.js
@@ -0,0 +1,1 @@
+db.getSiblingDB("solarDb").solarColl.find()
